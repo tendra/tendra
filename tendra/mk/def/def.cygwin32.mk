24875f7013d77fe4d41b13e2ff50d6ee447997aa
Amar Takhar <verm@darkbeer.org>
2002-10-20 15:28:24 +0000

    Of course, i forgot to add the basefiles for cygwin in the last commit.
    Submitted by:	Boris Popov <bp@butya.kz>

---
+++ tendra/mk/def/def.cygwin32.mk
@@ -0,0 +1,53 @@
+# $TenDRA$
+#
+# Binary / variable definitions for the cygwin32 environment.
+
+AWK?=		/bin/awk
+BASENAME?=	/bin/basename
+CAT?=		/bin/cat
+CHMOD?=		/bin/chmod
+CHOWN?=		/bin/chown
+CHGRP?=		/bin/chgrp
+CP?=		/bin/cp
+CUT?=		/bin/cut
+DC?=		/bin/dc
+DIRNAME?=	/bin/dirname
+ECHO?=		/bin/echo
+EGREP?=		/bin/egrep
+FALSE?=		/bin/false
+FILE?=		/bin/file
+FIND?=		/bin/find
+GREP?=		/bin/grep
+GTAR?=		/bin/tar
+GUNZIP_CMD?=	/bin/gunzip -f
+GZCAT?=		/bin/gzcat
+GZIP?=		-9
+GZIP_CMD?=	/bin/gzip -nf ${GZIP}
+HEAD?=		/bin/head
+ID?=		/bin/id
+LDCONFIG?=	NO/sbin/ldconfig
+LN?=		/bin/ln
+LS?=		/bin/ls
+MKDIR?=		/bin/mkdir -p
+MTREE?=		NO/usr/sbin/mtree
+MV?=		/bin/mv
+OP_DLS?=	1
+PATCH?=		GNU/usr/bin/patch
+PAX?=		NO/bin/pax
+PKGLOCALEDIR?=	share
+RM?=		/bin/rm
+RMDIR?=		/bin/rmdir
+SED?=		/bin/sed
+SETENV?=	/bin/env
+SH?=		/bin/sh
+SORT?=		/bin/sort
+SU?=		/bin/su
+TAIL?=		/bin/tail
+TEST?=		/bin/test
+TOUCH?=		/bin/touch
+TR?=		/bin/tr
+TRUE?=		/bin/true
+TYPE?=		type				# Shell builtin
+WC?=		/bin/wc
+XARGS?=		/bin/xargs
+PERL?=		/bin/perl
