f6d6a008b7d6db192da2f404df166c45c5cc3696
Amar Takhar <verm@darkbeer.org>
2002-11-11 10:21:48 +0000

    Fix a couple small nits with the Linux build.
    Submitted by:Christian Cornelssen <ccorn@cs.tu-berlin.de>

--- tendra/mk/config/config.linux.mk
+++ tendra/mk/config/config.linux.mk
@@ -5,14 +5,14 @@ UNAME=		/bin/uname
 PREFIX=		/usr/local
 TMP_DIR=	/tmp
 
-TMP_CPU!=	${UNAME} -p
+TMP_CPU!=	${UNAME} -m
 MACH_OS!=	${UNAME}
 MACH_VERS!=	${UNAME} -r
 
 #not sure of the best way to get the exec type, suggestions?
 MACH_EXEC=	elf
 
-.if ${TMP_CPU} == "i386" || ${TMP_CPU} == "i586" || ${TMP_CPU} == "i686"
+.if ${TMP_CPU} == "i386" || ${TMP_CPU} == "i486" || ${TMP_CPU} == "i586" || ${TMP_CPU} == "i686"
 MACH_CPU=	80x86
 .endif
 
