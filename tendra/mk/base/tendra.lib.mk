212fe251d702f6e4fac976238b7d73f91a1ca4b4
Amar Takhar <verm@darkbeer.org>
2002-11-26 02:31:02 +0000

    This file is required to build lib/libtdf, lib/libcpp and any other future libraries.

---
+++ tendra/mk/base/tendra.lib.mk
@@ -0,0 +1,15 @@
+# $TenDRA$
+
+# this hack strips out any path info from the sourcefiles since the objects
+# are placed into the current working directory!
+
+OBJS+=  ${SRCS:N*.h:R:S/$/.o/g}
+OBJS_NODIR+=  ${SRCS:N*.h:R:S/$/.o/g:C/.*\/(.*)$/\1/g}
+
+# add the library to the cleanfiles.
+CLEANFILES+= ${LIB}
+
+${LIB}: ${OBJS}
+	@echo ${OBJS}
+	${AR} cr ${.TARGET} ${.IMPSRC}
+	${RANLIB} ${.TARGET}
