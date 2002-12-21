bcb129ea967792d7d02048112900a71e704d3d2a
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:32:53 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: weights.c,v $
     * Revision 1.1.1.1  1998/01/17  15:55:52  release
     * First version to be checked into rolling release.
     *
     * Revision 1.14  1997/10/10  18:25:40  pwe
     * prep ANDF-DE revision
     *
     * Revision 1.13  1996/11/08  16:19:22  pwe
     * check_stack to check before modifying stack
     *
     * Revision 1.12  1996/05/20  14:30:45  pwe
     * improved 64-bit handling
     *
     * Revision 1.11  1996/05/13  12:52:10  pwe
     * undo premature commit
     *
     * Revision 1.9  1996/01/22  14:31:13  pwe
     * PIC const*const, contop top_tag & linux 64-bit ints
     *
     * Revision 1.8  1996/01/17  11:24:42  pwe
     * resurrect performance
     *
     * Revision 1.7  1995/09/05  16:25:14  pwe
     * specials and exception changes
     *
     * Revision 1.6  1995/08/04  08:29:56  pwe
     * 4.0 general procs implemented
     *
     * Revision 1.5  1995/05/05  12:47:32  pwe
     * missing bracket
     *
     * Revision 1.4  1995/05/02  13:27:42  pwe
     * strengthen test for no side effect
     *
     * Revision 1.3  1995/03/23  13:25:39  pwe
     * limit scale in deeply nested repeats
     *
     * Revision 1.2  1995/01/30  12:56:59  pwe
     * Ownership -> PWE, tidy banners
     *
     * Revision 1.1  1994/10/27  14:15:22  jmf
     * Initial revision
     *
     * Revision 1.1  1994/07/12  14:43:24  jmf
     * Initial revision
     *
    *********************************************************************

--- tendra/src/installers/80x86/common/weights.c
+++ tendra/src/installers/80x86/common/weights.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -33,57 +60,7 @@
 /**********************************************************************
 $Author$
 $Date$
-$Revision$
-$Log$
-Revision 1.1  2002/01/26 21:31:12  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.1.1.1  1998/01/17  15:55:52  release
- * First version to be checked into rolling release.
- *
- * Revision 1.14  1997/10/10  18:25:40  pwe
- * prep ANDF-DE revision
- *
- * Revision 1.13  1996/11/08  16:19:22  pwe
- * check_stack to check before modifying stack
- *
- * Revision 1.12  1996/05/20  14:30:45  pwe
- * improved 64-bit handling
- *
- * Revision 1.11  1996/05/13  12:52:10  pwe
- * undo premature commit
- *
- * Revision 1.9  1996/01/22  14:31:13  pwe
- * PIC const*const, contop top_tag & linux 64-bit ints
- *
- * Revision 1.8  1996/01/17  11:24:42  pwe
- * resurrect performance
- *
- * Revision 1.7  1995/09/05  16:25:14  pwe
- * specials and exception changes
- *
- * Revision 1.6  1995/08/04  08:29:56  pwe
- * 4.0 general procs implemented
- *
- * Revision 1.5  1995/05/05  12:47:32  pwe
- * missing bracket
- *
- * Revision 1.4  1995/05/02  13:27:42  pwe
- * strengthen test for no side effect
- *
- * Revision 1.3  1995/03/23  13:25:39  pwe
- * limit scale in deeply nested repeats
- *
- * Revision 1.2  1995/01/30  12:56:59  pwe
- * Ownership -> PWE, tidy banners
- *
- * Revision 1.1  1994/10/27  14:15:22  jmf
- * Initial revision
- *
- * Revision 1.1  1994/07/12  14:43:24  jmf
- * Initial revision
- *
-**********************************************************************/
+$Revision$*/
 
 
 #include "config.h"
