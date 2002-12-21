19429ad8106df080f1a09e8baf2eff1f4a7ae847
Amar Takhar <verm@darkbeer.org>
2002-12-21 07:03:18 +0000

    Change the order of the build defs, so it's above the def: target rather than below, makes it far easier to read.  Cleaned up the comment, as well.

--- tendra/mk/base/tendra.def.mk
+++ tendra/mk/base/tendra.def.mk
@@ -1,7 +1,20 @@
 # $TenDRA$
 #
-# Entries so we know where our build utilities are.
+# Entries so we know where our build utilities are and various other nits we
+# need during the build process.
 #
+
+LEXI=		${OBJ_PREFIX}/utilities/lexi/lexi
+SID=		${OBJ_PREFIX}/utilities/sid/sid
+CALCULUS=	${OBJ_PREFIX}/utilities/calculus/calculus
+MAKE_ERR=	${OBJ_PREFIX}/utilities/make_err/make_err
+MAKE_TDF=	${OBJ_PREFIX}/utilities/make_tdf/make_tdf
+TLD=		${OBJ_PREFIX}/tools/tld/tld
+DISP=		${OBJ_PREFIX}/tools/disp/disp
+PL=		${OBJ_PREFIX}/tools/pl/pl
+TCC=		${OBJ_PREFIX}/tools/tcc/tcc
+TNC=		${OBJ_PREFIX}/tools/tnc/tnc
+TSPEC=		${OBJ_PREFIX}/tools/tspec/tspec
 
 def:
 	@echo "lexi:     ${LEXI}"
@@ -15,15 +28,4 @@ def:
 	@echo "tcc:      ${TCC}"
 	@echo "tnc:      ${TNC}"
 	@echo "tspec:    ${TSPEC}"
-
-LEXI=		${OBJ_PREFIX}/utilities/lexi/lexi
-SID=		${OBJ_PREFIX}/utilities/sid/sid
-CALCULUS=	${OBJ_PREFIX}/utilities/calculus/calculus
-MAKE_ERR=	${OBJ_PREFIX}/utilities/make_err/make_err
-MAKE_TDF=	${OBJ_PREFIX}/utilities/make_tdf/make_tdf
-TLD=		${OBJ_PREFIX}/tools/tld/tld
-DISP=		${OBJ_PREFIX}/tools/disp/disp
-PL=		${OBJ_PREFIX}/tools/pl/pl
-TCC=		${OBJ_PREFIX}/tools/tcc/tcc
-TNC=		${OBJ_PREFIX}/tools/tnc/tnc
-TSPEC=		${OBJ_PREFIX}/tools/tspec/tspec
+	@echo DEFAULT_BUILD:	${DEFAULT_BUILD}
