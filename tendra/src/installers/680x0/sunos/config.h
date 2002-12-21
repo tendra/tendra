aa0e437f40855070cce7c1cc723687dc147d93e2
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:31:22 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: config.h,v $
     * Revision 1.2  1998/03/16  11:25:19  release
     * Modifications prior to version 4.1.2.
     *
     * Revision 1.1.1.1  1998/01/17  15:55:50  release
     * First version to be checked into rolling release.
     *
    Revision 1.2  1997/10/29 10:22:11  ma
    Replaced use_alloca with has_alloca.
    
    Revision 1.1.1.1  1997/10/13 12:42:49  ma
    First version.
    
    Revision 1.5  1997/10/13 08:49:21  ma
    Made all pl_tests for general proc & exception handling pass.
    
    Revision 1.4  1997/06/18 12:04:51  ma
    Merged with Input Baseline changes.
    
    Revision 1.3  1997/04/20 11:30:22  ma
    Introduced gcproc.c & general_proc.[ch].
    Added cases for apply_general_proc next to apply_proc in all files.
    
    Revision 1.2  1997/03/20 12:50:00  ma
    Removed #if 0 and #endif around used definitions.
    
    Revision 1.1.1.1  1997/03/14 07:50:11  ma
    Imported from DRA
    
     * Revision 1.1.1.1  1996/09/20  10:56:53  john
     *
     * Revision 1.2  1996/07/05  14:19:05  john
     * Changes for spec 3.1
     *
     * Revision 1.1.1.1  1996/03/26  15:45:10  john
     *
     * Revision 1.7  94/11/16  10:36:26  10:36:26  ra (Robert Andrews)
     * Added support for complex numbers.
     *
     * Revision 1.6  94/11/08  11:22:40  11:22:40  ra (Robert Andrews)
     * Added constants for cases optimisation.
     *
     * Revision 1.5  94/06/29  14:19:50  14:19:50  ra (Robert Andrews)
     * Added flags for common installer section options.
     *
     * Revision 1.4  94/02/21  15:57:45  15:57:45  ra (Robert Andrews)
     * Move prototype information to compiler.h.
     *
     * Revision 1.3  93/11/19  16:17:44  16:17:44  ra (Robert Andrews)
     * Now never use alloca.  Added has64bits (false).
     *
     * Revision 1.2  93/05/24  15:55:49  15:55:49  ra (Robert Andrews)
     * Declare check_anyway.
     *
     * Revision 1.1  93/02/22  17:15:29  17:15:29  ra (Robert Andrews)
     * Initial revision
     *
    --------------------------------------------------------------------------

--- tendra/src/installers/680x0/sunos/config.h
+++ tendra/src/installers/680x0/sunos/config.h
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 /*
 			    VERSION INFORMATION
@@ -32,69 +59,7 @@
 
 --------------------------------------------------------------------------
 $Header$
---------------------------------------------------------------------------
-$Log$
-Revision 1.1  2002/01/26 21:31:11  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.2  1998/03/16  11:25:19  release
- * Modifications prior to version 4.1.2.
- *
- * Revision 1.1.1.1  1998/01/17  15:55:50  release
- * First version to be checked into rolling release.
- *
-Revision 1.2  1997/10/29 10:22:11  ma
-Replaced use_alloca with has_alloca.
-
-Revision 1.1.1.1  1997/10/13 12:42:49  ma
-First version.
-
-Revision 1.5  1997/10/13 08:49:21  ma
-Made all pl_tests for general proc & exception handling pass.
-
-Revision 1.4  1997/06/18 12:04:51  ma
-Merged with Input Baseline changes.
-
-Revision 1.3  1997/04/20 11:30:22  ma
-Introduced gcproc.c & general_proc.[ch].
-Added cases for apply_general_proc next to apply_proc in all files.
-
-Revision 1.2  1997/03/20 12:50:00  ma
-Removed #if 0 and #endif around used definitions.
-
-Revision 1.1.1.1  1997/03/14 07:50:11  ma
-Imported from DRA
-
- * Revision 1.1.1.1  1996/09/20  10:56:53  john
- *
- * Revision 1.2  1996/07/05  14:19:05  john
- * Changes for spec 3.1
- *
- * Revision 1.1.1.1  1996/03/26  15:45:10  john
- *
- * Revision 1.7  94/11/16  10:36:26  10:36:26  ra (Robert Andrews)
- * Added support for complex numbers.
- * 
- * Revision 1.6  94/11/08  11:22:40  11:22:40  ra (Robert Andrews)
- * Added constants for cases optimisation.
- * 
- * Revision 1.5  94/06/29  14:19:50  14:19:50  ra (Robert Andrews)
- * Added flags for common installer section options.
- * 
- * Revision 1.4  94/02/21  15:57:45  15:57:45  ra (Robert Andrews)
- * Move prototype information to compiler.h.
- * 
- * Revision 1.3  93/11/19  16:17:44  16:17:44  ra (Robert Andrews)
- * Now never use alloca.  Added has64bits (false).
- * 
- * Revision 1.2  93/05/24  15:55:49  15:55:49  ra (Robert Andrews)
- * Declare check_anyway.
- * 
- * Revision 1.1  93/02/22  17:15:29  17:15:29  ra (Robert Andrews)
- * Initial revision
- * 
---------------------------------------------------------------------------
-*/
+--------------------------------------------------------------------------*/
 
 
 /*
