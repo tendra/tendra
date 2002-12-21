3ffd9a21e29e95ba7e2f9ce330a3af8a582e0251
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:39:55 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: config.h,v $
     * Revision 1.2  1998/03/16  11:25:33  release
     * Modifications prior to version 4.1.2.
     *
     * Revision 1.1.1.1  1998/01/17  15:56:04  release
     * First version to be checked into rolling release.
     *
     * Revision 1.4  1996/02/14  17:19:12  wfs
     * "next_caller_offset" and "next_callee_offset" have become special tokens
     * defined in "spec_tok.c". Bug fix to a "and_tag" optimization in
     * "oprators.c". A few bug fixes in "makecode.c" arising from the variable
     * caller tests. "promote_pars" defined in "config.h".
     *
     * Revision 1.3  1996/01/08  12:01:48  wfs
     * Cosmetic changes to usage message in "hppatrans.c". "issparc" set to 0 in
     * "config.h". Change to "scan()" regarding the passing of 64 bit int params.
     *
     * Revision 1.2  1995/12/18  13:11:05  wfs
     * Put hppatrans uder cvs control. Major Changes made since last release
     * include:
     * (i) PIC code generation.
     * (ii) Profiling.
     * (iii) Dynamic Initialization.
     * (iv) Debugging of Exception Handling and Diagnostics.
     *
     * Revision 5.3  1995/10/16  14:42:39  wfs
     * *** empty log message ***
     *
     * Revision 5.2  1995/09/21  12:41:43  wfs
     * #included "compiler.h".
     *
     * Revision 5.1  1995/09/20  11:29:18  wfs
     * Added "#define isAlpha 0".
     *
     * Revision 5.0  1995/08/25  13:55:12  wfs
     * Preperation for August 95 Glue release
     *
     * Revision 4.0  1995/08/25  13:34:00  wfs
     * *** empty log message ***
     *
     * Revision 4.0  1995/08/25  13:34:00  wfs
     * *** empty log message ***
     *
     * Revision 3.4  1995/08/25  10:51:44  wfs
     * Removed a few sparc specific macros. Some new flags have been
     * introduced
     *
     * Revision 3.4  1995/08/25  10:51:44  wfs
     * Removed a few sparc specific macros. Some new flags have been
     * introduced
     *
     * Revision 3.1  95/04/10  16:26:01  16:26:01  wfs (William Simmonds)
     * Apr95 tape version.
     *
     * Revision 3.0  95/03/30  11:15:49  11:15:49  wfs (William Simmonds)
     * Mar95 tape version with CRCR95_178 bug fix.
     *
     * Revision 2.0  95/03/15  15:25:32  15:25:32  wfs (William Simmonds)
     * spec 3.1 changes implemented, tests outstanding.
     *
     * Revision 1.1  95/01/11  13:36:39  13:36:39  wfs (William Simmonds)
     * Initial revision
     *

--- tendra/src/installers/hppa/hpux/config.h
+++ tendra/src/installers/hppa/hpux/config.h
@@ -1,101 +1,61 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
-/*
-$Log$
-Revision 1.1  2002/01/26 21:31:22  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.2  1998/03/16  11:25:33  release
- * Modifications prior to version 4.1.2.
- *
- * Revision 1.1.1.1  1998/01/17  15:56:04  release
- * First version to be checked into rolling release.
- *
- * Revision 1.4  1996/02/14  17:19:12  wfs
- * "next_caller_offset" and "next_callee_offset" have become special tokens
- * defined in "spec_tok.c". Bug fix to a "and_tag" optimization in
- * "oprators.c". A few bug fixes in "makecode.c" arising from the variable
- * caller tests. "promote_pars" defined in "config.h".
- *
- * Revision 1.3  1996/01/08  12:01:48  wfs
- * Cosmetic changes to usage message in "hppatrans.c". "issparc" set to 0 in
- * "config.h". Change to "scan()" regarding the passing of 64 bit int params.
- *
- * Revision 1.2  1995/12/18  13:11:05  wfs
- * Put hppatrans uder cvs control. Major Changes made since last release
- * include:
- * (i) PIC code generation.
- * (ii) Profiling.
- * (iii) Dynamic Initialization.
- * (iv) Debugging of Exception Handling and Diagnostics.
- *
- * Revision 5.3  1995/10/16  14:42:39  wfs
- * *** empty log message ***
- *
- * Revision 5.2  1995/09/21  12:41:43  wfs
- * #included "compiler.h".
- *
- * Revision 5.1  1995/09/20  11:29:18  wfs
- * Added "#define isAlpha 0".
- *
- * Revision 5.0  1995/08/25  13:55:12  wfs
- * Preperation for August 95 Glue release
- *
- * Revision 4.0  1995/08/25  13:34:00  wfs
- * *** empty log message ***
- *
- * Revision 4.0  1995/08/25  13:34:00  wfs
- * *** empty log message ***
- *
- * Revision 3.4  1995/08/25  10:51:44  wfs
- * Removed a few sparc specific macros. Some new flags have been
- * introduced
- *
- * Revision 3.4  1995/08/25  10:51:44  wfs
- * Removed a few sparc specific macros. Some new flags have been
- * introduced
- *
- * Revision 3.1  95/04/10  16:26:01  16:26:01  wfs (William Simmonds)
- * Apr95 tape version.
- * 
- * Revision 3.0  95/03/30  11:15:49  11:15:49  wfs (William Simmonds)
- * Mar95 tape version with CRCR95_178 bug fix.
- * 
- * Revision 2.0  95/03/15  15:25:32  15:25:32  wfs (William Simmonds)
- * spec 3.1 changes implemented, tests outstanding.
- * 
- * Revision 1.1  95/01/11  13:36:39  13:36:39  wfs (William Simmonds)
- * Initial revision
- * 
-*/
+/**/
 
 
 #ifndef CONFIG_INCLUDED
