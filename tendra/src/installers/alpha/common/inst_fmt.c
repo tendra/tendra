8c39412295b07f9576269400b39218d2fbafb564
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:34:11 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: inst_fmt.c,v $
     * Revision 1.1.1.1  1998/01/17  15:56:00  release
     * First version to be checked into rolling release.
     *
     * Revision 1.17  1997/09/05  12:22:47  john
     * Change to jmp
     *
     * Revision 1.16  1996/02/15  09:53:16  john
     * Changed integer_jump_fn
     *
     * Revision 1.15  1995/12/04  09:12:33  john
     * Portability fixes
     *
     * Revision 1.14  1995/11/13  12:43:30  john
     * Minor change
     *
     * Revision 1.13  1995/10/16  12:07:46  john
     * Change to alignment calculation
     *
     * Revision 1.12  1995/09/04  16:23:37  john
     * Fix to general procs
     *
     * Revision 1.11  1995/08/30  16:14:51  john
     * Fix for use of AT register
     *
     * Revision 1.10  1995/08/21  08:44:49  john
     * Changed include files
     *
     * Revision 1.9  1995/05/25  15:33:10  john
     * Cosmetic changes
     *
     * Revision 1.8  1995/05/23  10:56:51  john
     * Changes for 64 bit support
     *
     * Revision 1.7  1995/05/16  10:52:34  john
     * Now uses correct register names (where supported by the assembler).
     *
     * Revision 1.6  1995/04/10  14:13:18  john
     * Minor changes
     *
     * Revision 1.5  1995/04/07  11:03:45  john
     * Changes to assembler output, plus change to floating point operations.
     *
     * Revision 1.4  1995/03/29  14:07:36  john
     * Fixed error in noat block handling
     *
     * Revision 1.3  1995/03/29  10:30:51  john
     * Added resources for scheduler
     *
     * Revision 1.2  1995/03/28  12:44:30  john
     * Changed resources for scheduler
     *
     * Revision 1.1.1.1  1995/03/23  10:39:11  john
     * Entered into CVS
     *
     * Revision 1.1  1995/03/23  10:06:32  john
     * Initial revision
     *

--- tendra/src/installers/alpha/common/inst_fmt.c
+++ tendra/src/installers/alpha/common/inst_fmt.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -36,69 +63,7 @@
   the code scheduler at appropriate points.
 */
 
-/*
-$Log$
-Revision 1.1  2002/01/26 21:31:14  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.1.1.1  1998/01/17  15:56:00  release
- * First version to be checked into rolling release.
- *
- * Revision 1.17  1997/09/05  12:22:47  john
- * Change to jmp
- *
- * Revision 1.16  1996/02/15  09:53:16  john
- * Changed integer_jump_fn
- *
- * Revision 1.15  1995/12/04  09:12:33  john
- * Portability fixes
- *
- * Revision 1.14  1995/11/13  12:43:30  john
- * Minor change
- *
- * Revision 1.13  1995/10/16  12:07:46  john
- * Change to alignment calculation
- *
- * Revision 1.12  1995/09/04  16:23:37  john
- * Fix to general procs
- *
- * Revision 1.11  1995/08/30  16:14:51  john
- * Fix for use of AT register
- *
- * Revision 1.10  1995/08/21  08:44:49  john
- * Changed include files
- *
- * Revision 1.9  1995/05/25  15:33:10  john
- * Cosmetic changes
- *
- * Revision 1.8  1995/05/23  10:56:51  john
- * Changes for 64 bit support
- *
- * Revision 1.7  1995/05/16  10:52:34  john
- * Now uses correct register names (where supported by the assembler).
- *
- * Revision 1.6  1995/04/10  14:13:18  john
- * Minor changes
- *
- * Revision 1.5  1995/04/07  11:03:45  john
- * Changes to assembler output, plus change to floating point operations.
- *
- * Revision 1.4  1995/03/29  14:07:36  john
- * Fixed error in noat block handling
- *
- * Revision 1.3  1995/03/29  10:30:51  john
- * Added resources for scheduler
- *
- * Revision 1.2  1995/03/28  12:44:30  john
- * Changed resources for scheduler
- *
- * Revision 1.1.1.1  1995/03/23  10:39:11  john
- * Entered into CVS
- *
- * Revision 1.1  1995/03/23  10:06:32  john
- * Initial revision
- *
-*/
+/**/
 #include "config.h"
 #include "inst_fmt.h"
 #include "main.h"
