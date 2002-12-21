05e53bf81d0e69a553b563896d999525d9047a51
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:33:55 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: eval.c,v $
     * Revision 1.1.1.1  1998/01/17  15:55:59  release
     * First version to be checked into rolling release.
     *
     * Revision 1.11  1996/02/15  09:52:38  john
     * Added offset constructions
     *
     * Revision 1.10  1995/08/30  16:14:28  john
     * Fix for new string_tag
     *
     * Revision 1.9  1995/08/21  08:44:13  john
     * Changed include files
     *
     * Revision 1.8  1995/07/27  09:36:52  john
     * Reformatting & changes for spec 4.0
     *
     * Revision 1.7  1995/05/23  10:56:11  john
     * Changes for 64 bit support
     *
     * Revision 1.6  1995/05/16  10:47:33  john
     * Changes for spec 3.1
     *
     * Revision 1.5  1995/04/10  14:12:57  john
     * Minor changes
     *
     * Revision 1.4  1995/04/07  11:02:02  john
     * Re-arranged the assembler output routines
     *
     * Revision 1.3  1995/04/04  08:17:46  john
     * Fixed evaluation of compounds containing bitfields
     *
     * Revision 1.2  1995/03/29  14:01:22  john
     * Changes to keep tcheck happy
     *
     * Revision 1.1.1.1  1995/03/23  10:39:06  john
     * Entered into CVS
     *
     * Revision 1.16  1995/03/23  10:03:54  john
     * Added support for scheduler + fixes for AVS
     *
     * Revision 1.15  1995/01/31  14:21:53  john
     * Minor change
     *
     * Revision 1.14  1995/01/26  13:39:07  john
     * Various portability improvements
     *
     * Revision 1.13  1995/01/20  13:43:12  john
     * Added default case to oneval
     *
     * Revision 1.12  1995/01/12  15:14:43  john
     * Removed dead code
     *

--- tendra/src/installers/alpha/common/eval.c
+++ tendra/src/installers/alpha/common/eval.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -34,63 +61,7 @@
 static char vcid[] = "$Id$";
 #endif /* lint */
 
-/*
-$Log$
-Revision 1.1  2002/01/26 21:31:14  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.1.1.1  1998/01/17  15:55:59  release
- * First version to be checked into rolling release.
- *
- * Revision 1.11  1996/02/15  09:52:38  john
- * Added offset constructions
- *
- * Revision 1.10  1995/08/30  16:14:28  john
- * Fix for new string_tag
- *
- * Revision 1.9  1995/08/21  08:44:13  john
- * Changed include files
- *
- * Revision 1.8  1995/07/27  09:36:52  john
- * Reformatting & changes for spec 4.0
- *
- * Revision 1.7  1995/05/23  10:56:11  john
- * Changes for 64 bit support
- *
- * Revision 1.6  1995/05/16  10:47:33  john
- * Changes for spec 3.1
- *
- * Revision 1.5  1995/04/10  14:12:57  john
- * Minor changes
- *
- * Revision 1.4  1995/04/07  11:02:02  john
- * Re-arranged the assembler output routines
- *
- * Revision 1.3  1995/04/04  08:17:46  john
- * Fixed evaluation of compounds containing bitfields
- *
- * Revision 1.2  1995/03/29  14:01:22  john
- * Changes to keep tcheck happy
- *
- * Revision 1.1.1.1  1995/03/23  10:39:06  john
- * Entered into CVS
- *
- * Revision 1.16  1995/03/23  10:03:54  john
- * Added support for scheduler + fixes for AVS
- *
- * Revision 1.15  1995/01/31  14:21:53  john
- * Minor change
- *
- * Revision 1.14  1995/01/26  13:39:07  john
- * Various portability improvements
- *
- * Revision 1.13  1995/01/20  13:43:12  john
- * Added default case to oneval
- *
- * Revision 1.12  1995/01/12  15:14:43  john
- * Removed dead code
- *
-*/
+/**/
 
 /*
   eval.c
