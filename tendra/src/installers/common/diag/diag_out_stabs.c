1f9f924450e78df0f25d04a71f09951689e25872
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:33:09 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: diag_out.c,v $
     * Revision 1.2  1998/03/27  09:47:50  release
     * Changes for 4.1.2 release.
     *
     * Revision 1.1.1.1  1998/01/17  15:55:52  release
     * First version to be checked into rolling release.
     *
     * Revision 1.12  1997/04/02  10:33:21  pwe
     * diagnose pl_tests
     *
     * Revision 1.11  1997/03/24  12:43:26  pwe
     * outn int->long
     *
     * Revision 1.10  1996/10/29  14:55:32  pwe
     * correct linux/elf stabs for global variables
     *
     * Revision 1.9  1996/07/05  09:07:43  pwe
     * correct stabs enums
     *
     * Revision 1.8  1996/05/09  17:30:47  pwe
     * shift invalidate_dest, and stabs postlude
     *
     * Revision 1.7  1996/04/19  16:14:16  pwe
     * simplified use of global id = id, correcting linux call problem
     *
     * Revision 1.6  1996/03/13  13:43:26  pwe
     * diags for long long
     *
     * Revision 1.5  1996/02/08  13:45:29  pwe
     * Linux elf v aout option
     *
     * Revision 1.4  1995/12/20  11:13:54  pwe
     * stabs local variable scopes
     *
     * Revision 1.3  1995/11/30  10:19:43  pwe
     * diag struct struct
     *
     * Revision 1.2  1995/03/28  14:28:53  pwe
     * correct diagnose empty file
     *
     * Revision 1.1  1995/03/17  18:29:52  pwe
     * stabs diagnostics for solaris and linux
     *
    *********************************************************************

--- tendra/src/installers/common/diag/diag_out_stabs.c
+++ tendra/src/installers/common/diag/diag_out_stabs.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -33,54 +60,7 @@
 /**********************************************************************
 $Author$
 $Date$
-$Revision$
-$Log$
-Revision 1.1  2002/01/26 21:31:13  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.2  1998/03/27  09:47:50  release
- * Changes for 4.1.2 release.
- *
- * Revision 1.1.1.1  1998/01/17  15:55:52  release
- * First version to be checked into rolling release.
- *
- * Revision 1.12  1997/04/02  10:33:21  pwe
- * diagnose pl_tests
- *
- * Revision 1.11  1997/03/24  12:43:26  pwe
- * outn int->long
- *
- * Revision 1.10  1996/10/29  14:55:32  pwe
- * correct linux/elf stabs for global variables
- *
- * Revision 1.9  1996/07/05  09:07:43  pwe
- * correct stabs enums
- *
- * Revision 1.8  1996/05/09  17:30:47  pwe
- * shift invalidate_dest, and stabs postlude
- *
- * Revision 1.7  1996/04/19  16:14:16  pwe
- * simplified use of global id = id, correcting linux call problem
- *
- * Revision 1.6  1996/03/13  13:43:26  pwe
- * diags for long long
- *
- * Revision 1.5  1996/02/08  13:45:29  pwe
- * Linux elf v aout option
- *
- * Revision 1.4  1995/12/20  11:13:54  pwe
- * stabs local variable scopes
- *
- * Revision 1.3  1995/11/30  10:19:43  pwe
- * diag struct struct
- *
- * Revision 1.2  1995/03/28  14:28:53  pwe
- * correct diagnose empty file
- *
- * Revision 1.1  1995/03/17  18:29:52  pwe
- * stabs diagnostics for solaris and linux
- *
-**********************************************************************/
+$Revision$*/
 
 #include "config.h"
 #include "common_types.h"
