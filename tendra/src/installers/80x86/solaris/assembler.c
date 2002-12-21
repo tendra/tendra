feada508c5ebb0593899d058705fe12e3f2d7aa9
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:33:26 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: assembler.c,v $
     * Revision 1.1.1.1  1998/01/17  15:55:52  release
     * First version to be checked into rolling release.
     *
     * Revision 1.10  1997/10/10  18:25:56  pwe
     * prep ANDF-DE revision
     *
     * Revision 1.9  1997/03/20  16:24:27  pwe
     * dwarf2
     *
     * Revision 1.8  1997/02/18  11:43:22  pwe
     * NEWDIAGS for debugging optimised code
     *
     * Revision 1.7  1995/10/23  17:34:47  pwe
     * dynamic initialisation PIC, and sco diags
     *
     * Revision 1.6  1995/10/13  15:20:21  pwe
     * solaris PIC and linux tcc
     *
     * Revision 1.5  1995/10/09  15:14:40  pwe
     * dynamic initialisation etc
     *
     * Revision 1.4  1995/01/30  12:57:32  pwe
     * Ownership -> PWE, tidy banners
     *
     * Revision 1.3  1994/10/28  11:21:27  jmf
     * Changed out_switch_jump to subtract lower.
     *
     * Revision 1.2  1994/07/13  07:48:52  jmf
     * Added Log
     *
    *********************************************************************

--- tendra/src/installers/80x86/solaris/assembler.c
+++ tendra/src/installers/80x86/solaris/assembler.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -33,42 +60,7 @@
 /**********************************************************************
 $Author$
 $Date$
-$Revision$
-$Log$
-Revision 1.1  2002/01/26 21:31:13  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.1.1.1  1998/01/17  15:55:52  release
- * First version to be checked into rolling release.
- *
- * Revision 1.10  1997/10/10  18:25:56  pwe
- * prep ANDF-DE revision
- *
- * Revision 1.9  1997/03/20  16:24:27  pwe
- * dwarf2
- *
- * Revision 1.8  1997/02/18  11:43:22  pwe
- * NEWDIAGS for debugging optimised code
- *
- * Revision 1.7  1995/10/23  17:34:47  pwe
- * dynamic initialisation PIC, and sco diags
- *
- * Revision 1.6  1995/10/13  15:20:21  pwe
- * solaris PIC and linux tcc
- *
- * Revision 1.5  1995/10/09  15:14:40  pwe
- * dynamic initialisation etc
- *
- * Revision 1.4  1995/01/30  12:57:32  pwe
- * Ownership -> PWE, tidy banners
- *
- * Revision 1.3  1994/10/28  11:21:27  jmf
- * Changed out_switch_jump to subtract lower.
- *
- * Revision 1.2  1994/07/13  07:48:52  jmf
- * Added Log
- *
-**********************************************************************/
+$Revision$*/
 
 
 #include "config.h"
