692d5809c12d14e6a94d92cac0c0cd201153379a
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:35:00 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: config.h,v $
     * Revision 1.2  1998/03/16  11:25:30  release
     * Modifications prior to version 4.1.2.
     *
     * Revision 1.1.1.1  1998/01/17  15:56:01  release
     * First version to be checked into rolling release.
     *
     * Revision 1.7  1995/09/20  10:46:48  john
     * Changes isAlpha macro
     *
     * Revision 1.6  1995/09/13  11:03:44  john
     * Added HAS_MAGIC flag
     *
     * Revision 1.5  1995/07/27  09:34:50  john
     * Set diagnose_registers macro
     *
     * Revision 1.4  1995/06/28  10:23:17  john
     * New option: has_rotate
     *
     * Revision 1.3  1995/06/13  13:59:54  john
     * Changed default value of DO_SCHEDULE
     *
     * Revision 1.2  1995/05/23  10:54:18  john
     * New configuration flag : has_bitfield_ops
     *
     * Revision 1.1.1.1  1995/03/23  10:39:04  john
     * Entered into CVS
     *
     * Revision 1.18  1995/03/23  10:02:34  john
     * Added DO_SCHEDULE macro
     *
     * Revision 1.17  1995/01/31  14:24:50  john
     * Moved some definitions into TenDRA.h
     *
     * Revision 1.16  1995/01/10  09:39:16  john
     * Added definition of value_of_null
     *

--- tendra/src/installers/alpha/osf1/config.h
+++ tendra/src/installers/alpha/osf1/config.h
@@ -1,76 +1,62 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
 /* 	$Id$	 */
-/*
-$Log$
-Revision 1.1  2002/01/26 21:31:16  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.2  1998/03/16  11:25:30  release
- * Modifications prior to version 4.1.2.
- *
- * Revision 1.1.1.1  1998/01/17  15:56:01  release
- * First version to be checked into rolling release.
- *
- * Revision 1.7  1995/09/20  10:46:48  john
- * Changes isAlpha macro
- *
- * Revision 1.6  1995/09/13  11:03:44  john
- * Added HAS_MAGIC flag
- *
- * Revision 1.5  1995/07/27  09:34:50  john
- * Set diagnose_registers macro
- *
- * Revision 1.4  1995/06/28  10:23:17  john
- * New option: has_rotate
- *
- * Revision 1.3  1995/06/13  13:59:54  john
- * Changed default value of DO_SCHEDULE
- *
- * Revision 1.2  1995/05/23  10:54:18  john
- * New configuration flag : has_bitfield_ops
- *
- * Revision 1.1.1.1  1995/03/23  10:39:04  john
- * Entered into CVS
- *
- * Revision 1.18  1995/03/23  10:02:34  john
- * Added DO_SCHEDULE macro
- *
- * Revision 1.17  1995/01/31  14:24:50  john
- * Moved some definitions into TenDRA.h
- *
- * Revision 1.16  1995/01/10  09:39:16  john
- * Added definition of value_of_null
- *
-*/
+/**/
 #ifndef config_key
 #define config_key 1
 
