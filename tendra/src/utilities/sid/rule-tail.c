13a82f90c23594ba218f9fcc7979f20fac7c0c27
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:50:58 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
     * _Log: rule-tail.c,v $
     * Revision 1.1.1.1  1998/01/17  15:57:47  release
     * First version to be checked into rolling release.
     *
     * Revision 1.3  1994/12/15  09:58:53  smf
     * Brought into line with OSSG C Coding Standards Document, as per
     * "CR94_178.sid+tld-update".
     *
     * Revision 1.2  1994/11/11  11:47:09  smf
     * Fixed a bug in the tail recursion elimination, for bug fix
     * CR94_127.sid-tail-rec.
     * There was a problem with tail calls that had reference parameters in an
     * earlier version of SID, and they had been disabled.  This should have been
     * fixed when the output routines were fixed to do references properly, but the
     * check wasn't removed.  It has been now.
     *
     * Revision 1.1.1.1  1994/07/25  16:04:41  smf
     * Initial import of SID 1.8 non shared files.
     *
    *

--- tendra/src/utilities/sid/rule-tail.c
+++ tendra/src/utilities/sid/rule-tail.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -71,30 +98,7 @@
  * graph.  If any such cycles are found, then all of the rules in the cycle
  * are marked as needing a function implementation.
  *
- *** Change Log:
- * $Log$
- * Revision 1.1  2002/01/26 21:32:13  asmodai
- * Initial version of TenDRA 4.1.2.
- *
- * Revision 1.1.1.1  1998/01/17  15:57:47  release
- * First version to be checked into rolling release.
- *
- * Revision 1.3  1994/12/15  09:58:53  smf
- * Brought into line with OSSG C Coding Standards Document, as per
- * "CR94_178.sid+tld-update".
- *
- * Revision 1.2  1994/11/11  11:47:09  smf
- * Fixed a bug in the tail recursion elimination, for bug fix
- * CR94_127.sid-tail-rec.
- * There was a problem with tail calls that had reference parameters in an
- * earlier version of SID, and they had been disabled.  This should have been
- * fixed when the output routines were fixed to do references properly, but the
- * check wasn't removed.  It has been now.
- *
- * Revision 1.1.1.1  1994/07/25  16:04:41  smf
- * Initial import of SID 1.8 non shared files.
- *
-**/
+ *** Change Log:*/
 
 /****************************************************************************/
 
