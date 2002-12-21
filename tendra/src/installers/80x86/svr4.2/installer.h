91b919eef5553ea012743411f8537f2e156da622
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:33:40 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: config.h,v $
     * Revision 1.2  1998/03/16  11:25:28  release
     * Modifications prior to version 4.1.2.
     *
     * Revision 1.1.1.1  1998/01/17  15:55:53  release
     * First version to be checked into rolling release.
     *
     * Revision 1.20  1998/01/11  15:15:10  pwe
     * remove NEWDIAGS
     *
     * Revision 1.19  1997/04/02  10:33:57  pwe
     * diagnose pl_tests
     *
     * Revision 1.18  1996/05/20  14:31:13  pwe
     * improved 64-bit handling
     *
     * Revision 1.17  1996/05/13  12:52:22  pwe
     * undo premature commit
     *
     * Revision 1.15  1996/02/20  14:45:44  pwe
     * linux/elf return struct
     *
     * Revision 1.14  1996/02/08  13:45:48  pwe
     * Linux elf v aout option
     *
     * Revision 1.13  1995/12/01  11:54:42  pwe
     * solaris proc equality
     *
     * Revision 1.12  1995/10/09  15:14:51  pwe
     * dynamic initialisation etc
     *
     * Revision 1.11  1995/09/29  16:18:25  pwe
     * gcc_compatible default on Linux
     *
     * Revision 1.10  1995/09/20  12:00:51  pwe
     * svr4 and solaris initial_value, and solaris 64 bit correction
     *
     * Revision 1.9  1995/09/19  15:43:19  pwe
     * round, fp overflow etc
     *
     * Revision 1.8  1995/09/05  16:25:40  pwe
     * specials and exception changes
     *
     * Revision 1.7  1995/09/01  17:30:22  pwe
     * traps and Build scripts
     *
     * Revision 1.6  1995/08/30  16:07:24  pwe
     * prepare exception trapping
     *
     * Revision 1.5  1995/07/07  15:22:42  pwe
     * prepare for 4.0, and correct PIC switch
     *
     * Revision 1.4  1995/01/30  14:43:57  pwe
     * defined value_of_null for CR95_032
     *
     * Revision 1.3  1995/01/30  12:57:41  pwe
     * Ownership -> PWE, tidy banners
     *
     * Revision 1.2  1994/11/08  15:09:13  jmf
     * Added substitute_complex
     *
     * Revision 1.1  1994/10/27  12:16:59  jmf
     * Initial revision
     *
     * Revision 1.3  1994/08/04  13:37:04  jmf
     * Added condassign_tag
     *
     * Revision 1.2  1994/07/13  07:51:05  jmf
     * Added Log
     *
    *********************************************************************

--- tendra/src/installers/80x86/svr4.2/installer.h
+++ tendra/src/installers/80x86/svr4.2/installer.h
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -33,81 +60,7 @@
 /**********************************************************************
 $Author$
 $Date$
-$Revision$
-$Log$
-Revision 1.1  2002/01/26 21:31:13  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.2  1998/03/16  11:25:28  release
- * Modifications prior to version 4.1.2.
- *
- * Revision 1.1.1.1  1998/01/17  15:55:53  release
- * First version to be checked into rolling release.
- *
- * Revision 1.20  1998/01/11  15:15:10  pwe
- * remove NEWDIAGS
- *
- * Revision 1.19  1997/04/02  10:33:57  pwe
- * diagnose pl_tests
- *
- * Revision 1.18  1996/05/20  14:31:13  pwe
- * improved 64-bit handling
- *
- * Revision 1.17  1996/05/13  12:52:22  pwe
- * undo premature commit
- *
- * Revision 1.15  1996/02/20  14:45:44  pwe
- * linux/elf return struct
- *
- * Revision 1.14  1996/02/08  13:45:48  pwe
- * Linux elf v aout option
- *
- * Revision 1.13  1995/12/01  11:54:42  pwe
- * solaris proc equality
- *
- * Revision 1.12  1995/10/09  15:14:51  pwe
- * dynamic initialisation etc
- *
- * Revision 1.11  1995/09/29  16:18:25  pwe
- * gcc_compatible default on Linux
- *
- * Revision 1.10  1995/09/20  12:00:51  pwe
- * svr4 and solaris initial_value, and solaris 64 bit correction
- *
- * Revision 1.9  1995/09/19  15:43:19  pwe
- * round, fp overflow etc
- *
- * Revision 1.8  1995/09/05  16:25:40  pwe
- * specials and exception changes
- *
- * Revision 1.7  1995/09/01  17:30:22  pwe
- * traps and Build scripts
- *
- * Revision 1.6  1995/08/30  16:07:24  pwe
- * prepare exception trapping
- *
- * Revision 1.5  1995/07/07  15:22:42  pwe
- * prepare for 4.0, and correct PIC switch
- *
- * Revision 1.4  1995/01/30  14:43:57  pwe
- * defined value_of_null for CR95_032
- *
- * Revision 1.3  1995/01/30  12:57:41  pwe
- * Ownership -> PWE, tidy banners
- *
- * Revision 1.2  1994/11/08  15:09:13  jmf
- * Added substitute_complex
- *
- * Revision 1.1  1994/10/27  12:16:59  jmf
- * Initial revision
- *
- * Revision 1.3  1994/08/04  13:37:04  jmf
- * Added condassign_tag
- *
- * Revision 1.2  1994/07/13  07:51:05  jmf
- * Added Log
- *
-**********************************************************************/
+$Revision$*/
 
 #ifndef config_key
 #define config_key 1
