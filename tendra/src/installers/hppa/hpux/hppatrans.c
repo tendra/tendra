f97181db0987c6bf71f352f7296008ab4e56a8a1
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:39:57 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: hppatrans.c,v $
     * Revision 1.1.1.1  1998/01/17  15:56:04  release
     * First version to be checked into rolling release.
     *
     * Revision 1.6  1996/04/18  11:09:12  wfs
     * Fixed a bug in "needscan.c" where "locptr_tag" was being wrapped around
     * the wrong exps.
     *
     * Revision 1.5  1996/01/30  15:36:29  wfs
     * Added the dynamic initialization files "dyn_begin.s" and "dyn_end.s" to
     * the hppatrans repository. The bl_install and bl_update scripts were
     * expanded to deal with these new "initcode" files.
     *
     * Revision 1.4  1996/01/08  12:01:50  wfs
     * Cosmetic changes to usage message in "hppatrans.c". "issparc" set to 0 in
     * "config.h". Change to "scan()" regarding the passing of 64 bit int params.
     *
     * Revision 1.3  1995/12/20  12:11:18  wfs
     * "-v" option disabled. "ManualPages" put under cvs control.
     *
     * Revision 1.2  1995/12/18  13:11:35  wfs
     * Put hppatrans uder cvs control. Major Changes made since last release
     * include:
     * (i) PIC code generation.
     * (ii) Profiling.
     * (iii) Dynamic Initialization.
     * (iv) Debugging of Exception Handling and Diagnostics.
     *
     * Revision 5.5  1995/10/30  12:57:56  wfs
     * Standardized the format of the warning messages.
     *
     * Revision 5.4  1995/10/25  14:32:46  wfs
     * Removed a few commented out instructions.
     *
     * Revision 5.3  1995/10/20  13:55:38  wfs
     * Changes for gcc compilation.
     *
     * Revision 5.2  1995/09/25  10:17:42  wfs
     * Added "#ifdef _SYMTAB_INCLUDED" provisios around any code which refers
     * to "hpux-symtab.h". We cannot legally distribute this header file.
     *
     * Revision 5.1  1995/09/15  12:29:03  wfs
     * Minor changes made to stop gcc compiler from complaining + changed
     * messages citing "hppatrans" to "trans".
     *
     * Revision 5.0  1995/08/25  13:42:58  wfs
     * Preperation for August 25 Glue release
     *
     * Revision 3.4  1995/08/25  11:22:17  wfs
     * Name of installer occuring in messages changed from "hppatrans" to
     * "trans"
     *
     * Revision 3.4  1995/08/25  11:22:17  wfs
     * Name of installer occuring in messages changed from "hppatrans" to
     * "trans"
     *
     * Revision 3.3  1995/08/25  09:36:12  wfs
     * some superflous code removed ( includingincluding the SunOS "-G" option).
     * minor alterations for diagnostics stuff.
     *
     * Revision 3.1  95/04/10  16:26:49  16:26:49  wfs (William Simmonds)
     * Apr95 tape version.
     *
     * Revision 3.0  95/03/30  11:17:27  11:17:27  wfs (William Simmonds)
     * Mar95 tape version with CRCR95_178 bug fix.
     *
     * Revision 2.0  95/03/15  15:27:34  15:27:34  wfs (William Simmonds)
     * spec 3.1 changes implemented, tests outstanding.
     *
     * Revision 1.1  95/01/17  13:58:43  13:58:43  wfs (William Simmonds)
     * Initial revision
     *

--- tendra/src/installers/hppa/hpux/hppatrans.c
+++ tendra/src/installers/hppa/hpux/hppatrans.c
@@ -1,110 +1,61 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
-/*
-$Log$
-Revision 1.1  2002/01/26 21:31:22  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.1.1.1  1998/01/17  15:56:04  release
- * First version to be checked into rolling release.
- *
- * Revision 1.6  1996/04/18  11:09:12  wfs
- * Fixed a bug in "needscan.c" where "locptr_tag" was being wrapped around
- * the wrong exps.
- *
- * Revision 1.5  1996/01/30  15:36:29  wfs
- * Added the dynamic initialization files "dyn_begin.s" and "dyn_end.s" to
- * the hppatrans repository. The bl_install and bl_update scripts were
- * expanded to deal with these new "initcode" files.
- *
- * Revision 1.4  1996/01/08  12:01:50  wfs
- * Cosmetic changes to usage message in "hppatrans.c". "issparc" set to 0 in
- * "config.h". Change to "scan()" regarding the passing of 64 bit int params.
- *
- * Revision 1.3  1995/12/20  12:11:18  wfs
- * "-v" option disabled. "ManualPages" put under cvs control.
- *
- * Revision 1.2  1995/12/18  13:11:35  wfs
- * Put hppatrans uder cvs control. Major Changes made since last release
- * include:
- * (i) PIC code generation.
- * (ii) Profiling.
- * (iii) Dynamic Initialization.
- * (iv) Debugging of Exception Handling and Diagnostics.
- *
- * Revision 5.5  1995/10/30  12:57:56  wfs
- * Standardized the format of the warning messages.
- *
- * Revision 5.4  1995/10/25  14:32:46  wfs
- * Removed a few commented out instructions.
- *
- * Revision 5.3  1995/10/20  13:55:38  wfs
- * Changes for gcc compilation.
- *
- * Revision 5.2  1995/09/25  10:17:42  wfs
- * Added "#ifdef _SYMTAB_INCLUDED" provisios around any code which refers
- * to "hpux-symtab.h". We cannot legally distribute this header file.
- *
- * Revision 5.1  1995/09/15  12:29:03  wfs
- * Minor changes made to stop gcc compiler from complaining + changed
- * messages citing "hppatrans" to "trans".
- *
- * Revision 5.0  1995/08/25  13:42:58  wfs
- * Preperation for August 25 Glue release
- *
- * Revision 3.4  1995/08/25  11:22:17  wfs
- * Name of installer occuring in messages changed from "hppatrans" to
- * "trans"
- *
- * Revision 3.4  1995/08/25  11:22:17  wfs
- * Name of installer occuring in messages changed from "hppatrans" to
- * "trans"
- *
- * Revision 3.3  1995/08/25  09:36:12  wfs
- * some superflous code removed ( includingincluding the SunOS "-G" option).
- * minor alterations for diagnostics stuff.
- *
- * Revision 3.1  95/04/10  16:26:49  16:26:49  wfs (William Simmonds)
- * Apr95 tape version.
- * 
- * Revision 3.0  95/03/30  11:17:27  11:17:27  wfs (William Simmonds)
- * Mar95 tape version with CRCR95_178 bug fix.
- * 
- * Revision 2.0  95/03/15  15:27:34  15:27:34  wfs (William Simmonds)
- * spec 3.1 changes implemented, tests outstanding.
- * 
- * Revision 1.1  95/01/17  13:58:43  13:58:43  wfs (William Simmonds)
- * Initial revision
- * 
-*/
+/**/
 
 
 
