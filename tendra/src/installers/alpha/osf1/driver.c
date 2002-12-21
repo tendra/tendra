0f46caabb8144d4fa9ea03cc35ef2153e8ce03bd
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:35:03 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: driver.c,v $
     * Revision 1.2  1998/02/06  17:04:28  release
     * Last minute pre-release polishing.
     *
     * Revision 1.1.1.1  1998/01/17  15:56:01  release
     * First version to be checked into rolling release.
     *
     * Revision 1.10  1996/03/25  14:56:18  john
     * introduced do_extern_adds
     *
     * Revision 1.9  1995/09/06  09:00:42  john
     * removed old command line argument parsing
     *
     * Revision 1.8  1995/06/28  10:23:36  john
     * Added RELEASE info to version information
     *
     * Revision 1.7  1995/06/13  14:00:22  john
     * Changed denormal constant handling
     *
     * Revision 1.6  1995/05/23  10:55:35  john
     * Removed unneeded header file.
     *
     * Revision 1.5  1995/05/16  10:46:51  john
     * Fixed error message
     *
     * Revision 1.4  1995/04/07  11:01:18  john
     * Added new command line arg processing.  Changed handling of IEEE denormals
     *
     * Revision 1.3  1995/03/29  14:06:52  john
     * Added inclusion of bool.h
     *
     * Revision 1.2  1995/03/29  10:30:10  john
     * Added option to control handling of denormalised numbers.
     *
     * Revision 1.1.1.1  1995/03/23  10:39:30  john
     * Entered into CVS
     *
     * Revision 1.21  1995/03/03  15:22:35  john
     * Added scheduling code
     *
     * Revision 1.20  1995/02/13  08:58:39  john
     * Turned foralls optimisation off, by default.
     *
     * Revision 1.19  1995/01/26  13:38:35  john
     * Added symboldecs.h to list of included files
     *
     * Revision 1.18  1995/01/18  11:44:33  john
     * Added : to version numbers
     *
     * Revision 1.17  1995/01/12  15:07:12  john
     * Minor change
     *

--- tendra/src/installers/alpha/osf1/driver.c
+++ tendra/src/installers/alpha/osf1/driver.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -40,63 +67,7 @@ static char vcid[] = "$Id$";
   JM  17/9/93
 */
 
-/*
-$Log$
-Revision 1.1  2002/01/26 21:31:16  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.2  1998/02/06  17:04:28  release
- * Last minute pre-release polishing.
- *
- * Revision 1.1.1.1  1998/01/17  15:56:01  release
- * First version to be checked into rolling release.
- *
- * Revision 1.10  1996/03/25  14:56:18  john
- * introduced do_extern_adds
- *
- * Revision 1.9  1995/09/06  09:00:42  john
- * removed old command line argument parsing
- *
- * Revision 1.8  1995/06/28  10:23:36  john
- * Added RELEASE info to version information
- *
- * Revision 1.7  1995/06/13  14:00:22  john
- * Changed denormal constant handling
- *
- * Revision 1.6  1995/05/23  10:55:35  john
- * Removed unneeded header file.
- *
- * Revision 1.5  1995/05/16  10:46:51  john
- * Fixed error message
- *
- * Revision 1.4  1995/04/07  11:01:18  john
- * Added new command line arg processing.  Changed handling of IEEE denormals
- *
- * Revision 1.3  1995/03/29  14:06:52  john
- * Added inclusion of bool.h
- *
- * Revision 1.2  1995/03/29  10:30:10  john
- * Added option to control handling of denormalised numbers.
- *
- * Revision 1.1.1.1  1995/03/23  10:39:30  john
- * Entered into CVS
- *
- * Revision 1.21  1995/03/03  15:22:35  john
- * Added scheduling code
- *
- * Revision 1.20  1995/02/13  08:58:39  john
- * Turned foralls optimisation off, by default.
- *
- * Revision 1.19  1995/01/26  13:38:35  john
- * Added symboldecs.h to list of included files
- *
- * Revision 1.18  1995/01/18  11:44:33  john
- * Added : to version numbers
- *
- * Revision 1.17  1995/01/12  15:07:12  john
- * Minor change
- *
-*/
+/**/
 
 
 #include "config.h"
