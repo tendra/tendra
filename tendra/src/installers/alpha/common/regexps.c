e4a3263d9711d28219e5d752f5de0956ec13febf
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:34:32 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: regexps.c,v $
     * Revision 1.1.1.1  1998/01/17  15:56:01  release
     * First version to be checked into rolling release.
     *
     * Revision 1.7  1996/01/17  09:31:37  john
     * Fix to support current_env dependancy
     *
     * Revision 1.6  1995/12/04  09:13:11  john
     * Fix to dependancy check
     *
     * Revision 1.5  1995/10/16  12:09:21  john
     * Change to alignment calculation
     *
     * Revision 1.4  1995/08/21  08:46:20  john
     * Changed include files
     *
     * Revision 1.3  1995/05/16  10:55:17  john
     * Cosmetic change
     *
     * Revision 1.2  1995/03/29  14:05:14  john
     * Changes to keep tcheck happy
     *
     * Revision 1.1.1.1  1995/03/23  10:39:20  john
     * Entered into CVS
     *
     * Revision 1.7  1995/03/13  11:44:19  john
     * Removed bitad case
     *
     * Revision 1.6  1995/01/26  13:48:31  john
     * Removed unused variable
     *

--- tendra/src/installers/alpha/common/regexps.c
+++ tendra/src/installers/alpha/common/regexps.c
@@ -1,30 +1,57 @@
 /*
-    		 Crown Copyright (c) 1997
-    
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-    
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-    
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-    
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-    
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *    
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *    
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *    
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *    
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *    
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
@@ -38,42 +65,7 @@ static char vcid[] = "$Id$";
    regexps.c
 */
 
-/*
-$Log$
-Revision 1.1  2002/01/26 21:31:15  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.1.1.1  1998/01/17  15:56:01  release
- * First version to be checked into rolling release.
- *
- * Revision 1.7  1996/01/17  09:31:37  john
- * Fix to support current_env dependancy
- *
- * Revision 1.6  1995/12/04  09:13:11  john
- * Fix to dependancy check
- *
- * Revision 1.5  1995/10/16  12:09:21  john
- * Change to alignment calculation
- *
- * Revision 1.4  1995/08/21  08:46:20  john
- * Changed include files
- *
- * Revision 1.3  1995/05/16  10:55:17  john
- * Cosmetic change
- *
- * Revision 1.2  1995/03/29  14:05:14  john
- * Changes to keep tcheck happy
- *
- * Revision 1.1.1.1  1995/03/23  10:39:20  john
- * Entered into CVS
- *
- * Revision 1.7  1995/03/13  11:44:19  john
- * Removed bitad case
- *
- * Revision 1.6  1995/01/26  13:48:31  john
- * Removed unused variable
- *
-*/
+/**/
 
 #include "config.h"
 #include "expmacs.h"
