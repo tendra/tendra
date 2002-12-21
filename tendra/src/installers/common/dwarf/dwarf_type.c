da57913c975143011375c770fb0c47baa01b9fec
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:36:48 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: dwarf_type.c,v $
     * Revision 1.2  1998/02/04  10:43:38  release
     * Changes during testing.
     *
     * Revision 1.1.1.1  1998/01/17  15:55:48  release
     * First version to be checked into rolling release.
     *
     * Revision 1.7  1996/07/05  09:03:58  pwe
     * correction for enums
     *
     * Revision 1.6  1996/03/13  13:50:37  pwe
     * diags for long long
     *
     * Revision 1.5  1995/11/28  13:40:31  pwe
     * int more likely than long for 32 bit
     *
     * Revision 1.4  1995/09/28  12:40:00  pwe
     * dwarf.h via import, and type changes for tcc checks
     *
     * Revision 1.3  1995/09/13  16:13:18  pwe
     * tidy for gcc
     *
     * Revision 1.2  1995/09/13  14:25:41  pwe
     * tidy for gcc
     *
     * Revision 1.1.1.1  1995/08/14  14:30:25  pwe
     * transferred from DJCH
     *
    *********************************************************************

--- tendra/src/installers/common/dwarf/dwarf_type.c
+++ tendra/src/installers/common/dwarf/dwarf_type.c
@@ -1,69 +1,64 @@
 /*
-    		 Crown Copyright (c) 1997
-
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
 /**********************************************************************
 $Author$
 $Date$
-$Revision$
-$Log$
-Revision 1.1  2002/01/26 21:31:18  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.2  1998/02/04  10:43:38  release
- * Changes during testing.
- *
- * Revision 1.1.1.1  1998/01/17  15:55:48  release
- * First version to be checked into rolling release.
- *
- * Revision 1.7  1996/07/05  09:03:58  pwe
- * correction for enums
- *
- * Revision 1.6  1996/03/13  13:50:37  pwe
- * diags for long long
- *
- * Revision 1.5  1995/11/28  13:40:31  pwe
- * int more likely than long for 32 bit
- *
- * Revision 1.4  1995/09/28  12:40:00  pwe
- * dwarf.h via import, and type changes for tcc checks
- *
- * Revision 1.3  1995/09/13  16:13:18  pwe
- * tidy for gcc
- *
- * Revision 1.2  1995/09/13  14:25:41  pwe
- * tidy for gcc
- *
- * Revision 1.1.1.1  1995/08/14  14:30:25  pwe
- * transferred from DJCH
- *
-**********************************************************************/
+$Revision$*/
 
 /* LOG 3/6/93 added extra message to is_fund_dwarf when using
    non-diag libraries djch */
