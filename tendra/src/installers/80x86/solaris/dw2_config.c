d4872e373d7f24d4d6eadada51d0a0833ca20d2a
Amos B. Haven <nonce@ten15.org>
2002-12-21 21:33:30 +0000

    Remove Log keyword.  The following log entry was made prior to the creation of the tendra.org repository.
    _Log: dw2_config.c,v $
     * Revision 1.4  1998/03/17  16:54:15  release
     * Couple of minor fixes.
     *
     * Revision 1.3  1998/03/11  11:03:13  pwe
     * DWARF optimisation info
     *
     * Revision 1.2  1998/01/21  10:30:02  pwe
     * labdiff change
     *
     * Revision 1.1.1.1  1998/01/17  15:55:52  release
     * First version to be checked into rolling release.
     *
     * Revision 1.6  1998/01/09  14:47:55  pwe
     * prep restructure
     *
     * Revision 1.5  1997/12/04  20:01:30  pwe
     * ANDF-DE V1.9
     *
     * Revision 1.4  1997/10/23  09:37:29  pwe
     * extra_diags
     *
     * Revision 1.3  1997/10/10  18:26:02  pwe
     * prep ANDF-DE revision
     *
     * Revision 1.2  1997/03/24  11:15:46  pwe
     * dwarf2 option/default
     *
     * Revision 1.1  1997/03/20  16:24:39  pwe
     * dwarf2
     *
    *********************************************************************

--- tendra/src/installers/80x86/solaris/dw2_config.c
+++ tendra/src/installers/80x86/solaris/dw2_config.c
@@ -1,72 +1,64 @@
 /*
-    		 Crown Copyright (c) 1997
-
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
+ * Copyright (c) 2002, The Tendra Project <http://www.tendra.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *    		 Crown Copyright (c) 1997
+ *
+ *    This TenDRA(r) Computer Program is subject to Copyright
+ *    owned by the United Kingdom Secretary of State for Defence
+ *    acting through the Defence Evaluation and Research Agency
+ *    (DERA).  It is made available to Recipients with a
+ *    royalty-free licence for its use, reproduction, transfer
+ *    to other parties and amendment for any purpose not excluding
+ *    product development provided that any such use et cetera
+ *    shall be deemed to be acceptance of the following conditions:-
+ *
+ *        (1) Its Recipients shall ensure that this Notice is
+ *        reproduced upon any copies or amended versions of it;
+ *
+ *        (2) Any amended version of it shall be clearly marked to
+ *        show both the nature of and the organisation responsible
+ *        for the relevant amendment or amendments;
+ *
+ *        (3) Its onward transfer from a recipient to another
+ *        party shall be deemed to be that party's acceptance of
+ *        these conditions;
+ *
+ *        (4) DERA gives no warranty or assurance as to its
+ *        quality or suitability for any purpose and DERA accepts
+ *        no liability whatsoever in relation to any use to which
+ *        it may be put.
+ *
+ * $TenDRA$
 */
 
 
 /**********************************************************************
 $Author$
 $Date$
-$Revision$
-$Log$
-Revision 1.1  2002/01/26 21:31:13  asmodai
-Initial version of TenDRA 4.1.2.
-
- * Revision 1.4  1998/03/17  16:54:15  release
- * Couple of minor fixes.
- *
- * Revision 1.3  1998/03/11  11:03:13  pwe
- * DWARF optimisation info
- *
- * Revision 1.2  1998/01/21  10:30:02  pwe
- * labdiff change
- *
- * Revision 1.1.1.1  1998/01/17  15:55:52  release
- * First version to be checked into rolling release.
- *
- * Revision 1.6  1998/01/09  14:47:55  pwe
- * prep restructure
- *
- * Revision 1.5  1997/12/04  20:01:30  pwe
- * ANDF-DE V1.9
- *
- * Revision 1.4  1997/10/23  09:37:29  pwe
- * extra_diags
- *
- * Revision 1.3  1997/10/10  18:26:02  pwe
- * prep ANDF-DE revision
- *
- * Revision 1.2  1997/03/24  11:15:46  pwe
- * dwarf2 option/default
- *
- * Revision 1.1  1997/03/20  16:24:39  pwe
- * dwarf2
- *
-**********************************************************************/
+$Revision$*/
 
 #include "dw2_config.h"
 #include "flags.h"
