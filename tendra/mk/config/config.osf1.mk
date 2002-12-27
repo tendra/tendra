7355c5f6f0420aedd248b510b090e92a7f6e58b2
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2002-12-27 16:26:34 +0000

    Add osf1's config makefile.

---
+++ tendra/mk/config/config.osf1.mk
@@ -0,0 +1,15 @@
+# $TenDRA$
+
+PREFIX=		/usr/local
+TMP_DIR=	/tmp
+SRC_ENV=	${BUILD_OS}/common/${MACH_CPU}
+UNAME=		/usr/bin/uname
+
+MACH_CPU!=	${UNAME} -p
+MACH_OS!=	${UNAME}
+MACH_VERS!=	${UNAME} -r
+
+# Not sure of the best way to get the exec type, suggestions?
+MACH_EXEC=	elf
+
+.include "config.common.mk"
