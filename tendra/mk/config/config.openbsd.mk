dad33ba9a5b8922e81ebee154f95f2d9e91ee448
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2003-02-09 21:20:05 +0000

    Add first stab at a config file for OpenBSD.

---
+++ tendra/mk/config/config.openbsd.mk
@@ -0,0 +1,22 @@
+# $TenDRA$
+
+# Platform dependent commands' locations.
+UNAME=		/usr/bin/uname
+
+# SRC_ENV corresponds to the hierarchy in tendra/src/lib/env.
+PREFIX=		/usr/local
+TMP_DIR=	/tmp
+SRC_ENV=	${BUILD_OS}/common/${MACH_CPU}
+
+TMP_CPU!=	${UNAME} -m
+MACH_OS!=	${UNAME}
+MACH_VERS!=	${UNAME} -r
+
+# Not sure of the best way to get the exec type, suggestions?
+MACH_EXEC=	elf
+
+.if ${TMP_CPU} == "i386"
+MACH_CPU=	80x86
+.endif
+
+.include "config.common.mk"
