0fe56ef299ef9ad3fe76fa9d922c12b7eadc4426
Amos B. Haven <nonce@ten15.org>
2003-02-10 10:34:53 +0000

    Add start of openbsd support.  Additions include an a.out installer, and lib/env, lib/machines files.  This brings the build up to the first openbsd header that uses the #warning directive.  This can be solved by adding -f directives in the Makefile.inc files permit warning directives (with #pragma).  Once the design of this fix is settled, (see -dev list discussion) the remaining openbsd changes will follow.

---
+++ tendra/src/lib/machines/openbsd/80x86/include/sys/shm.h
@@ -0,0 +1,11 @@
+/*
+ * $TenDRA$
+ */
+
+#ifndef __HACKED_SYS_SHM_INCLUDED
+#define __HACKED_SYS_SHM_INCLUDED
+
+#include <machine/param.h>
+#include_next <sys/shm.h>
+
+#endif
---
+++ tendra/src/lib/machines/openbsd/80x86/startup/xpg4.h
@@ -0,0 +1,7 @@
+/*
+ * $TenDRA$
+ */
+
+#include "ansi.h"
+#define _XOPEN_SOURCE	1
+#define __WRONG_XPG4
