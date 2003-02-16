81b32eb2cc550ae3912ee3a654bc2086782f678c
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2003-02-16 18:37:02 +0000

    Add comments above the targets to denote what is what.

--- tendra/mk/base/tendra.install.mk
+++ tendra/mk/base/tendra.install.mk
@@ -14,19 +14,20 @@ INSTALL_SUB=		env
 .endif
 
 
-
+# Our main target.
 install-all: install-dir ${INSTALL_TARGETS}
 
 
 install-dir:
 	@echo mkdir -p ${INSTALL_PREFIX}/${INSTALL_SUB}
 
+# lib/env: our environment files.
 install-env:
 .for i in "${DATA_COMMON} ${DATA}"
 	@echo install ${.OBJDIR}/${i} ${INSTALL_PREFIX}/${INSTALL_SUB}
 .endfor
 
-
+# installers/*, tools/*, utilities/*: our binary files.
 install-bin:
 	@echo ${INSTALL} ${INSTALL_FLAGS} ${.OBJDIR}/${PROG} ${INSTALL_PREFIX}/${INSTALL_SUB}
 
