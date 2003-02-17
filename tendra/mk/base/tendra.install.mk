53edadca194908ff2cbe788472fc0c4ff5ed843b
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2003-02-17 00:17:19 +0000

    Add the installation capability.
    Submitted by:	verm (for the largest part)

--- tendra/mk/base/tendra.install.mk
+++ tendra/mk/base/tendra.install.mk
@@ -2,14 +2,18 @@
 
 INSTALL_PREFIX=		${INSTALL_DIR}/${TENDRA_VER}/${MACH_OS}/${MACH_VERS}/${MACH_CPU}
 
+# If you do not see an INSTALL_SUB below for a specific target changes are it
+# is set within the Makefile for that target.
 .if defined(PROG)
 INSTALL_TARGETS=	install-bin
 INSTALL_SUB=		bin
-.endif
-
-# this is broken.
-INSTALL_TARGETS=	
-.if (${INSTALL_TARGETS} == "install-env")
+.elif defined(INSTALL_STARTUP)
+INSTALL_TARGETS=	install-startup
+.elif defined(API)
+INSTALL_TARGETS=	install-api
+INSTALL_SUB=		lib/include
+.elif defined(INSTALL_ENV)
+INSTALL_TARGETS=	install-env
 INSTALL_SUB=		env
 .endif
 
@@ -17,18 +21,29 @@ INSTALL_SUB=		env
 # Our main target.
 install-all: install-dir ${INSTALL_TARGETS}
 
+# lib/startup: our startup files for compilation modes.
+install-startup:
+.for i in ${DATA}
+	${INSTALL} ${INSTALL_FLAGS} ${.OBJDIR}/${i} ${INSTALL_PREFIX}/${INSTALL_SUB}
+.endfor
 
-install-dir:
-	@echo mkdir -p ${INSTALL_PREFIX}/${INSTALL_SUB}
+# lib/apis: our api files.
+install-api:
+	@for i in ${.OBJDIR}.api/*.h; do \
+		${INSTALL} $$i ${INSTALL_PREFIX}/${INSTALL_SUB}/${API}.api; \
+	done
 
 # lib/env: our environment files.
 install-env:
-.for i in "${DATA_COMMON} ${DATA}"
-	@echo install ${.OBJDIR}/${i} ${INSTALL_PREFIX}/${INSTALL_SUB}
+.for i in ${DATA_COMMON} ${DATA}
+	@${INSTALL} ${.OBJDIR}/${i} ${INSTALL_PREFIX}/${INSTALL_SUB}
 .endfor
 
 # installers/*, tools/*, utilities/*: our binary files.
 install-bin:
-	@echo ${INSTALL} ${INSTALL_FLAGS} ${.OBJDIR}/${PROG} ${INSTALL_PREFIX}/${INSTALL_SUB}
+	@${INSTALL} ${INSTALL_FLAGS} ${.OBJDIR}/${PROG} ${INSTALL_PREFIX}/${INSTALL_SUB}
+
+install-dir:
+	@${MKDIR} ${INSTALL_PREFIX}/${INSTALL_SUB}
 
 .include "tendra.version.mk"
