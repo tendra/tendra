36314ed1113f73d72f3326424c4f1a591752dbdd
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2003-02-24 06:32:11 +0000

    Replace hardcoded OS value with ${BUILD_OS}.  [Which was wrong anyway, since OpenBSD was put there.]

--- tendra/mk/config/config.freebsd.mk
+++ tendra/mk/config/config.freebsd.mk
@@ -19,6 +19,6 @@ MACH_EXEC=	elf
 MACH_CPU=	80x86
 .endif
 
-TCC_OPTS+=	-f${SRC_DIR}/src/lib/machines/openbsd/${MACH_CPU}/include/gcc_dependency.h
+TCC_OPTS+=	-f${SRC_DIR}/src/lib/machines/${BUILD_OS}/${MACH_CPU}/include/gcc_dependency.h
 
 .include "config.common.mk"
