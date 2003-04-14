79facb2a91874d83a6ed58532c05bf8364bf1c10
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2003-04-14 15:01:25 +0000

    Add first try at HP-UX support. For now it is wired to HP PA-RISC (hppa) until the shell code in the comment section gets (re)written to work in the Makefile context.

---
+++ tendra/mk/config/config.hpux.mk
@@ -0,0 +1,42 @@
+# $TenDRA$
+
+# Platform dependent commands' locations.
+UNAME=		/usr/bin/uname
+
+# SRC_ENV corresponds to the hierarchy in tendra/src/lib/env.
+PREFIX?=	/usr/local
+TMP_DIR=	/tmp
+SRC_ENV=	${BUILD_OS}/common/${MACH_CPU}
+
+# GETCONF?=	/usr/bin/getconf
+#get_cpu_type()
+#{
+#        CPU_TYPE=`${GETCONF} CPU_VERSION`
+#
+#        if [ $CPU_TYPE -ge 768 ]; then ARCHITECTURE=ia64rev0
+#                # unistd.h: CPU_IA64_ARCHREV_0
+#        elif [ $CPU_TYPE -eq 532 ]; then ARCHITECTURE=hpparisc2x
+#                # unistd.h: CPU_PA_RISC2_0
+#        elif [ $CPU_TYPE -eq 529 ]; then ARCHITECTURE=hpparisc12
+#                # unistd.h: CPU_PA_RISC1_2
+#        elif [ $CPU_TYPE -eq 528 ]; then ARCHITECTURE=hpparisc11
+#                # unistd.h: CPU_PA_RISC1_1
+#        elif [ $CPU_TYPE -eq 526 ]; then ARCHITECTURE=mc68020
+#                # unistd.h: CPU_HP_MC68020
+#        elif [ $CPU_TYPE -eq 525 ]; then ARCHITECTURE=mc68030
+#                # unistd.h: CPU_HP_MC68030
+#        elif [ $CPU_TYPE -eq 524 ]; then ARCHITECTURE=mc68040
+#                # unistd.h: CPU_HP_MC68040
+#        elif [ $CPU_TYPE -eq 523 ]; then ARCHITECTURE=hpparisc10
+#                # unistd.h: CPU_PA_RISC1_0
+#        fi
+#}
+
+MACH_CPU!=	hppa # Be lame for now and hardwire it, until above works.
+MACH_OS!=	${UNAME}
+MACH_VERS!=	${UNAME} -r
+
+# Not sure of the best way to get the exec type, suggestions?
+MACH_EXEC=	elf
+
+.include "config.common.mk"
