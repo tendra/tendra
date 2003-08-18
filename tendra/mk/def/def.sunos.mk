ea59754dd416cc39ee7ba7fa8ceedb73787a361d
Amar Takhar <verm@darkbeer.org>
2003-08-18 04:44:13 +0000

    * Format for the new style. * Add BIN_TIME, BIN_INSTALL, they're blank and need to be added.

--- tendra/mk/def/def.sunos.mk
+++ tendra/mk/def/def.sunos.mk
@@ -7,65 +7,101 @@
 # gcrt is for profiling support (gprof).
 # mcrt is for profiling support (prof).
 
-CRT0?=		
-CRT1?=		
-CRTI?=		
-CRTN?=		
-GCRT0?=		
-GCRT1?=		
-MCRT0?=		
 
-AR?=		/usr/ccs/bin/ar
-AS?=		/usr/ccs/bin/as
-AWK?=		/usr/bin/awk
-BASENAME?=	/usr/bin/basename
-CAT?=		/bin/cat
-CHGRP?=		/usr/bin/chgrp
-CHMOD?=		/bin/chmod
-CHOWN?=		/usr/sbin/chown
-CP?=		/bin/cp
-CP_ARGS?=	
-CUT?=		/usr/bin/cut
-DC?=		/usr/bin/dc
-DIRNAME?=	/usr/bin/dirname
-ECHO?=		/bin/echo
-EGREP?=		/usr/bin/egrep
-FALSE?=		/usr/bin/false
-FILE?=		/usr/bin/file
-FIND?=		/usr/bin/find
-GREP?=		/usr/bin/grep
-GTAR?=		/usr/bin/tar
-GUNZIP?=	/usr/bin/gunzip
-GUNZIP_ARGS?=	-f
-GZCAT?=		NO-gzcat
-GZIP?=		NO-gzip
-GZIP_ARGS?=	-9
-HEAD?=		/usr/bin/head
-ID?=		/usr/bin/id
-LD?=		/usr/ccs/bin/ld
-LDCONFIG?=	NO-ldconfig
-LN?=		/bin/ln
-LS?=		/bin/ls
-MKDIR?=		/usr/bin/mkdir
-MKDIR_ARGS?=	-p
-MTREE?=		NO-mtree
-MV?=		/bin/mv
-PATCH?=		/usr/bin/patch
-PAX?=		/usr/bin/pax
-PERL?=		/usr/bin/perl
-RANLIB?=	/usr/ccs/bin/ranlib
-RM?=		/usr/bin/rm
-RMDIR?=		/usr/bin/rmdir
-SED?=		/usr/bin/sed
-SETENV?=	/usr/bin/env
-SH?=		/bin/sh
-SORT?=		/usr/bin/sort
-SU?=		/usr/bin/su
-TAIL?=		/usr/bin/tail
-TEST?=		/bin/test
-TOUCH?=		/usr/bin/touch
-TR?=		/usr/bin/tr
-TRUE?=		/usr/bin/true
-TYPE?=		type			# Shell builtin
-WC?=		/usr/bin/wc
-XARGS?=		/usr/bin/xargs
+# Libraries
+
+.if ${MACH_CPU} = "680x0"
+LIB_CRT0?=	/usr/lib/crt0.o
+LIB_CRT1?=	
+LIB_CRTI?=	
+LIB_CRTN?=	
+LIB_GCRT0?=	
+LIB_GCRT1?=	
+LIB_MCRT0?=	
+LIB_FCRT1?=	/usr/lib/Fcrt1.o
+.endif
+
+.if ${MACH_CPU} = "sparc"
+LIB_CRT0?=	
+LIB_CRT1?=	
+LIB_CRTI?=	
+LIB_CRTN?=	
+LIB_GCRT0?=	
+LIB_GCRT1?=	
+LIB_MCRT0?=	/usr/lib/mcrt0.o
+.endif
+
+
+# Arguments
+
+ARGS_GUNZIP?=	-f
+ARGS_GZIP?=	-9
+ARGS_MKDIR?=	-p
+
+
+# Binaries
+
+.if ${MACH_CPU} = "680x0"
+BIN_GAS         /usr/local/bin/gas
+BIN_LD          /bin/ld
+.endif
+
+.if ${MACH_CPU} = "sparc"
+BIN_AS          /bin/as
+BIN_LD          /bin/ld
+.endif
+
+
+
+BIN_AR?=	/usr/ccs/bin/ar
+#BIN_AS?=				# see above!
+BIN_AWK?=	/usr/bin/awk
+BIN_BASENAME?=	/usr/bin/basename
+BIN_CAT?=	/bin/cat
+BIN_CHGRP?=	/usr/bin/chgrp
+BIN_CHMOD?=	/bin/chmod
+BIN_CHOWN?=	/usr/sbin/chown
+BIN_CP?=	/bin/cp
+BIN_CUT?=	/usr/bin/cut
+BIN_DC?=	/usr/bin/dc
+BIN_DIRNAME?=	/usr/bin/dirname
+BIN_ECHO?=	/bin/echo
+BIN_EGREP?=	/usr/bin/egrep
+BIN_FALSE?=	usr/bin/false
+BIN_FILE?=	/usr/bin/file
+BIN_FIND?=	/usr/bin/find
+BIN_GREP?=	/usr/bin/grep
+BIN_GTAR?=	/usr/bin/tar
+BIN_GUNZIP?=	/usr/bin/gunzip
+BIN_GZCAT?=	NO-gzcat
+BIN_GZIP?=	NO-gzip
+BIN_HEAD?=	/usr/bin/head
+BIN_ID?=	/usr/bin/id
+BIN_INSTALL?=
+#BIN_LD?=				# see above!
+BIN_LDCONFIG?=	NO-ldconfig
+BIN_LN?=	/bin/ln
+BIN_LS?=	/bin/ls
+BIN_MKDIR?=	/usr/bin/mkdir
+BIN_MTREE?=	NO-mtree
+BIN_MV?=	/bin/mv
+BIN_PATCH?=	/usr/bin/patch
+BIN_PAX?=	/usr/bin/pax
+BIN_PERL?=	/usr/bin/perl
+BIN_RANLIB?=	/usr/ccs/bin/ranlib
+BIN_RM?=	/usr/bin/rm
+BIN_RMDIR?=	/usr/bin/rmdir
+BIN_SED?=	/usr/bin/sed
+BIN_SETENV?=	/usr/bin/env
+BIN_SH?=	/bin/sh
+BIN_SORT?=	/usr/bin/sort
+BIN_SU?=	/usr/bin/su
+BIN_TAIL?=	/usr/bin/tail
+BIN_TEST?=	/bin/test
+BIN_TIME?=
+BIN_TOUCH?=	/usr/bin/touch
+BIN_TR?=	/usr/bin/tr
+BIN_TRUE?=	/usr/bin/true
+BIN_TYPE?=	type			# Shell builtin
+BIN_WC?=	/usr/bin/wc
+BIN_XARGS?=	/usr/bin/xargs
