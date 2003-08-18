2b0a41a720ae15b313b9724859888cb39fc4e0a0
Amar Takhar <verm@darkbeer.org>
2003-08-18 03:45:34 +0000

    * Format for the new style. * Add BIN_TIME, BIN_INSTALL, they're blank and need to be added. * Add missing BIN_AS as well.

--- tendra/mk/def/def.netbsd.mk
+++ tendra/mk/def/def.netbsd.mk
@@ -7,63 +7,76 @@
 # gcrt is for profiling support (gprof).
 # mcrt is for profiling support (prof).
 
-CRT0?=		/usr/lib/crt0.o
-CRT1?=
-CRTI?=		/usr/lib/crti.o
-CRTN?=		/usr/lib/crtn.o
-GCRT0?=		/usr/lib/gcrt0.o
-GCRT1?=		
-MCRT0?=		
 
-AR?=		/usr/bin/ar
-AWK?=		/usr/bin/awk
-BASENAME?=	/usr/bin/basename
-CAT?=		/bin/cat
-CHGRP?=		/usr/bin/chgrp
-CHMOD?=		/bin/chmod
-CHOWN?=		/usr/sbin/chown
-CP?=		/bin/cp
-CP_ARGS?=
-CUT?=		/usr/bin/cut
-DC?=		/usr/bin/dc
-DIRNAME?=	/usr/bin/dirname
-ECHO?=		/bin/echo
-EGREP?=		/usr/bin/egrep
-FALSE?=		/usr/bin/false
-FILE?=		/usr/bin/file
-FIND?=		/usr/bin/find
-GREP?=		/usr/bin/grep
-GTAR?=		/usr/bin/tar
-GUNZIP?=	/usr/bin/gunzip
-GUNZIP_ARGS?=	-f
-GZCAT?=		/usr/bin/gzcat
-GZIP?=		/usr/bin/gzip
-GZIP_ARGS?=	-nf -9
-HEAD?=		/usr/bin/head
-ID?=		/usr/bin/id
-LDCONFIG?=	/sbin/ldconfig
-LN?=		/bin/ln
-LS?=		/bin/ls
-MKDIR?=		/bin/mkdir
-MKDIR_ARGS?=	-p
-MTREE?=		/usr/sbin/mtree
-MV?=		/bin/mv
-PATCH?=		/usr/bin/patch
-PAX?=		/bin/pax
-PERL?=		/usr/pkg/bin/perl
-RANLIB?=	/usr/bin/ranlib
-RM?=		/bin/rm
-RMDIR?=		/bin/rmdir
-SED?=		/usr/bin/sed
-SETENV?=	/usr/bin/env
-SH?=		/bin/sh
-SORT?=		/usr/bin/sort
-SU?=		/usr/bin/su
-TAIL?=		/usr/bin/tail
-TEST?=		/bin/test
-TOUCH?=		/usr/bin/touch
-TR?=		/usr/bin/tr
-TRUE?=		/usr/bin/true
-TYPE?=		type				# Shell builtin
-WC?=		/usr/bin/wc
-XARGS?=		/usr/bin/xargs
+# Libraries
+
+LIB_CRT0?=	/usr/lib/crt0.o
+LIB_CRT1?=	
+LIB_CRTI?=	/usr/lib/crti.o
+LIB_CRTN?=	/usr/lib/crtn.o
+LIB_GCRT0?=	/usr/lib/gcrt0.o
+LIB_GCRT1?=	
+LIB_MCRT0?=	
+
+
+# Arguments
+
+ARGS_CP?=	
+ARGS_GUNZIP?=	-f
+ARGS_GZIP?=	-nf -9
+ARGS_MKDIR?=	-p
+
+
+# Binaries
+
+BIN_AR?=	/usr/bin/ar
+BIN_AS?=	/bin/as
+BIN_AWK?=	/usr/bin/awk
+BIN_BASENAME?=	/usr/bin/basename
+BIN_CAT?=	/bin/cat
+BIN_CHGRP?=	/usr/bin/chgrp
+BIN_CHMOD?=	/bin/chmod
+BIN_CHOWN?=	/usr/sbin/chown
+BIN_CP?=	/bin/cp
+BIN_CUT?=	/usr/bin/cut
+BIN_DC?=	/usr/bin/dc
+BIN_DIRNAME?=	/usr/bin/dirname
+BIN_ECHO?=	/bin/echo
+BIN_EGREP?=	/usr/bin/egrep
+BIN_FALSE?=	/usr/bin/false
+BIN_FILE?=	/usr/bin/file
+BIN_FIND?=	/usr/bin/find
+BIN_GREP?=	/usr/bin/grep
+BIN_GTAR?=	/usr/bin/tar
+BIN_GUNZIP?=	/usr/bin/gunzip
+BIN_GZCAT?=	/usr/bin/gzcat
+BIN_GZIP?=	/usr/bin/gzip
+BIN_HEAD?=	/usr/bin/head
+BIN_ID?=	/usr/bin/id
+BIN_INSTALL?=
+BIN_LDCONFIG?=	/sbin/ldconfig
+BIN_LN?=	/bin/ln
+BIN_LS?=	/bin/ls
+BIN_MKDIR?=	/bin/mkdir
+BIN_MTREE?=	/usr/sbin/mtree
+BIN_MV?=	/bin/mv
+BIN_PATCH?=	/usr/bin/patch
+BIN_PAX?=	/bin/pax
+BIN_PERL?=	/usr/pkg/bin/perl
+BIN_RANLIB?=	/usr/bin/ranlib
+BIN_RM?=	/bin/rm
+BIN_RMDIR?=	/bin/rmdir
+BIN_SED?=	/usr/bin/sed
+BIN_SETENV?=	/usr/bin/env
+BIN_SH?=	/bin/sh
+BIN_SORT?=	/usr/bin/sort
+BIN_SU?=	/usr/bin/su
+BIN_TAIL?=	/usr/bin/tail
+BIN_TEST?=	/bin/test
+BIN_TIME?=
+BIN_TOUCH?=	/usr/bin/touch
+BIN_TR?=	/usr/bin/tr
+BIN_TRUE?=	/usr/bin/true
+BIN_TYPE?=	type			# shell builtin
+BIN_WC?=	/usr/bin/wc
+BIN_XARGS?=	/usr/bin/xargs
