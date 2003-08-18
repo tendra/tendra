0a7de9861ae3593e6c9a9568f4f9354fc3c74547
Amar Takhar <verm@darkbeer.org>
2003-08-18 03:25:15 +0000

    * Format for the new style. * Add BIN_TIME, BIN_INSTALL, they're blank and need to be added.
    I had to do some .ifdef gunk to handle elf/aout differences.
    This may be changed later when the design is thought through better.

--- tendra/mk/def/def.linux.mk
+++ tendra/mk/def/def.linux.mk
@@ -7,60 +7,101 @@
 # gcrt is for profiling support (gprof).
 # mcrt is for profiling support (prof).
 
-CRT0?=          
-CRT1?=		/usr/lib/crt1.o
-CRTI?=		/usr/lib/crti.o
-CRTN?=		/usr/lib/crtn.o
-GCRT0?=         
-GCRT1?=		/usr/lib/gcrt1.o
-MCRT0?=         /usr/lib/Mcrt1.o
 
-AR?=		/usr/bin/ar
-AWK?=		/bin/awk
+# Libraries
+
+.if ${MACH_EXEC} = "elf"
+LIB_CRT0?=		/usr/lib/crt0.o
+LIB_CRT1?=		/usr/lib/crt1.o
+LIB_CRTI?=		/usr/lib/crti.o
+LIB_CRTBEGIN?=		/usr/lib/crtbegin.o
+LIB_CRTEND?=		/usr/lib/crtend.o
+LIB_CRTN?=		/usr/lib/crtn.o
+LIB_LD_LINUX_1?=	/lib/ld-linux.so.1
+LIB_GCRT1?=		/usr/lib/gcrt1.o
+MCRT0?=?=		/usr/lib/Mcrt1.o
+.endif
+
+.if ${MACH_EXEC} = "aout"
+LIB_CRT0?=	/usr/i486-linuxaout/lib/crt0.o
+LIB_CRT1?=	/usr/lib/crt1.o
+LIB_CRTI?=	/usr/lib/crti.o
+LIB_CRTN?=	/usr/lib/crtn.o
+LIB_GCRT0?=	/usr/lib/gcrt0.o
+LIB_GCRT1?=	/usr/lib/gcrt1.o
+LIB_MCRT0?=	/usr/lib/Mcrt1.o
+.endif
+
+
+# Arguments
+
+ARGS_CP?=	-v
+ARGS_GUNZIP?=	-f
+ARGS_GZIP?=	-nf -9
+ARGS_MKDIR?=	-p
+
+
+# Binaries
+
+.if ${MACH_EXEC} = "elf"
+BIN_LD?=	/usr/bin/ld
+BIN_AS?=	/usr/bin/as
+.endif
+
+.if ${MACH_EXEC} = "aout"
+BIN_AS?=	/usr/i486-linuxaout/bin/as
+BIN_LD?=	/usr/i486-linuxaout/bin/ld
+.endif
+
+BIN_AR?=	/usr/bin/ar
+BIN_AWK?=	/bin/awk
+#BIN_AS?=			# see above!
 BASENAME?=	/bin/basename
-CAT?=		/bin/cat
-CHGRP?=		/bin/chgrp
-CHMOD?=		/bin/chmod
-CHOWN?=		/bin/chown
-CP?=		/bin/cp
-CP_ARGS?=	-v
-CUT?=		/usr/bin/cut
-DC?=		/usr/bin/dc
-DIRNAME?=	/usr/bin/dirname
-ECHO?=		/bin/echo
-EGREP?=		/bin/egrep
-FALSE?=		/bin/false
-FILE?=		/usr/bin/file
-FIND?=		/usr/bin/find
-GREP?=		/bin/grep
-GTAR?=		/bin/tar
-GUNZIP?=	/bin/gunzip
-GZCAT?=		/usr/pubsw/bin/gzcat
-GZIP?=		/bin/gzip
-HEAD?=		/usr/bin/head
-ID?=		/usr/bin/id
-LDCONFIG?=	
-LN?=		/bin/ln
-LS?=		/bin/ls
-MKDIR?=		/bin/mkdir
-MTREE?=		
-MV?=		/bin/mv
-PATCH?=		/usr/bin/patch
-PAX?=		
-PERL?=		/usr/bin/perl
-RANLIB?=	/usr/bin/ranlib
-RM?=		/bin/rm
-RMDIR?=		/bin/rmdir
-SED?=		/bin/sed
-SETENV?=	/usr/bin/env
-SH?=		/bin/sh
-SORT?=		/bin/sort
-SU?=		/bin/su
-TAIL?=		/usr/bin/tail
-TEST?=		/usr/bin/test
-TOUCH?=		/bin/touch
-TR?=		/usr/bin/tr
-TRUE?=		/bin/true
-TYPE?=		
-WC?=		/usr/bin/wc
-XARGS?=		/usr/bin/xargs
+BIN_CAT?=	/bin/cat
+BIN_CHGRP?=	/bin/chgrp
+BIN_CHMOD?=	/bin/chmod
+BIN_CHOWN?=	/bin/chown
+BIN_CP?=	/bin/cp
+BIN_CUT?=	/usr/bin/cut
+BIN_DC?=	/usr/bin/dc
+BIN_DIRNAME?=	/usr/bin/dirname
+BIN_ECHO?=	/bin/echo
+BIN_EGREP?=	/bin/egrep
+BIN_FALSE?=	/bin/false
+BIN_FILE?=	/usr/bin/file
+BIN_FIND?=	/usr/bin/find
+BIN_GREP?=	/bin/grep
+BIN_GTAR?=	/bin/tar
+BIN_GUNZIP?=	/bin/gunzip
+BIN_GZCAT?=	/usr/pubsw/bin/gzcat
+BIN_GZIP?=	/bin/gzip
+BIN_HEAD?=	/usr/bin/head
+BIN_ID?=	/usr/bin/id
+BIN_INSTALL?=
+BIN_LDCONFIG?=	
+#BIN_LD				# see above!
+BIN_LN?=	/bin/ln
+BIN_LS?=	/bin/ls
+BIN_MKDIR?=	/bin/mkdir
+BIN_MTREE?=	
+BIN_MV?=	/bin/mv
+BIN_PATCH?=	/usr/bin/patch
+BIN_PAX?=	
+BIN_PERL?=	/usr/bin/perl
+BIN_RANLIB?=	/usr/bin/ranlib
+BIN_RM?=	/bin/rm
+BIN_RMDIR?=	/bin/rmdir
+BIN_SED?=	/bin/sed
+BIN_SETENV?=	/usr/bin/env
+BIN_SH?=	/bin/sh
+BIN_SORT?=	/bin/sort
+BIN_SU?=	/bin/su
+BIN_TAIL?=	/usr/bin/tail
+BIN_TEST?=	/usr/bin/test
+BIN_TIME?=
+BIN_TOUCH?=	/bin/touch
+BIN_TR?=	/usr/bin/tr
+BIN_TRUE?=	/bin/true
+BIN_TYPE?=	type		# shell builtin.
+BIN_WC?=	/usr/bin/wc
+BIN_XARGS?=	/usr/bin/xargs
