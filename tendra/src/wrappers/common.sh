8c58366db81b97e094ca7a0d481530217532ba6a
Amar Takhar <verm@darkbeer.org>
2003-08-25 05:32:46 +0000

    * Relocate the manpages to src/man, this makes it far easier to maintain. * Remove src/tools/tcc/tcc.sh, this now resides in wrappers/*. * Add wrappers/*, which wrap all of our 'user' binaries. * Plug man and wrappers into the build.

---
+++ tendra/src/wrappers/common.sh
@@ -0,0 +1,25 @@
+#!-BIN_SH-
+#
+# -BIN_RUN- wrapper script
+#
+# $TenDRA$
+#
+
+# TenDRA settings
+TENDRA_BASEDIR="-TENDRA_BASEDIR-"
+TENDRA_VERSION="-TENDRA_VERSION-"
+
+
+# binary settings
+BIN_RUN="${TENDRA_BASEDIR}/bin/-BIN_RUN-"
+OPTS_DEFAULT=""
+
+
+
+# Execute -BIN_RUN- with our compiled options.
+
+if [ -n "$DEBUG" ]; then
+	${BIN_RUN} ${OPTS_DEFAULT} ${@+"$@"}
+else
+	-BIN_ECHO- ${BIN_RUN} ${OPTS_DEFAULT} ${@+"$@"}
+fi;
---
+++ tendra/src/wrappers/tcc.sh
@@ -0,0 +1,25 @@
+#!-BIN_SH-
+#
+# tcc wrapper script
+#
+# $TenDRA$
+#
+
+# TenDRA settings
+TENDRA_BASEDIR="-TENDRA_BASEDIR-"
+TENDRA_VERSION="-TENDRA_VERSION-"
+
+
+# tcc settings
+TCC_BIN="${TENDRA_BASEDIR}/bin/tcc"
+OPTS_DEFAULT="-yTENDRA_BASEDIR=${TENDRA_BASEDIR} -Y${TENDRA_BASEDIR}/env/default -Ysystem"
+
+
+
+# Execute tcc with our compiled options.
+
+if [ -n "$DEBUG "]; then
+	${TCC_BIN} ${OPTS_DEFAULT} ${@+"$@"}
+else
+	-BIN_ECHO- ${TCC_BIN} ${OPTS_DEFAULT} ${@+"$@"}
+fi;
