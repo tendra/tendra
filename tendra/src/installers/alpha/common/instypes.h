70d8a06df22df79018766900b220cc5add2f27bb
Stefan Farfeleder <stefanf@freebsd.org>
2003-12-14 01:46:41 +0000

    Parenthesise macro parameters.

--- tendra/src/installers/alpha/common/instypes.h
+++ tendra/src/installers/alpha/common/instypes.h
@@ -80,12 +80,12 @@ typedef struct{
 
 
 /*#define ins_equal(insa,insb) (!strcmp(insa.ins,insb.ins))*/
-#define ins_equal(insa,insb) (insa.val == insb.val)
-#define ins_symbolic_name(inst) (inst.ins)
-#define ins_binid(ins) (ins.val)
+#define ins_equal(insa,insb) ((insa).val == (insb).val)
+#define ins_symbolic_name(inst) ((inst).ins)
+#define ins_binid(ins) ((ins).val)
 
 #if DO_SCHEDULE
-#define ins_class(ins)	(ins.class)
+#define ins_class(ins)	((ins).class)
 #endif
 
 #endif /* INST_TYPES */
