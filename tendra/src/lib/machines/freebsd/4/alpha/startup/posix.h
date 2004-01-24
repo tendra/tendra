c323131311715551aff1bbe46d63622b8f72c5a9
Stefan Farfeleder <stefanf@freebsd.org>
2004-01-24 13:36:18 +0000

    Add a workaround for <limits.h> on FreeBSD -current which unconditionally defines _POSIX_CHILD_MAX, _POSIX_NGROUPS_MAX, _POSIX_OPEN_MAX and _POSIX_PATH_MAX in accordance with SUSv3, thus causing conflicts with older POSIX APIs.  The workaround is to include <limits.h> and to immediately #undef the offensive macros.

--- tendra/src/lib/machines/freebsd/4/alpha/startup/posix.h
+++ tendra/src/lib/machines/freebsd/4/alpha/startup/posix.h
@@ -5,3 +5,8 @@
 #include "ansi.h"
 #define _POSIX_SOURCE	1
 #define __WRONG_POSIX_TERMIOS_H
+#include <limits.h>
+#undef _POSIX_CHILD_MAX
+#undef _POSIX_NGROUPS_MAX
+#undef _POSIX_OPEN_MAX
+#undef _POSIX_PATH_MAX
--- tendra/src/lib/machines/freebsd/5/alpha/startup/posix1.h
+++ tendra/src/lib/machines/freebsd/5/alpha/startup/posix1.h
@@ -5,3 +5,8 @@
 #include "ansi.h"
 #define _POSIX_SOURCE	1
 #define __WRONG_POSIX1_LOCALE_H
+#include <limits.h>
+#undef _POSIX_CHILD_MAX
+#undef _POSIX_NGROUPS_MAX
+#undef _POSIX_OPEN_MAX
+#undef _POSIX_PATH_MAX
