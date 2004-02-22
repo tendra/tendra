1bdf9766fc4160abb7102f378ee73551a06fb41c
Amar Takhar <verm@darkbeer.org>
2004-02-22 04:34:29 +0000

    * Remove binary listings, these should be done on a type-by-type basis. * Remove setting of PREFIX -- should be in doc.docbook.mk
    These changes better set us up in the future for supporting XSL style sheets.
    Which is the absolute last thing on my todo list.

--- doc/share/mk/doc.project.mk
+++ doc/share/mk/doc.project.mk
@@ -1,34 +1,21 @@
 # $TenDRA$
 
-DOCFORMAT?=	docbook
 MAINTAINER?=	docs@ten15.org
 
 # Master list of known target formats.  The doc.<format>.mk files implement 
 # the code to convert from their source format to one or more of these target
 # formats
+# doc.docbook.ml
 ALL_FORMATS=	html html.tar html-split html-split.tar txt rtf ps pdf tex dvi tar pdb
-
-# User-modifiable
-#LOCALBASE?=	/usr/local
-PREFIX?=	/usr/local
-#PRI_LANG?=	en
-
-CP?=		/bin/cp
-CAT?=		/bin/cat
-ECHO_CMD?=	echo
-LN?=		/bin/ln
-MKDIR?=		/bin/mkdir -p
-RM?=		/bin/rm
-MV?=		/bin/mv
 
 
 .MAIN: all
 
 all: _SUBDIRUSE
 
+
 # Image processing (contains code used by the doc.<format>.mk files, so must
 # be listed first).
-
 
 .if defined(DOC)
 .include "doc.images.mk"
@@ -37,12 +24,11 @@ all: _SUBDIRUSE
 .endif
 
 
-
-
 clean: _SUBDIRUSE
 .if defined(CLEANFILES) && !empty(CLEANFILES)
 	${RM} -f ${CLEANFILES}
 .endif
+
 
 cleanall:
 	${MAKE} FORMATS="${ALL_FORMATS}" clean
