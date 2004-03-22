f069ff5b3efd1f4ace24c4754a6812ca563fbdc8
Amar Takhar <verm@darkbeer.org>
2004-03-22 06:48:51 +0000

    Complete re-write: * Dependancies are now properly tracked, so double invokations of make won't   regen the source if it's not needed. * All temporary files have been moved to ${.CURDIR}/tmp, there are a lot of   them, this way they won't get in the way. * The generated manpages match the names in CVS. * instead of a header file, spit the name of the directory where the manpage is   located back to gen.sh, which generates the entire file, vs just the header.   It's much easier to do this in SH rather than with some convoluted, ungly make   glue.
    TODO:
    * Prepare the generated manpages for cvs checkin.  I want to store the SGML
      $TenDRA$ as a comment within the generated mdoc to be put into the TenDRA
      source repo, so we can keep track of updates easily.

---
+++ doc/en/mdoc/gen.sh
@@ -0,0 +1,61 @@
+# $TenDRA$
+
+
+# Generate our header.
+
+echo "<!DOCTYPE refentry PUBLIC \"-//FreeBSD//DTD DocBook V4.1-Based Extension//EN\"["
+echo "<!ENTITY % man PUBLIC \"-//FreeBSD//ENTITIES DocBook Manual Page Entities//EN\">"
+echo "%man;"
+
+
+case $1 in
+	*80x86*)
+		echo "<!ENTITY % ref.man.80x86-common SYSTEM \"tmp/80x86-common.sgml_tmp\"> %ref.man.80x86-common;"
+		echo "<!ENTITY trans \"trans386\">"
+	;;
+	*alpha*)
+		echo "<!ENTITY % ref.man.alpha-common SYSTEM \"tmp/alpha-common.sgml_tmp\"> %ref.man.alpha-common;"
+		echo "<!ENTITY trans \"alphatrans\">"
+	;;
+	*hppa*)
+		echo "<!ENTITY % ref.man.hppa-common SYSTEM \"tmp/hppa-common.sgml_tmp\"> %ref.man.hppa-common;"
+		echo "<!ENTITY trans \"hppatrans\">"
+	;;
+	*mips*)
+		echo "<!ENTITY % ref.man.mips-common SYSTEM \"tmp/mips-common.sgml_tmp\"> %ref.man.mips-common;"
+		echo "<!ENTITY trans \"mipstrans\">"
+	;;
+	*power*)
+		echo "<!ENTITY % ref.man.power-common SYSTEM \"tmp/power-common.sgml_tmp\"> %ref.man.power-common;"
+		echo "<!ENTITY trans \"powertrans\">"
+	;;
+	*680x0*)
+		echo "<!ENTITY % ref.man.power-common SYSTEM \"tmp/power-common.sgml_tmp\"> %ref.man.power-common;"
+		echo "<!ENTITY trans \"powertrans\">"
+	;;
+	*ppc601*)
+		echo "<!ENTITY % ref.man.ppc601-common SYSTEM \"tmp/ppc601-common.sgml_tmp\"> %ref.man.ppc601-common;"
+		echo "<!ENTITY trans \"powertrans\">"
+	;;
+	*sparc*)
+		echo "<!ENTITY % ref.man.sparc-common SYSTEM \"tmp/sparc-common.sgml_tmp\"> %ref.man.sparc-common;"
+		echo "<!ENTITY trans \"sparctrans\">"
+	;;
+esac
+
+
+
+if [ -f "../manpages/$1/manpage.sgml" ]; then
+	echo "]>"
+	echo "<refentry id=\"$1\">"
+	cat ../manpages/$1/manpage.sgml
+elif [ -f "../manpages/trans/$1/manpage.sgml" ]; then
+	echo "<!ENTITY % ref.man.options SYSTEM \"tmp/options.sgml_tmp\"> %ref.man.options;"
+	echo "<!ENTITY % ref.man.common SYSTEM \"tmp/common.sgml_tmp\"> %ref.man.common;"
+	echo "]>"
+	echo "<refentry id=\"$1\">"
+	cat ../manpages/trans/$1/manpage.sgml
+fi
+
+
+echo "</refentry>"
