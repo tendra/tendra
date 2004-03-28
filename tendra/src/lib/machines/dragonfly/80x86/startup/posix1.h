9f8d1d978243ff012dd13f330cfefef1b96ab153
Stefan Farfeleder <stefanf@freebsd.org>
2004-03-28 01:28:04 +0000

    The workaround for <limits.h> on FreeBSD 5.x isn't needed here.

--- tendra/src/lib/machines/dragonfly/80x86/startup/posix1.h
+++ tendra/src/lib/machines/dragonfly/80x86/startup/posix1.h
@@ -1,12 +1,7 @@
 /*
- * $TenDRA: tendra/src/lib/machines/freebsd/4/80x86/startup/posix1.h,v 1.2 2004/01/24 13:36:15 stefanf Exp $
+ * $TenDRA$
  */
 
 #include "ansi.h"
 #define _POSIX_SOURCE	1
 #define __WRONG_POSIX1_LOCALE_H
-#include <limits.h>
-#undef _POSIX_CHILD_MAX
-#undef _POSIX_NGROUPS_MAX
-#undef _POSIX_OPEN_MAX
-#undef _POSIX_PATH_MAX
