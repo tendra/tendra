351720dbc745cb5c32df8ed74f2b22ba683be0bc
Stefan Farfeleder <stefanf@freebsd.org>
2004-05-28 17:18:51 +0000

    Merge changes from FreeBSD to work around the same inconsistent definitions of _POSIX_* macros.
    Requested by:	Antoine Brodin <antoine.brodin@laposte.net>

--- tendra/src/lib/machines/linux/80x86/startup/posix.h
+++ tendra/src/lib/machines/linux/80x86/startup/posix.h
@@ -1,3 +1,11 @@
+/*
+ * $TenDRA$
+ */
 #include "ansi.h"
 #define _POSIX_SOURCE		1
 #define _POSIX_C_SOURCE		1
+#include <limits.h>
+#undef _POSIX_CHILD_MAX
+#undef _POSIX_NGROUPS_MAX
+#undef _POSIX_OPEN_MAX
+#undef _POSIX_PATH_MAX
--- tendra/src/lib/machines/linux/80x86/startup/posix1.h
+++ tendra/src/lib/machines/linux/80x86/startup/posix1.h
@@ -1,2 +1,10 @@
+/*
+ * $TenDRA$
+ */
 #include "ansi.h"
 #define _POSIX_SOURCE	1
+#include <limits.h>
+#undef _POSIX_CHILD_MAX
+#undef _POSIX_NGROUPS_MAX
+#undef _POSIX_OPEN_MAX
+#undef _POSIX_PATH_MAX
