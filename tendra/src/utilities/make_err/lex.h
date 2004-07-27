878b36ce03e88a92bcd826f71b320e865d8f5a73
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2004-07-27 12:55:49 +0000

    Fix some stray trailing whitespaces, which ironically came from the cleanup script's insertion of the copyright/license.
    svn path=/tendra/trunk/; revision=283

--- tendra/src/utilities/make_err/lex.h
+++ tendra/src/utilities/make_err/lex.h
@@ -4,10 +4,10 @@
  *
  * This code is derived from software contributed to The TenDRA Project by
  * Jeroen Ruigrok van der Werven.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
@@ -16,7 +16,7 @@
  * 3. Neither the name of The TenDRA Project nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific, prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
  * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
