680a192d2eeaa48c7e6d46e6f3d453d4b0e016fc
Boris Popov <bp@freebsd.org>
2004-09-01 14:15:28 +0000

    Fix more identation bugs in the output files. Use tab character instead of spaces.

--- tendra/src/utilities/calculus/disk.c
+++ tendra/src/utilities/calculus/disk.c
@@ -113,60 +113,60 @@ disk_read_struct(void)
 static void
 disk_read_union(void)
 {
-    output ("    x_ = NULL_%UM;\n");
-    output ("    if (READ_BITS (1) == 1) {\n");
-    LOOP_UNION_COMPONENT output ("\t%CT %CN;\n");
-    output ("\tunsigned tag_ = READ_BITS (%UO2);\n");
-    output ("\tswitch (tag_) {\n");
+    output ("\tx_ = NULL_%UM;\n");
+    output ("\tif (READ_BITS (1) == 1) {\n");
+    LOOP_UNION_COMPONENT output ("\t\t%CT %CN;\n");
+    output ("\t\tunsigned tag_ = READ_BITS (%UO2);\n");
+    output ("\t\tswitch (tag_) {\n");
     LOOP_UNION_FIELD {
 		int al = DEREF_int (fld_flag (CRT_FIELD));
-		output ("\t    case %UM_%FN_tag : {\n");
-		LOOP_FIELD_COMPONENT output ("\t\t%CT %CN;\n");
+		output ("\t\tcase %UM_%FN_tag : {\n");
+		LOOP_FIELD_COMPONENT output ("\t\t\t%CT %CN;\n");
 
 		/* Deal with aliasing */
 		if (al) {
-			output ("\t\tunsigned alias_ = READ_ALIAS ();\n");
-			output ("\t\tif (READ_BITS (1) == 0) {\n");
-			output ("\t\t    x_ = FIND_ALIAS_%UM_%FN (alias_);\n");
-			output ("\t\t    break;\n");
-			output ("\t\t}\n");
+			output ("\t\t\tunsigned alias_ = READ_ALIAS ();\n");
+			output ("\t\t\tif (READ_BITS (1) == 0) {\n");
+			output ("\t\t\t\tx_ = FIND_ALIAS_%UM_%FN (alias_);\n");
+			output ("\t\t\t\tbreak;\n");
+			output ("\t\t\t}\n");
 			if (al == 2) {
-				output ("\t\tUNALIAS_%UM_%FN (x_);\n");
-				output ("\t\tSET_ALIAS_%UM_%FN (x_, alias_);\n");
+				output ("\t\t\tUNALIAS_%UM_%FN (x_);\n");
+				output ("\t\t\tSET_ALIAS_%UM_%FN (x_, alias_);\n");
 			} else {
-				output ("\t\tNEW_ALIAS_%UM_%FN (x_, alias_);\n");
+				output ("\t\t\tNEW_ALIAS_%UM_%FN (x_, alias_);\n");
 			}
 		}
 
 		/* Read the components */
 		LOOP_UNION_COMPONENT {
 			TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-			output ("\t\t%CN = READ_%TI ();\n", t);
+			output ("\t\t\t%CN = READ_%TI ();\n", t);
 		}
 		LOOP_FIELD_COMPONENT {
 			TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-			output ("\t\t%CN = READ_%TI ();\n", t);
+			output ("\t\t\t%CN = READ_%TI ();\n", t);
 		}
 
 		/* Assign components into x_ */
 		if (al == 2) {
-			output ("\t\tUNIFY_%UM_%FN (");
+			output ("\t\t\tUNIFY_%UM_%FN (");
 			LOOP_UNION_COMPONENT output ("%CN, ");
 			LOOP_FIELD_COMPONENT output ("%CN, ");
 			output ("x_);\n");
-			output ("\t\tSET_ALIAS_%UM_%FN (x_, alias_);\n");
+			output ("\t\t\tSET_ALIAS_%UM_%FN (x_, alias_);\n");
 		} else if (al) {
 			LOOP_UNION_COMPONENT {
 				TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-				output ("\t\tCOPY_%TM (%UM_%CN (x_), %CN);\n", t);
+				output ("\t\t\tCOPY_%TM (%UM_%CN (x_), %CN);\n", t);
 			}
 			LOOP_FIELD_COMPONENT {
 				TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-				output ("\t\tCOPY_%TM (%UM_%FN_%CN (x_), %CN);\n", t);
+				output ("\t\t\tCOPY_%TM (%UM_%FN_%CN (x_), %CN);\n", t);
 			}
 		} else {
 			int def = 0;
-			output ("\t\tMAKE_%UM_%FN (");
+			output ("\t\t\tMAKE_%UM_%FN (");
 			LOOP_UNION_COMPONENT {
 				string v = DEREF_string (cmp_value (CRT_COMPONENT));
 				if (v == NULL) {
@@ -190,7 +190,7 @@ disk_read_union(void)
 					string v = DEREF_string (cmp_value (CRT_COMPONENT));
 					if (v) {
 						TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-						output ("\t\tCOPY_%TM ", t);
+						output ("\t\t\tCOPY_%TM ", t);
 						output ("(%UM_%CN (x_), %CN);\n");
 					}
 				}
@@ -198,17 +198,17 @@ disk_read_union(void)
 					string v = DEREF_string (cmp_value (CRT_COMPONENT));
 					if (v) {
 						TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-						output ("\t\tCOPY_%TM ", t);
+						output ("\t\t\tCOPY_%TM ", t);
 						output ("(%UM_%FN_%CN (x_), %CN);\n");
 					}
 				}
 			}
 		}
-		output ("\t\tbreak;\n");
-		output ("\t    }\n");
+		output ("\t\t\tbreak;\n");
+		output ("\t\t}\n");
     }
+    output ("\t\t}\n");
     output ("\t}\n");
-    output ("    }\n");
     return;
 }
 
@@ -295,73 +295,73 @@ disk_read_def(char *dir)
 
 			case type_ptr_tag : {
 				TYPE_P s = DEREF_ptr (type_ptr_sub (t0));
-				output ("    if (READ_BITS (1) == 0) {\n");
-				output ("\tx_ = NULL_ptr (%TT);\n", s);
-				output ("    } else {\n");
-				output ("\tx_ = MAKE_ptr (%TS);\n", s);
-				output ("\tCOPY_%TM (x_, READ_%TI ());\n", s, s);
-				output ("    }\n");
+				output ("\tif (READ_BITS (1) == 0) {\n");
+				output ("\t\tx_ = NULL_ptr (%TT);\n", s);
+				output ("\t} else {\n");
+				output ("\t\tx_ = MAKE_ptr (%TS);\n", s);
+				output ("\t\tCOPY_%TM (x_, READ_%TI ());\n", s, s);
+				output ("\t}\n");
 				break;
 			}
 
 			case type_list_tag : {
 				TYPE_P s = DEREF_ptr (type_list_sub (t0));
-				output ("    x_ = NULL_list (%TT);\n", s);
-				output ("    while (READ_BITS (1)) {\n");
-				output ("\t%TT y_;\n", s);
-				output ("\t%TT z_;\n", t);
-				output ("\ty_ = READ_%TI ();\n", s);
-				output ("\tCONS_%TM (y_, NULL_list (%TT), z_);\n",
+				output ("\tx_ = NULL_list (%TT);\n", s);
+				output ("\twhile (READ_BITS (1)) {\n");
+				output ("\t\t%TT y_;\n", s);
+				output ("\t\t%TT z_;\n", t);
+				output ("\t\ty_ = READ_%TI ();\n", s);
+				output ("\t\tCONS_%TM (y_, NULL_list (%TT), z_);\n",
 						s, s);
-				output ("\tx_ = APPEND_list (x_, z_);\n");
-				output ("    }\n");
+				output ("\t\tx_ = APPEND_list (x_, z_);\n");
+				output ("\t}\n");
 				break;
 			}
 
 			case type_stack_tag : {
 				TYPE_P s = DEREF_ptr (type_stack_sub (t0));
-				output ("    LIST (%TT) w_;\n", s);
-				output ("    w_ = NULL_list (%TT);\n", s);
-				output ("    while (READ_BITS (1)) {\n");
-				output ("\t%TT y_;\n", s);
-				output ("\t%TT z_;\n", t);
-				output ("\ty_ = READ_%TI ();\n", s);
-				output ("\tCONS_%TM (y_, NULL_list (%TT), z_);\n",
+				output ("\tLIST (%TT) w_;\n", s);
+				output ("\tw_ = NULL_list (%TT);\n", s);
+				output ("\twhile (READ_BITS (1)) {\n");
+				output ("\t\t%TT y_;\n", s);
+				output ("\t\t%TT z_;\n", t);
+				output ("\t\ty_ = READ_%TI ();\n", s);
+				output ("\t\tCONS_%TM (y_, NULL_list (%TT), z_);\n",
 						s, s);
-				output ("\tw_ = APPEND_list (w_, z_);\n");
-				output ("    }\n");
-				output ("    x_ = STACK_list (w_);\n");
+				output ("\t\tw_ = APPEND_list (w_, z_);\n");
+				output ("\t}\n");
+				output ("\tx_ = STACK_list (w_);\n");
 				break;
 			}
 
 			case type_vec_tag : {
 				TYPE_P s = DEREF_ptr (type_vec_sub (t0));
-				output ("    PTR (%TT) y_;\n", s);
-				output ("    %X_dim n_ = (%X_dim) READ_DIM ();\n");
-				output ("    MAKE_vec (%TS, n_, x_);\n", s);
-				output ("    y_ = PTR_vec_ptr (");
+				output ("\tPTR (%TT) y_;\n", s);
+				output ("\t%X_dim n_ = (%X_dim) READ_DIM ();\n");
+				output ("\tMAKE_vec (%TS, n_, x_);\n", s);
+				output ("\ty_ = PTR_vec_ptr (");
 				output ("VEC_PTR_vec (x_));\n");
-				output ("    while (n_--) {\n");
-				output ("\tCOPY_%TM (y_, READ_%TI ());\n", s, s);
-				output ("\ty_ = STEP_ptr (y_, %TS);\n", s);
-				output ("    }\n");
+				output ("\twhile (n_--) {\n");
+				output ("\t\tCOPY_%TM (y_, READ_%TI ());\n", s, s);
+				output ("\t\ty_ = STEP_ptr (y_, %TS);\n", s);
+				output ("\t}\n");
 				break;
 			}
 
 			case type_vec_ptr_tag : {
 				TYPE_P s = DEREF_ptr (type_vec_ptr_sub (t0));
-				output ("    VEC (%TT) y_;\n", s);
-				output ("    PTR (%TT) z_;\n", s);
-				output ("    MAKE_vec (%TS, (%X_dim) 1, y_);\n", s);
-				output ("    x_ = VEC_PTR_vec (y_);\n");
-				output ("    z_ = PTR_vec_ptr (x_);\n");
-				output ("    COPY_%TM (z_, READ_%TI ());\n", s, s);
+				output ("\tVEC (%TT) y_;\n", s);
+				output ("\tPTR (%TT) z_;\n", s);
+				output ("\tMAKE_vec (%TS, (%X_dim) 1, y_);\n", s);
+				output ("\tx_ = VEC_PTR_vec (y_);\n");
+				output ("\tz_ = PTR_vec_ptr (x_);\n");
+				output ("\tCOPY_%TM (z_, READ_%TI ());\n", s, s);
 				break;
 			}
 			}
 
 			/* Function trailer */
-			output ("    return (x_);\n");
+			output ("\treturn (x_);\n");
 			output ("}\n\n");
 			output ("#endif\n\n\n", t);
 		}
@@ -384,11 +384,11 @@ disk_write_enum(void)
 {
     number n = log2 (DEREF_number (en_order (CRT_ENUM)));
     if (n <= 16) {
-		output ("    WRITE_BITS (%n, (unsigned) x_);\n", n);
+		output ("\tWRITE_BITS (%n, (unsigned) x_);\n", n);
     } else {
 		n -= 16;
-		output ("    WRITE_BITS (16, (unsigned) (x_ & 0xffff));\n");
-		output ("    WRITE_BITS (%n, (unsigned) (x_ >> 16));\n", n);
+		output ("\tWRITE_BITS (16, (unsigned) (x_ & 0xffff));\n");
+		output ("\tWRITE_BITS (%n, (unsigned) (x_ >> 16));\n", n);
     }
     return;
 }
@@ -405,7 +405,7 @@ disk_write_struct(void)
 {
     LOOP_STRUCTURE_COMPONENT {
 		TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-		output ("    WRITE_%TI (x_.%CN);\n", t);
+		output ("\tWRITE_%TI (x_.%CN);\n", t);
     }
     return;
 }
@@ -421,43 +421,43 @@ static void
 disk_write_union(void)
 {
     int have_ucmp = 0;
-    output ("    if (IS_NULL_%UM (x_)) {\n");
-    output ("\tWRITE_BITS (1, (unsigned) 0);\n");
-    output ("    } else {\n");
+    output ("\tif (IS_NULL_%UM (x_)) {\n");
+    output ("\t\tWRITE_BITS (1, (unsigned) 0);\n");
+    output ("\t} else {\n");
     LOOP_UNION_COMPONENT {
-		output ("\t%CT %CN;\n");
+		output ("\t\t%CT %CN;\n");
 		have_ucmp = 1;
     }
-    output ("\tunsigned tag_ = TAG_%UM (x_);\n");
-    output ("\tWRITE_BITS (1, (unsigned) 1);\n");
-    output ("\tWRITE_BITS (%UO2, tag_);\n");
-    output ("\tswitch (tag_) {\n");
+    output ("\t\tunsigned tag_ = TAG_%UM (x_);\n");
+    output ("\t\tWRITE_BITS (1, (unsigned) 1);\n");
+    output ("\t\tWRITE_BITS (%UO2, tag_);\n");
+    output ("\t\tswitch (tag_) {\n");
     LOOP_UNION_FIELD {
 		int have_cmp = have_ucmp;
 		int al = DEREF_int (fld_flag (CRT_FIELD));
-		output ("\t    case %UM_%FN_tag : {\n");
+		output ("\t\tcase %UM_%FN_tag : {\n");
 		LOOP_FIELD_COMPONENT {
-			output ("\t\t%CT %CN;\n");
+			output ("\t\t\t%CT %CN;\n");
 			have_cmp = 1;
 		}
 
 		/* Deal with aliasing */
 		if (al) {
-			output ("\t\tunsigned alias_ = GET_ALIAS_%UM_%FN (x_);\n");
-			output ("\t\tif (alias_) {\n");
-			output ("\t\t    WRITE_ALIAS (alias_);\n");
-			output ("\t\t    WRITE_BITS (1, (unsigned) 0);\n");
-			output ("\t\t    break;\n");
-			output ("\t\t}\n");
-			output ("\t\talias_ = ++crt_%X_alias;\n");
-			output ("\t\tSET_ALIAS_%UM_%FN (x_, alias_);\n");
-			output ("\t\tWRITE_ALIAS (alias_);\n");
-			output ("\t\tWRITE_BITS (1, (unsigned) 1);\n");
+			output ("\t\t\tunsigned alias_ = GET_ALIAS_%UM_%FN (x_);\n");
+			output ("\t\t\tif (alias_) {\n");
+			output ("\t\t\t\tWRITE_ALIAS (alias_);\n");
+			output ("\t\t\t\tWRITE_BITS (1, (unsigned) 0);\n");
+			output ("\t\t\t\tbreak;\n");
+			output ("\t\t\t}\n");
+			output ("\t\t\talias_ = ++crt_%X_alias;\n");
+			output ("\t\t\tSET_ALIAS_%UM_%FN (x_, alias_);\n");
+			output ("\t\t\tWRITE_ALIAS (alias_);\n");
+			output ("\t\t\tWRITE_BITS (1, (unsigned) 1);\n");
 		}
 
 		/* Deconstruct union */
 		if (have_cmp) {
-			output ("\t\tDECONS_%UM_%FN (");
+			output ("\t\t\tDECONS_%UM_%FN (");
 			LOOP_UNION_COMPONENT output ("%CN, ");
 			LOOP_FIELD_COMPONENT output ("%CN, ");
 			output (" x_);\n");
@@ -465,7 +465,7 @@ disk_write_union(void)
 
 		/* Process further if necessary */
 		if (al == 2) {
-			output ("\t\tALIAS_%UM_%FN (");
+			output ("\t\t\tALIAS_%UM_%FN (");
 			LOOP_UNION_COMPONENT output ("%CN, ");
 			LOOP_FIELD_COMPONENT output ("%CN, ");
 			output (" x_);\n");
@@ -474,17 +474,17 @@ disk_write_union(void)
 		/* Write out components */
 		LOOP_UNION_COMPONENT {
 			TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-			output ("\t\tWRITE_%TI (%CN);\n", t);
+			output ("\t\t\tWRITE_%TI (%CN);\n", t);
 		}
 		LOOP_FIELD_COMPONENT {
 			TYPE_P t = DEREF_ptr (cmp_type (CRT_COMPONENT));
-			output ("\t\tWRITE_%TI (%CN);\n", t);
+			output ("\t\t\tWRITE_%TI (%CN);\n", t);
 		}
-		output ("\t\tbreak;\n");
-		output ("\t    }\n");
+		output ("\t\t\tbreak;\n");
+		output ("\t\t}\n");
     }
+    output ("\t\t}\n");
     output ("\t}\n");
-    output ("    }\n");
     return;
 }
 
@@ -573,7 +573,7 @@ disk_write_def(char *dir)
 				output ("\tif (IS_NULL_ptr (x_)) {\n");
 				output ("\t\tWRITE_BITS (1, (unsigned) 0);\n");
 				output ("\t} else {\n");
-				output ("\t\t%TT y_;\n\t", s);
+				output ("\t\t%TT y_;\n\t\t", s);
 				print_deref (s, "x_", "y_");
 				output ("\t\tWRITE_BITS (1, (unsigned) 1);\n");
 				output ("\t\tWRITE_%TI (y_);\n", s);
@@ -584,7 +584,7 @@ disk_write_def(char *dir)
 			case type_list_tag : {
 				TYPE_P s = DEREF_ptr (type_list_sub (t0));
 				output ("\twhile (!IS_NULL_list (x_)) {\n");
-				output ("\t\t%TT y_;\n\t", s);
+				output ("\t\t%TT y_;\n\t\t", s);
 				print_deref (s, "HEAD_list (x_)", "y_");
 				output ("\t\tWRITE_BITS (1, (unsigned) 1);\n");
 				output ("\t\tWRITE_%TI (y_);\n", s);
@@ -598,7 +598,7 @@ disk_write_def(char *dir)
 				TYPE_P s = DEREF_ptr (type_stack_sub (t0));
 				output ("\tLIST (%TT) w_ = LIST_stack (x_);\n", s);
 				output ("\twhile (!IS_NULL_list (w_)) {\n");
-				output ("\t%TT y_;\n\t", s);
+				output ("\t%TT y_;\n\t\t", s);
 				print_deref (s, "HEAD_list (w_)", "y_");
 				output ("\t\tWRITE_BITS (1, (unsigned) 1);\n");
 				output ("\t\tWRITE_%TI (y_);\n", s);
@@ -615,7 +615,7 @@ disk_write_def(char *dir)
 				output (" = PTR_vec_ptr (VEC_PTR_vec (x_));\n");
 				output ("\tWRITE_DIM ((unsigned) n_);\n");
 				output ("\twhile (n_--) {\n");
-				output ("\t\t%TT z_;\n\t", s);
+				output ("\t\t%TT z_;\n\t\t", s);
 				print_deref (s, "y_", "z_");
 				output ("\t\tWRITE_%TI (z_);\n", s);
 				output ("\t\ty_ = STEP_ptr (y_, %TS);\n", s);
@@ -626,7 +626,7 @@ disk_write_def(char *dir)
 			case type_vec_ptr_tag : {
 				TYPE_P s = DEREF_ptr (type_vec_ptr_sub (t0));
 				output ("\tPTR (%TT) y_ = PTR_vec_ptr (x_);\n", s);
-				output ("\t%TT z_;\n    ", s);
+				output ("\t%TT z_;\n\t", s);
 				print_deref (s, "y_", "z_");
 				output ("\tWRITE_%TI (z_);\n", s);
 				break;
