889726145645cb2a3d189c4e14c67ab7ff29577b
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2004-09-22 06:32:59 +0000

    Get rid of OSSG and works towards ANSI C. Add KW_WEAK_PROTOTYPE to action.c and basic.c and wrap for non-TenDRA compilers.
    svn path=/tendra/trunk/; revision=326

--- tendra/src/utilities/sid/action.c
+++ tendra/src/utilities/sid/action.c
@@ -1,6 +1,39 @@
 /*
+ * Copyright (c) 2002, 2003, 2004 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The TenDRA Project by
+ * Jeroen Ruigrok van der Werven.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1997
-    
+
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
     acting through the Defence Evaluation and Research Agency
@@ -9,18 +42,18 @@
     to other parties and amendment for any purpose not excluding
     product development provided that any such use et cetera
     shall be deemed to be acceptance of the following conditions:-
-    
+
         (1) Its Recipients shall ensure that this Notice is
         reproduced upon any copies or amended versions of it;
-    
+
         (2) Any amended version of it shall be clearly marked to
         show both the nature of and the organisation responsible
         for the relevant amendment or amendments;
-    
+
         (3) Its onward transfer from a recipient to another
         party shall be deemed to be that party's acceptance of
         these conditions;
-    
+
         (4) DERA gives no warranty or assurance as to its
         quality or suitability for any purpose and DERA accepts
         no liability whatsoever in relation to any use to which
@@ -60,80 +93,79 @@
 
 /*--------------------------------------------------------------------------*/
 
-ActionP
-action_create PROTO_Z ()
-{
-    ActionP action = ALLOCATE (ActionT);
+#if FS_WEAK_PROTOTYPES
+#pragma TenDRA keyword KW_WEAK_PROTOTYPE for weak
+#else
+#define KW_WEAK_PROTOTYPE
+#endif
 
-    types_init (action_param (action));
-    types_init (action_result (action));
-    action->code = NIL (GenericP);
-    return (action);
+ActionP
+action_create(void)
+{
+    ActionP action = ALLOCATE(ActionT);
+
+    types_init(action_param(action));
+    types_init(action_result(action));
+    action->code = NIL(GenericP);
+    return(action);
 }
 
 #ifdef FS_FAST
 #undef action_param
 #endif /* defined (FS_FAST) */
 TypeTupleP
-action_param PROTO_N ((action))
-	     PROTO_T (ActionP action)
+action_param(ActionP action)
 {
-    return (&(action->param));
+    return(&(action->param));
 }
 #ifdef FS_FAST
-#define action_param(a) (&((a)->param))
+#define action_param(a)	(&((a) ->param))
 #endif /* defined (FS_FAST) */
 
 #ifdef FS_FAST
 #undef action_result
 #endif /* defined (FS_FAST) */
 TypeTupleP
-action_result PROTO_N ((action))
-	      PROTO_T (ActionP action)
+action_result(ActionP action)
 {
-    return (&(action->result));
+    return(&(action->result));
 }
 #ifdef FS_FAST
-#define action_result(a) (&((a)->result))
+#define action_result(a)	(&((a)->result))
 #endif /* defined (FS_FAST) */
 
 #ifdef FS_FAST
 #undef action_get_code
 #endif /* defined (FS_FAST) */
 GenericP
-action_get_code PROTO_N ((action))
-		PROTO_T (ActionP action)
+action_get_code(ActionP action)
 {
-    return (action->code);
+    return(action->code);
 }
 #ifdef FS_FAST
-#define action_get_code(a) ((a)->code)
+#define action_get_code(a)	((a)->code)
 #endif /* defined (FS_FAST) */
 
 #ifdef FS_FAST
 #undef action_set_code
 #endif /* defined (FS_FAST) */
 void
-action_set_code PROTO_N ((action, code))
-		PROTO_T (ActionP  action X
-			 GenericP code)
+action_set_code(ActionP action, GenericP code)
 {
     action->code = code;
 }
 #ifdef FS_FAST
-#define action_set_code(a, c) ((a)->code = (c))
+#define action_set_code(a, c)	((a) ->code = (c))
 #endif /* defined (FS_FAST) */
 
 void
-action_iter_for_table PROTO_N ((action, full, proc, closure))
-		      PROTO_T (ActionP  action X
-			       BoolT    full X
-			       void   (*proc) PROTO_S ((EntryP, GenericP)) X
-			       GenericP closure)
+action_iter_for_table(ActionP action, BoolT full,
+		      void (*proc)KW_WEAK_PROTOTYPE(EntryP, GenericP),
+		      GenericP closure)
 {
     if (full) {
-	types_iter_for_table (action_param (action), proc, closure);
-	types_iter_for_table (action_result (action), proc, closure);
+	types_iter_for_table(action_param(action), proc, closure);
+	types_iter_for_table(action_result(action), proc, closure);
     }
 }
 
--- tendra/src/utilities/sid/basic.h
+++ tendra/src/utilities/sid/basic.h
@@ -1,6 +1,39 @@
 /*
+ * Copyright (c) 2002, 2003, 2004 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The TenDRA Project by
+ * Jeroen Ruigrok van der Werven.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1997
-    
+
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
     acting through the Defence Evaluation and Research Agency
@@ -9,18 +42,18 @@
     to other parties and amendment for any purpose not excluding
     product development provided that any such use et cetera
     shall be deemed to be acceptance of the following conditions:-
-    
+
         (1) Its Recipients shall ensure that this Notice is
         reproduced upon any copies or amended versions of it;
-    
+
         (2) Any amended version of it shall be clearly marked to
         show both the nature of and the organisation responsible
         for the relevant amendment or amendments;
-    
+
         (3) Its onward transfer from a recipient to another
         party shall be deemed to be that party's acceptance of
         these conditions;
-    
+
         (4) DERA gives no warranty or assurance as to its
         quality or suitability for any purpose and DERA accepts
         no liability whatsoever in relation to any use to which
@@ -80,32 +113,26 @@ typedef struct BasicClosureT {
 
 /*--------------------------------------------------------------------------*/
 
-extern BasicP			basic_create
-	PROTO_S ((GrammarP, BoolT));
-extern unsigned			basic_terminal
-	PROTO_S ((BasicP));
-extern TypeTupleP		basic_result
-	PROTO_S ((BasicP));
-extern GenericP			basic_get_result_code
-	PROTO_S ((BasicP));
-extern void			basic_set_result_code
-	PROTO_S ((BasicP, GenericP));
-extern BoolT			basic_get_ignored
-	PROTO_S ((BasicP));
-extern void			basic_iter_for_table
-	PROTO_S ((BasicP, BoolT, void (*) (EntryP, GenericP), GenericP));
+extern BasicP		basic_create(GrammarP, BoolT);
+extern unsigned		basic_terminal(BasicP);
+extern TypeTupleP	basic_result(BasicP);
+extern GenericP		basic_get_result_code(BasicP);
+extern void		basic_set_result_code(BasicP, GenericP);
+extern BoolT		basic_get_ignored(BasicP);
+extern void		basic_iter_for_table(BasicP, BoolT,
+					     void(*)(EntryP, GenericP),
+					     GenericP);
 
-extern void			write_basics
-	PROTO_S ((OStreamP, BasicClosureP));
+extern void		write_basics(OStreamP, BasicClosureP);
 
 /*--------------------------------------------------------------------------*/
 
 #ifdef FS_FAST
-#define basic_terminal(b) ((b)->terminal)
-#define basic_result(b) (&((b)->result))
-#define basic_get_result_code(b) ((b)->result_code)
-#define basic_set_result_code(b, c) ((b)->result_code = (c))
-#define basic_get_ignored(b) ((b)->ignored)
+#define basic_terminal(b)		((b) ->terminal)
+#define basic_result(b)			(& ((b) ->result))
+#define basic_get_result_code(b)	((b) ->result_code)
+#define basic_set_result_code(b, c)	((b) ->result_code = (c))
+#define basic_get_ignored(b)		((b) ->ignored)
 #endif /* defined (FS_FAST) */
 
 #endif /* !defined (H_BASIC) */
