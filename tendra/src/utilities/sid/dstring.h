0d22f23b0fdfa1c768360afe082f82e658887629
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2004-09-23 09:08:19 +0000

    Clean up OSSG and move towards ANSI C.
    svn path=/tendra/trunk/; revision=327

--- tendra/src/utilities/sid/dstring.h
+++ tendra/src/utilities/sid/dstring.h
@@ -1,6 +1,39 @@
 /*
+ * Copyright (c) 2002, 2003, 2004 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The TenDRA Project by
+ * Jeroen Ruigrok van der Werven.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1997
-    
+
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
     acting through the Defence Evaluation and Research Agency
@@ -9,18 +42,18 @@
     to other parties and amendment for any purpose not excluding
     product development provided that any such use et cetera
     shall be deemed to be acceptance of the following conditions:-
-    
+
         (1) Its Recipients shall ensure that this Notice is
         reproduced upon any copies or amended versions of it;
-    
+
         (2) Any amended version of it shall be clearly marked to
         show both the nature of and the organisation responsible
         for the relevant amendment or amendments;
-    
+
         (3) Its onward transfer from a recipient to another
         party shall be deemed to be that party's acceptance of
         these conditions;
-    
+
         (4) DERA gives no warranty or assurance as to its
         quality or suitability for any purpose and DERA accepts
         no liability whatsoever in relation to any use to which
@@ -292,69 +325,42 @@ typedef struct DStringT {
 
 /*--------------------------------------------------------------------------*/
 
-extern void			nstring_init
-	PROTO_S ((NStringP));
-extern void			nstring_init_length
-	PROTO_S ((NStringP, unsigned));
-extern void			nstring_assign
-	PROTO_S ((NStringP, NStringP));
-extern void			nstring_copy_cstring
-	PROTO_S ((NStringP, CStringP));
-extern void			nstring_insert_cstring
-	PROTO_S ((NStringP, CStringP));
-extern void			nstring_copy
-	PROTO_S ((NStringP, NStringP));
-extern CStringP			nstring_to_cstring
-	PROTO_S ((NStringP));
-extern unsigned			nstring_hash_value
-	PROTO_S ((NStringP));
-extern unsigned			nstring_length
-	PROTO_S ((NStringP));
-extern CStringP			nstring_contents
-	PROTO_S ((NStringP));
-extern CmpT			nstring_compare
-	PROTO_S ((NStringP, NStringP));
-extern BoolT			nstring_equal
-	PROTO_S ((NStringP, NStringP));
-extern BoolT			nstring_ci_equal
-	PROTO_S ((NStringP, NStringP));
-extern BoolT			nstring_contains
-	PROTO_S ((NStringP, char));
-extern BoolT			nstring_is_prefix
-	PROTO_S ((NStringP, NStringP));
-extern void			nstring_destroy
-	PROTO_S ((NStringP));
+extern void		nstring_init(NStringP);
+extern void		nstring_init_length(NStringP, unsigned);
+extern void		nstring_assign(NStringP, NStringP);
+extern void		nstring_copy_cstring(NStringP, CStringP);
+extern void		nstring_insert_cstring(NStringP, CStringP);
+extern void		nstring_copy(NStringP, NStringP);
+extern CStringP		nstring_to_cstring(NStringP);
+extern unsigned		nstring_hash_value(NStringP);
+extern unsigned		nstring_length(NStringP);
+extern CStringP		nstring_contents(NStringP);
+extern CmpT		nstring_compare(NStringP, NStringP);
+extern BoolT		nstring_equal(NStringP, NStringP);
+extern BoolT		nstring_ci_equal(NStringP, NStringP);
+extern BoolT		nstring_contains(NStringP, char);
+extern BoolT		nstring_is_prefix(NStringP, NStringP);
+extern void		nstring_destroy(NStringP);
 
-extern void			write_nstring
-	PROTO_S ((OStreamP, NStringP));
+extern void		write_nstring(OStreamP, NStringP);
 
-extern void			dstring_init
-	PROTO_S ((DStringP));
-extern unsigned			dstring_length
-	PROTO_S ((DStringP));
-extern void			dstring_append_char
-	PROTO_S ((DStringP, char));
-extern void			dstring_append_cstring
-	PROTO_S ((DStringP, CStringP));
-extern void			dstring_append_nstring
-	PROTO_S ((DStringP, NStringP));
-extern BoolT			dstring_last_char_equal
-	PROTO_S ((DStringP, char));
-extern void			dstring_to_nstring
-	PROTO_S ((DStringP, NStringP));
-extern CStringP			dstring_to_cstring
-	PROTO_S ((DStringP));
-extern CStringP			dstring_destroy_to_cstring
-	PROTO_S ((DStringP));
-extern void			dstring_destroy
-	PROTO_S ((DStringP));
+extern void		dstring_init(DStringP);
+extern unsigned		dstring_length(DStringP);
+extern void		dstring_append_char(DStringP, char);
+extern void		dstring_append_cstring(DStringP, CStringP);
+extern void		dstring_append_nstring(DStringP, NStringP);
+extern BoolT		dstring_last_char_equal(DStringP, char);
+extern void		dstring_to_nstring(DStringP, NStringP);
+extern CStringP		dstring_to_cstring(DStringP);
+extern CStringP		dstring_destroy_to_cstring(DStringP);
+extern void		dstring_destroy(DStringP);
 
 /*--------------------------------------------------------------------------*/
 
 #ifdef FS_FAST
-#define nstring_length(s) ((s)->length)
-#define nstring_contents(s) ((s)->contents)
-#define dstring_length(s) ((s)->length)
+#define nstring_length(s)	((s) ->length)
+#define nstring_contents(s)	((s) ->contents)
+#define dstring_length(s)	((s) ->length)
 #endif /* defined (FS_FAST) */
 
 #endif /* !defined (H_DSTRING) */
