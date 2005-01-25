d5bcdf3834aa31e94b24323a002410b0157a10d3
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2005-01-25 20:12:08 +0000

    Add $Id$. Add TenDRA copyright. Add vim filetype identifier at the bottom to ease syntax highlighting.
    svn path=/tendra/trunk/; revision=551

--- tendra/src/lib/apis/ansi/time.h
+++ tendra/src/lib/apis/ansi/time.h
@@ -1,3 +1,29 @@
+#
+# Copyright (c) 2002-2005 The TenDRA Project <http://www.tendra.org/>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
 #   		 Crown Copyright (c) 1997
 #   
 #   This TenDRA(r) Computer Program is subject to Copyright
@@ -25,6 +51,9 @@
 #       no liability whatsoever in relation to any use to which
 #       it may be put.
 #
+# $Id$
+#
+
 +IMPLEMENT "ansi", "stddef.h", "null" ;
 +IMPLEMENT "ansi", "stddef.h", "size_t" ;
 
@@ -59,3 +88,5 @@
 +FUNC double difftime ( time_t, time_t ) ;
 +ENDIF
 +ENDIF
+
+# vim:set filetype=tspec:
