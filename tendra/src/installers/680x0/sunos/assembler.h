b98245808ac29ee5310ec0f7cb8252b98691f89a
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2005-08-05 08:00:15 +0000

    Clean up files.
    svn path=/tendra/trunk/; revision=677

--- tendra/src/installers/680x0/sunos/assembler.h
+++ tendra/src/installers/680x0/sunos/assembler.h
@@ -1,6 +1,36 @@
 /*
+ * Copyright (c) 2002-2005 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1997
-    
+
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
     acting through the Defence Evaluation and Research Agency
@@ -9,18 +39,18 @@
     to other parties and amendment for any purpose not excluding
     product development provided that any such use et cetera
     shall be deemed to be acceptance of the following conditions:-
-    
+
         (1) Its Recipients shall ensure that this Notice is
         reproduced upon any copies or amended versions of it;
-    
+
         (2) Any amended version of it shall be clearly marked to
         show both the nature of and the organisation responsible
         for the relevant amendment or amendments;
-    
+
         (3) Its onward transfer from a recipient to another
         party shall be deemed to be that party's acceptance of
         these conditions;
-    
+
         (4) DERA gives no warranty or assurance as to its
         quality or suitability for any purpose and DERA accepts
         no liability whatsoever in relation to any use to which
@@ -58,7 +88,7 @@ Imported from DRA
  *
  * Revision 1.1  93/02/22  17:15:10  17:15:10  ra (Robert Andrews)
  * Initial revision
- * 
+ *
 --------------------------------------------------------------------------
 */
 
@@ -108,31 +138,31 @@ Imported from DRA
 #define  asm_percent_regs		/* registers are %d0 etc */
 
 #define  asm_data_first			/* true */
-#define  asm_nprefix			outc ( '&' )
-#define  asm_fprefix			outs ( "0f" )
-#define  asm_ind_before			outc ( '(' )
+#define  asm_nprefix			outc(' & ')
+#define  asm_fprefix			outs("0f")
+#define  asm_ind_before			outc('(')
 #define  asm_ind_middle			/* empty */
-#define  asm_ind_after			outc ( ')' )
-#define  asm_predec_before		outs ( "-(" )
-#define  asm_predec_after		outc ( ')' )
-#define  asm_postinc_before		outc ( '(' )
-#define  asm_postinc_after		outs ( ")+" )
-#define  asm_scale_before		outc ( ',' )
-#define  asm_scale			outs ( ".l*" )
-#define  asm_scale_1			outs ( ".l" )
-#define  asm_mem_before			outs ( "([" )
+#define  asm_ind_after			outc(')')
+#define  asm_predec_before		outs(" - (")
+#define  asm_predec_after		outc(')')
+#define  asm_postinc_before		outc('(')
+#define  asm_postinc_after		outs(") + ")
+#define  asm_scale_before		outc(',')
+#define  asm_scale			outs(".l*")
+#define  asm_scale_1			outs(".l")
+#define  asm_mem_before			outs("([")
 #define  asm_mem_second			/* empty */
-#define  asm_mem_third			outc ( ']' )
-#define  asm_mem_after			outc ( ')' )
-#define  asm_bf_before			outc ( '{' )
-#define  asm_bf_middle			outc ( ':' )
-#define  asm_bf_after			outc ( '}' )
-#define  asm_rpair_sep			outc ( ':' )
-#define  asm_comment			outc ( '#' )
+#define  asm_mem_third			outc(']')
+#define  asm_mem_after			outc(')')
+#define  asm_bf_before			outc('{')
+#define  asm_bf_middle			outc(':')
+#define  asm_bf_after			outc('}')
+#define  asm_rpair_sep			outc(':')
+#define  asm_comment			outc('#')
 
 #define  asm_cmp_reversed		/* cmp instructions are reversed */
-#define  asm_version			outs ( "\tversion 2" )
-#define  asm_version_aux		outs ( "\tversion 3" )
+#define  asm_version			outs("\tversion 2")
+#define  asm_version_aux		outs("\tversion 3")
 
 #endif /* HP_ASSEMBLER */
 
@@ -142,7 +172,7 @@ Imported from DRA
 */
 
 #ifdef XDB_DEBUGGER
-#define  default_diag			DIAG_UNKNOWN
+#define default_diag			DIAG_UNKNOWN
 #endif /* XDB_DEBUGGER */
 
 
@@ -152,12 +182,12 @@ Imported from DRA
 
 #ifdef HP_INTERFACE
 
-#define  hp_cc_conventions		/* use HP conventions */
-#define  float_to_unsigned		"___fixu"
-#define  unsigned_to_float		"___floatu"
-#define  profiling_routine		"mcount"
-#define  profiling_reg			REG_A0
-#define  profiling_uses_lea		0
+#define hp_cc_conventions		/* use HP conventions */
+#define float_to_unsigned		"___fixu"
+#define unsigned_to_float		"___floatu"
+#define profiling_routine		"mcount"
+#define profiling_reg			REG_A0
+#define profiling_uses_lea		0
 
 #endif /* HP_INTERFACE */
 
@@ -168,36 +198,36 @@ Imported from DRA
 
 #ifdef GAS_ASSEMBLER
 
-#define  asm_simple_instrs		/* instructions are movl etc */
-#define  asm_simple_regs		/* registers are d0 etc */
+#define asm_simple_instrs		/* instructions are movl etc */
+#define asm_simple_regs			/* registers are d0 etc */
 
-#undef 	 asm_data_first			/* false */
-#define  asm_nprefix			outc ( '#' )
-#define  asm_fprefix			outs ( "0r" )
-#define  asm_ind_before			/* empty */
-#define  asm_ind_middle			outc ( '@' )
-#define  asm_ind_after			/* empty */
-#define  asm_predec_before		/* empty */
-#define  asm_predec_after		outs ( "@-" )
-#define  asm_postinc_before		/* empty */
-#define  asm_postinc_after		outs ( "@+" )
-#define  asm_scale_before		/* empty */
-#define  asm_scale			outs ( ":l:" )
-#define  asm_scale_1			outs ( ":l" )
-#define  asm_mem_before			/* empty */
-#define  asm_mem_second			outc ( '@' )
-#define  asm_mem_third			outc ( '@' )
-#define  asm_mem_after			/* empty */
-#define  asm_bf_before			outc ( '{' )
-#define  asm_bf_middle			outc ( ':' )
-#define  asm_bf_after			outc ( '}' )
-#define  asm_rpair_sep			outc ( ':' )
-#define  asm_comment			outc ( '#' )
+#undef 	sm_data_first			/* false */
+#define sm_nprefix			outc('#')
+#define sm_fprefix			outs("0r")
+#define sm_ind_before			/* empty */
+#define sm_ind_middle			outc('@')
+#define sm_ind_after			/* empty */
+#define sm_predec_before		/* empty */
+#define sm_predec_after			outs("@ - ")
+#define sm_postinc_before		/* empty */
+#define sm_postinc_after		outs("@ + ")
+#define sm_scale_before			/* empty */
+#define sm_scale			outs(":l:")
+#define sm_scale_1			outs(":l")
+#define sm_mem_before			/* empty */
+#define sm_mem_second			outc('@')
+#define sm_mem_third			outc('@')
+#define sm_mem_after			/* empty */
+#define sm_bf_before			outc('{')
+#define sm_bf_middle			outc(':')
+#define sm_bf_after			outc('}')
+#define sm_rpair_sep			outc(':')
+#define sm_comment			outc('#')
 
-#define  asm_does_jump_lens		/* calculates jump lengths */
-#define  asm_uses_equals		/* uses = rather than set */
-#define  asm_uses_lcomm			/* uses lcomm rather than bss */
-#define  asm_no_btst_suffix		/* doesn't understand btst.[bl] */
+#define asm_does_jump_lens		/* calculates jump lengths */
+#define asm_uses_equals			/* uses = rather than set */
+#define asm_uses_lcomm			/* uses lcomm rather than bss */
+#define asm_no_btst_suffix		/* doesn't understand btst.[bl] */
 
 #endif /* GAS_ASSEMBLER */
 
@@ -207,7 +237,7 @@ Imported from DRA
 */
 
 #ifdef GDB_DEBUGGER
-#define  default_diag			DIAG_STAB
+#define default_diag			DIAG_STAB
 #endif /* GDB_DEBUGGER */
 
 
@@ -217,12 +247,12 @@ Imported from DRA
 
 #ifdef NEXT_INTERFACE
 
-#define  gcc_conventions		/* use gcc conventions */
-#define  float_to_unsigned		"__fixunsdfsi"
-#undef   unsigned_to_float		/* use built-in version */
-#define  profiling_routine		"mcount"
-#define  profiling_reg			REG_A0
-#define  profiling_uses_lea		1
+#define gcc_conventions			/* use gcc conventions */
+#define float_to_unsigned		"__fixunsdfsi"
+#undef  unsigned_to_float		/* use built-in version */
+#define profiling_routine		"mcount"
+#define profiling_reg			REG_A0
+#define profiling_uses_lea		1
 
 #endif /* NEXT_INTERFACE */
 
@@ -235,4 +265,3 @@ Imported from DRA
 #define profiling_reg REG_A0        /* are probably wrong. */
 #define profiling_uses_lea 1
 #endif
-
--- tendra/src/installers/680x0/sunos/main.c
+++ tendra/src/installers/680x0/sunos/main.c
@@ -1,4 +1,34 @@
 /*
+ * Copyright (c) 2002-2005 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1996
 
     This TenDRA(r) Computer Program is subject to Copyright
@@ -134,58 +164,58 @@ Imported from DRA
 #include "xdb_basics.h"
 #include "xdb_output.h"
 #endif
-extern int errors ;
-extern int max_errors ;
+extern int errors;
+extern int max_errors;
 
 
 /*
     PROGRAM NAME AND VERSION NUMBER
 */
 
-char *progname = "hptrans" ;
-static char *version_str = "Version: 0.6" ;
-static char *revision = REVISION_STRING ;
-static char *revdate = DATE_STRING ;
-int normal_version = 1 ;
+char *progname = "hptrans";
+static char *version_str = "Version: 0.6";
+static char *revision = REVISION_STRING;
+static char *revdate = DATE_STRING;
+int normal_version = 1;
 
 
 /*
     EXTRA COMPILATION FLAGS
 */
 
-int do_peephole = 1 ;
-int do_pic = 0 ;
-static int do_quit = 0 ;
-static int do_sep_units = 0 ;
-static int ignore_errors = 0 ;
+int do_peephole = 1;
+int do_pic = 0;
+static int do_quit = 0;
+static int do_sep_units = 0;
+static int ignore_errors = 0;
 #ifdef EBUG
-int optimize = 0 ;
+int optimize = 0;
 #else
-static int optimize = 1 ;
+static int optimize = 1;
 #endif
-static int report_version = 0 ;
-static int report_tdf_versions = 0 ;
-static int show_options = 0 ;
-static int dummy_option = 0 ;
+static int report_version = 0;
+static int report_tdf_versions = 0;
+static int show_options = 0;
+static int dummy_option = 0;
 
 #ifdef EBUG
 
-int do_test = 0 ;
-int seek_label = 0 ;
-int seek_extern = 0 ;
-int seek_label_no ;
-char *seek_extern_id ;
+int do_test = 0;
+int seek_label = 0;
+int seek_extern = 0;
+int seek_label_no;
+char *seek_extern_id;
 
-int seek_line = 0 ;
-int seek_line_no ;
-static char *seek_line_id ;
+int seek_line = 0;
+int seek_line_no;
+static char *seek_line_id;
 
 #endif
 
-int diag_override ;
-static int diag_stab_override = 0 ;
-static int diag_xdb_new_override = 0 ;
-static int diag_xdb_old_override = 0 ;
+int diag_override;
+static int diag_stab_override = 0;
+static int diag_xdb_new_override = 0;
+static int diag_xdb_old_override = 0;
 
 
 
@@ -193,14 +223,14 @@ static int diag_xdb_old_override = 0 ;
     VARIABLE SIZES AND ALIGNMENTS
 */
 
-alignment MAX_BF_SIZE ;
+alignment MAX_BF_SIZE;
 
 
 /*
     MAXIMUM EXPONENT FOR FLOATING-POINT NUMBERS
 */
 
-int target_dbl_maxexp = 1024 ;
+int target_dbl_maxexp = 1024;
 
 
 /*
@@ -221,49 +251,49 @@ int target_dbl_maxexp = 1024 ;
 */
 
 struct {
-    char *opt ;
-    char on ;
-    char off ;
-    char sw ;
-    int *flag ;
-    char **value ;
-} options [] = {
-    { "alloca", 0, 0, 'A', &do_alloca, null },
-    { "cc", 'c', 'g', 0, &cc_conventions, null },
-    { "diag", 'H', 0, 0, &diagnose, null },
-    { "extra_checks", 0, 'E', 0, &extra_checks, null },
-    { "float_div", 0, 0, 'M', &strict_fl_div, null },
-    { "float_inf", 0, 0, 'B', &flpt_const_overflow_fail, null },
-    { "foralls", 0, 0, 'F', &do_foralls, null },
-    { "ignore", 0, 0, 0, &ignore_errors, null },
-    { "immediate", 'i', 0, 0, &output_immediately, null },
-    { "inlining", 0, 0, 'I', &do_inlining, null },
-    { "loopconsts", 0, 0, 'C', &do_loopconsts, null },
+    char *opt;
+    char on;
+    char off;
+    char sw;
+    int *flag;
+    char **value;
+} options[] = {
+    {"alloca", 0, 0, 'A', &do_alloca, null},
+    {"cc", 'c', 'g', 0, &cc_conventions, null},
+    {"diag", 'H', 0, 0, &diagnose, null},
+    {"extra_checks", 0, 'E', 0, &extra_checks, null},
+    {"float_div", 0, 0, 'M', &strict_fl_div, null},
+    {"float_inf", 0, 0, 'B', &flpt_const_overflow_fail, null},
+    {"foralls", 0, 0, 'F', &do_foralls, null},
+    {"ignore", 0, 0, 0, &ignore_errors, null},
+    {"immediate", 'i', 0, 0, &output_immediately, null},
+    {"inlining", 0, 0, 'I', &do_inlining, null},
+    {"loopconsts", 0, 0, 'C', &do_loopconsts, null},
 #if have_diagnostics
-    { "stab", 0, 0, 0, &diag_stab_override, null },
-    { "new_diag", 0, 0, 0, &diag_xdb_new_override, null },
-    { "old_diag", 0, 0, 0, &diag_xdb_old_override, null },
+    {"stab", 0, 0, 0, &diag_stab_override, null},
+    {"new_diag", 0, 0, 0, &diag_xdb_new_override, null},
+    {"old_diag", 0, 0, 0, &diag_xdb_old_override, null},
 #endif
-    { "opt", 'O', 'X', 0, &optimize, null },
-    { "options", 'o', 0, 0, &show_options, null },
-    { "peephole", 0, 0, 0, &do_peephole, null },
-    { "pic", 0, 0, 'D', &do_pic, null },
-    { "profile", 'P', 0, 0, &do_profile, null },
-    { "quit", 'Q', 0, 0, &do_quit, null },
-    { "report_version", 'Z', 0, 0, &report_tdf_versions, null },
-    { "round", 'r', 0, 'R', &round_after_flop, null },
-    { "separate_units", 'U', 0, 0, &do_sep_units, null },
-    { "special_fns", 0, 0, 0, &do_special_fns, null },
-    { "sub_params", 0, 0, 0, &do_sub_params, null },
+    {"opt", 'O', 'X', 0, &optimize, null},
+    {"options", 'o', 0, 0, &show_options, null},
+    {"peephole", 0, 0, 0, &do_peephole, null},
+    {"pic", 0, 0, 'D', &do_pic, null},
+    {"profile", 'P', 0, 0, &do_profile, null},
+    {"quit", 'Q', 0, 0, &do_quit, null},
+    {"report_version", 'Z', 0, 0, &report_tdf_versions, null},
+    {"round", 'r', 0, 'R', &round_after_flop, null},
+    {"separate_units", 'U', 0, 0, &do_sep_units, null},
+    {"special_fns", 0, 0, 0, &do_special_fns, null},
+    {"sub_params", 0, 0, 0, &do_sub_params, null},
 #ifdef EBUG
-    { "label", 'L', 0, 0, &seek_extern, &seek_extern_id },
-    { "line", 'l', 0, 0, &seek_line, &seek_line_id },
-    { "test", 'l', 0, 0, &do_test, null },
+    {"label", 'L', 0, 0, &seek_extern, &seek_extern_id},
+    {"line", 'l', 0, 0, &seek_line, &seek_line_id},
+    {"test", 'l', 0, 0, &do_test, null},
 #endif
-    { "unroll", 0, 0, 'U', &do_unroll, null },
-    { "version", 'V', 0, 0, &report_version, null },
-    { "write_strings", 0, 0, 'W', &dummy_option, null }
-} ;
+    {"unroll", 0, 0, 'U', &do_unroll, null},
+    {"version", 'V', 0, 0, &report_version, null},
+    {"write_strings", 0, 0, 'W', &dummy_option, null}
+};
 
 
 /*
@@ -275,107 +305,108 @@ struct {
 */
 
 int main
-    PROTO_N ( ( argc, argv ) )
-    PROTO_T ( int argc X char **argv )
+(int argc, char **argv)
 {
-    int a ;
-    char **p = null ;
-    char *input = null ;
-    char *output = null ;
+    int a;
+    char **p = null;
+    char *input = null;
+    char *output = null;
 
     /* Set up program name */
-    progname = basename ( argv [0] ) ;
+    progname = basename(argv[0]);
 
     /* Set default options */
-    diagnose = 0 ;
-    do_alloca = 1 ;
-    do_foralls = 1 ;
-    do_inlining = 1 ;
-    do_loopconsts = 1 ;
-    do_profile = 0 ;
-    do_special_fns = 1 ;
-    do_unroll = 1 ;
-    extra_checks = 1 ;
-    redo_structfns = 0 ;
+    diagnose = 0;
+    do_alloca = 1;
+    do_foralls = 1;
+    do_inlining = 1;
+    do_loopconsts = 1;
+    do_profile = 0;
+    do_special_fns = 1;
+    do_unroll = 1;
+    extra_checks = 1;
+    redo_structfns = 0;
 
     /* Process arguments */
-    for ( a = 1 ; a < argc ; a++ ) {
-	if ( p ) {
+    for (a = 1; a < argc; a++) {
+	if (p) {
 	    /* Set extra part of two part options */
-	    *p = argv [a] ;
-	    p = null ;
-	} else if ( argv [a][0] == '-' && argv [a][1] ) {
+	    *p = argv[a];
+	    p = null;
+	} else if (argv[a][0] == '-' && argv[a][1]) {
 	    /* Search option table */
-	    bool found = 0 ;
-	    char *s = argv [a] + 1 ;
-	    int i, n = array_size ( options ) ;
-	    if ( ( s [1] == '0' || s [1] == '1' ) && s [2] == 0 ) {
-		for ( i = 0 ; !found && i < n ; i++ ) {
-		    if ( *s == options [i].sw ) {
-			*( options [i].flag ) = ( s [1] - '0' ) ;
-			p = options [i].value ;
-			found = 1 ;
+	    bool found = 0;
+	    char *s = argv[a] + 1;
+	    int i, n = array_size(options);
+	    if ((s[1] == '0' || s[1] == '1') && s[2] == 0) {
+		for (i = 0; !found && i < n; i++) {
+		    if (*s == options[i].sw) {
+			*(options[i].flag) = (s[1] - '0');
+			p = options[i].value;
+			found = 1;
 		    }
 		}
-	    } else if ( s [1] ) {
-		bool b = 1 ;
-		if ( strncmp ( s, "no_", 3 ) == 0 ) {
-		    s += 3 ;
-		    b = 0 ;
+	    } else if (s[1]) {
+		bool b = 1;
+		if (strncmp(s, "no_", 3) == 0) {
+		    s + = 3;
+		    b = 0;
 		}
-		for ( i = 0 ; !found && i < n ; i++ ) {
-		    if ( eq ( s, options [i].opt ) ) {
-			*( options [i].flag ) = b ;
-			if ( b ) p = options [i].value ;
-			found = 1 ;
+		for (i = 0; !found && i < n; i++) {
+		    if (eq(s, options[i].opt)) {
+			*(options[i].flag) = b;
+			if (b)p = options[i].value;
+			found = 1;
 		    }
 		}
 	    } else {
-		for ( i = 0 ; !found && i < n ; i++ ) {
-		    if ( *s == options [i].on ) {
-			*( options [i].flag ) = 1 ;
-			p = options [i].value ;
-			found = 1 ;
-		    } else if ( *s == options [i].off ) {
-			*( options [i].flag ) = 0 ;
-			found = 1 ;
+		for (i = 0; !found && i < n; i++) {
+		    if (*s == options[i].on) {
+			*(options[i].flag) = 1;
+			p = options[i].value;
+			found = 1;
+		    } else if (*s == options[i].off) {
+			*(options[i].flag) = 0;
+			found = 1;
 		    }
 		}
 	    }
-	    if ( !found ) warning ( "Unknown option, %s", argv [a] ) ;
+	    if (!found) {
+		    warning("Unknown option, %s", argv[a]);
+	    }
 	} else {
 	    /* Set up input and output files */
-	    if ( input == null ) {
-		input = argv [a] ;
-	    } else if ( output == null ) {
-		output = argv [a] ;
+	    if (input == null) {
+		input = argv[a];
+	    } else if (output == null) {
+		output = argv[a];
 	    } else {
-		error ( "Too many arguments" ) ;
-		exit ( EXIT_FAILURE ) ;
+		error("Too many arguments");
+		exit(EXIT_FAILURE);
 	    }
 	}
     }
 
 #ifdef EBUG
     /* Deal with debugging options */
-    if ( seek_extern ) {
-	if ( is_local ( seek_extern_id ) ) {
-	    seek_label = 1 ;
-	    seek_label_no = atoi ( seek_extern_id + 1 ) ;
+    if (seek_extern) {
+	if (is_local(seek_extern_id)) {
+	    seek_label = 1;
+	    seek_label_no = atoi(seek_extern_id + 1);
 	}
     }
-    if ( seek_line ) seek_line_no = atoi ( seek_line_id ) ;
+    if (seek_line)seek_line_no = atoi(seek_line_id);
 #endif
 
     /* Report version if required */
-    if ( report_version ) {
+    if (report_version) {
 #ifdef NEXT
-	char *machine = "NeXT" ;
+	char *machine = "NeXT";
 #else
 #ifdef SUN
-	char *machine = "Sun/3" ;
+	char *machine = "Sun / 3";
 #else
-	char *machine = "HP" ;
+	char *machine = "HP";
 #endif
 #endif
 	fprintf(stderr, "DRA TDF translator (TDF version %d.%d)\n",
@@ -391,112 +422,136 @@ int main
 	fprintf(stderr," : installer compilation %s\n", __DATE__);
 #endif
 	fprintf(stderr,"release: %s\n",RELEASE);
-	fprintf ( stderr, ".\n" ) ;
+	fprintf(stderr, ".\n");
 #ifdef EBUG
-	fprintf ( stderr, "Last revised %s.\n", revdate ) ;
+	fprintf(stderr, "Last revised %s.\n", revdate);
 #endif
     }
-    if ( report_tdf_versions ) report_versions = 1 ;
+    if (report_tdf_versions) {
+	    report_versions = 1;
+    }
 
     /* Check on diagnostics */
-    if ( !have_diagnostics && diagnose ) {
-	error ( "Diagnostics not supported" ) ;
-	diagnose = 0 ;
+    if (!have_diagnostics && diagnose) {
+	error("Diagnostics not supported");
+	diagnose = 0;
     }
 
 #if have_diagnostics
-    diag_override = DIAG_UNKNOWN ;
-    if ( diag_stab_override ) diag_override = DIAG_STAB ;
-    if ( diag_xdb_new_override ) diag_override = DIAG_XDB_NEW ;
-    if ( diag_xdb_old_override ) diag_override = DIAG_XDB_OLD ;
+    diag_override = DIAG_UNKNOWN;
+    if (diag_stab_override) {
+	    diag_override = DIAG_STAB;
+    }
+    if (diag_xdb_new_override) {
+	    diag_override = DIAG_XDB_NEW;
+    }
+    if (diag_xdb_old_override) {
+	    diag_override = DIAG_XDB_OLD;
+    }
 #endif
 
     /* Switch off optimizations if required */
-    if ( diagnose || !optimize ) {
-	do_inlining = 0 ;
-	do_loopconsts = 0 ;
-	do_foralls = 0 ;
-	do_peephole = 0 ;
-	do_unroll = 0 ;
+    if (diagnose || !optimize) {
+	do_inlining = 0;
+	do_loopconsts = 0;
+	do_foralls = 0;
+	do_peephole = 0;
+	do_unroll = 0;
     }
 
     /* Show options if necessary */
-    if ( show_options ) {
-	int i, n = array_size ( options ) ;
-	for ( i = 0 ; i < n ; i++ ) {
-	    bool b = *( options [i].flag ) ;
-	    char **pv = options [i].value ;
-	    printf ( "%s = %s", options [i].opt, ( b ? "True" : "False" ) ) ;
-	    if ( pv && b ) printf ( " (%s)", *pv ) ;
-	    printf ( "\n" ) ;
+    if (show_options) {
+	int i, n = array_size(options);
+	for (i = 0; i < n; i++) {
+	    bool b = *(options[i].flag);
+	    char **pv = options[i].value;
+	    printf("%s = %s", options[i].opt,(b ? "True" : "False"));
+	    if (pv && b) {
+		    printf(" (%s)", *pv);
+	    }
+	    printf("\n");
 	}
     }
 
     /* Check on separate units */
-    if ( do_sep_units ) {
-	separate_units = 1 ;
+    if (do_sep_units) {
+	separate_units = 1;
 #if 0
-	current_alloc_size = first_alloc_size ;
+	current_alloc_size = first_alloc_size;
 #endif
     }
 
     do_pic = 0 ; /* TODO */
 
     /* Other options */
-    if ( do_pic ) PIC_code = 1 ;
-    if ( !extra_checks ) target_dbl_maxexp = 16384 ;
-    if ( do_quit ) exit ( EXIT_SUCCESS ) ;
+    if (do_pic) {
+	    PIC_code = 1;
+    }
+    
+    if (!extra_checks) {
+	    target_dbl_maxexp = 16384;
+    }
+
+    if (do_quit) {
+	    exit(EXIT_SUCCESS);
+    }
 
     /* Open input file */
-    if ( input == null ) {
-	error ( "Not enough arguments" ) ;
-	exit ( EXIT_FAILURE ) ;
+    if (input == null) {
+	error("Not enough arguments");
+	exit(EXIT_FAILURE);
     }
-    if ( !initreader ( input ) ) {
-	exit ( EXIT_FAILURE ) ;
+    if (!initreader(input)) {
+	exit(EXIT_FAILURE);
     }
-    /*open_input ( input ) ;*/
+    /* open_input(input);*/
 
     /* Set up alignment rules */
-    double_align = DBL_ALIGN ;
-    param_align = PARAM_ALIGN ;
-    stack_align = STACK_ALIGN ;
+    double_align = DBL_ALIGN;
+    param_align = PARAM_ALIGN;
+    stack_align = STACK_ALIGN;
 
-    MAX_BF_SIZE = ( cc_conventions ? MAX_BF_SIZE_CC : MAX_BF_SIZE_GCC ) ;
+    MAX_BF_SIZE = (cc_conventions ? MAX_BF_SIZE_CC : MAX_BF_SIZE_GCC);
 
     /* Call initialization routines */
-    top_def = null ;
-    init_flpt () ;
-    init_instructions () ;
+    top_def = null;
+    init_flpt();
+    init_instructions();
 #include "inits.h"
-    init_weights () ;
-    init_wheres () ;
+    init_weights();
+    init_wheres();
 
     /* Decode, optimize and process the input TDF */
-    open_output ( output ) ;
-    asm_comment ;
-    outs ( " TDF to 680x0, " ) ;
-    outs ( version_str ) ;
-    outs ( ", " ) ;
-    outs ( revision ) ;
-    outnl () ;
-    init_output () ;
-    area ( ptext ) ;
+    open_output(output);
+    asm_comment;
+    outs(" TDF to 680x0, ");
+    outs(version_str);
+    outs(", ");
+    outs(revision);
+    outnl();
+    init_output();
+    area(ptext);
 #if have_diagnostics
-    if ( diagnose ) diag_prologue () ;
+    if (diagnose) {
+	    diag_prologue();
+    }
 #endif
-    d_capsule () ;
+    d_capsule();
 #if have_diagnostics
-    if ( diagnose ) diag_epilogue () ;
+    if (diagnose) {
+	    diag_epilogue();
+    }
 #endif
 #ifdef asm_version
-    if ( normal_version ) {
-	asm_version ;
+    if (normal_version) {
+	asm_version;
     } else {
-	asm_version_aux ;
+	asm_version_aux;
     }
-    outnl () ;
+    outnl();
 #endif
-    if ( errors && !ignore_errors ) exit ( EXIT_FAILURE ) ;
-    return ( 0 ) ;
+    if (errors && !ignore_errors) {
+	    exit(EXIT_FAILURE);
+    }
+    return(0);
 }
