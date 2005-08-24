e02cdcd2a54e0d689182bb7dc564fb7de79a0cc0
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2005-08-24 17:16:24 +0000

    Clean up template files from OSSG and move a bit to ANSI C.
    svn path=/tendra/trunk/; revision=695

--- tendra/src/installers/common/templ/temp_fns.c
+++ tendra/src/installers/common/templ/temp_fns.c
@@ -1,6 +1,36 @@
 /*
+ * Copyright (c) 2002-2005 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1997
-    
+
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
     acting through the Defence Evaluation and Research Agency
@@ -9,18 +39,18 @@
     to other parties and amendment for any purpose not excluding
     product development provided that any such use et cetera
     shall be deemed to be acceptance of the following conditions:-
-    
+
         (1) Its Recipients shall ensure that this Notice is
         reproduced upon any copies or amended versions of it;
-    
+
         (2) Any amended version of it shall be clearly marked to
         show both the nature of and the organisation responsible
         for the relevant amendment or amendments;
-    
+
         (3) Its onward transfer from a recipient to another
         party shall be deemed to be that party's acceptance of
         these conditions;
-    
+
         (4) DERA gives no warranty or assurance as to its
         quality or suitability for any purpose and DERA accepts
         no liability whatsoever in relation to any use to which
@@ -38,67 +68,64 @@
 @if cons.params
 
 %SN f_%CN
-    PROTO_N ( (%
+    (%
 @loop cons.param
  %PN,%
 @end
-%b ) )
-    PROTO_T (%
+%b)
+(%
 @loop cons.param
  %PSN %PN X%
 @end
 %b)
 {
 @loop cons.param
-    UNUSED ( %PN ) ;
+    UNUSED(%PN);
 @end
-    return ( f_dummy_%SN ) ;
+    return(f_dummy_%SN);
 }
 @else
 
-%SN f_%CN ;
+%SN f_%CN;
 @endif
 @end
 
-%SN f_dummy_%SN ;
+%SN f_dummy_%SN;
 @else
 @if sort.option
 
-%SN no_%SN ;
+%SN no_%SN;
 
-%SN yes_%SN
-    PROTO_N ( ( elem ) )
-    PROTO_T ( %SSN elem )
+%SN
+yes_%SN(%SSN elem)
 {
-    UNUSED ( elem ) ;
-    return ( f_dummy_%SN ) ;
+    UNUSED(elem);
+    return(f_dummy_%SN);
 }
 @else
 
-%SN new_%SN
-    PROTO_N ( ( n ) )
-    PROTO_T ( int n )
+%SN
+new_%SN(int n)
 {
-    UNUSED ( n ) ;
-    return ( f_dummy_%SN ) ;
+    UNUSED(n);
+    return(f_dummy_%SN);
 }
 
-%SN add_%SN
-   PROTO_N ( ( list, elem, index ) )
-   PROTO_T ( %SN list X %SSN elem X int index )
+%SN
+add_%SN(%SN list, %SSN elem, int index)
 {
-    UNUSED ( list ) ;
-    UNUSED ( elem ) ;
-    UNUSED ( index ) ;
-    return ( f_dummy_%SN ) ;
+    UNUSED(list);
+    UNUSED(elem);
+    UNUSED(index);
+    return(f_dummy_%SN);
 }
 @endif
 @endif
 
-void init_%SN
-    PROTO_Z ()
+void
+init_%SN(void)
 {
-    return ;
+    return;
 }
 @endif
 @end
