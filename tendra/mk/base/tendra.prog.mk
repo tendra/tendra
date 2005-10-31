02f7e6cc63de718dd56844602daa5b6acfb919f6
Stefan Farfeleder <stefanf@freebsd.org>
2005-10-31 21:09:03 +0000

    Put the first part of tendra.prog.mk that sets a few variables into a new file tendra.var.mk (couldn't think of a better name).
    I want to .include a generated Makefile (with a path relative to ${OBJ_DIR})
    that sets the variable TL.  This case couldn't be handled by the old
    configuration because
    
    1) If I include tendra.prog.mk before that file, TL wasn't set yet and
       tendra.api.mk doesn't get included.
    
    2) I can't include the generated Makefile first because OBJ_DIR is set in
       tendra.prog.mk.
    
    With tendra.var.mk the following is possible and works:
    
    .include "tendra.var.mk"
    .include "${OBJ_DIR}/Makefile"
    .include "tendra.prog.mk"

--- tendra/mk/base/tendra.prog.mk
+++ tendra/mk/base/tendra.prog.mk
@@ -2,14 +2,7 @@
 
 CFLAGS+=${COPTS} ${DEBUG_FLAGS}
 
-# Calculate our object directory and obj prefix.
-OBJ_PREFIX?=   ${.CURDIR:C/(.*)\/src(.*)/\1\/obj/}
-OBJ_DIR=       ${.CURDIR:C/(.*)\/src(.*)/${OBJ_PREFIX}\/\2/}
-
-
-.if !defined(SRC_DIR)
-SRC_DIR=        ${.CURDIR:C/(.*)\/src.*/\1/}
-.endif
+.include "tendra.var.mk"
 
 .if exists (${SRC_DIR}/config.mk)
 HAVE_CONF=      yes
