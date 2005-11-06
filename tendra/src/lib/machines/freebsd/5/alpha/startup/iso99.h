3f0994be5300b5934e7f1984e40085a6179e73d4
Stefan Farfeleder <stefanf@freebsd.org>
2005-11-06 08:35:03 +0000

    The macro __WRONG_ISO99_STDINT_H is defined here because early FreeBSD 5.x versions failed to expose WINT_{MAX,MIN} in some cases.  It was fixed a long time ago, so use __WRONG_ISO99_STDINT_H only on those old systems.

--- tendra/src/lib/machines/freebsd/5/alpha/startup/iso99.h
+++ tendra/src/lib/machines/freebsd/5/alpha/startup/iso99.h
@@ -5,4 +5,8 @@
 #define	__STDC_VERSION__	199901
 #define	__WRONG_ISO99_FENV_H
 #define	__WRONG_ISO99_MATH_H
+
+#include <osreldate.h>
+#if __FreeBSD_version < 502113
 #define	__WRONG_ISO99_STDINT_H
+#endif
