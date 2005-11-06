f43d75a3aaa7a81155eb105debb8e8b21689b98a
Boris Popov <bp@freebsd.org>
2005-11-06 16:15:46 +0000

    Recover seriosly damaged formatting.

--- tendra/src/installers/common/reader/toktypes.h
+++ tendra/src/installers/common/reader/toktypes.h
@@ -55,20 +55,20 @@
  */
 
 
-#ifndef toktypes_key
-#define toktypes_key 1
+#ifndef TOKTYPES_INCLUDED
+#define	TOKTYPES_INCLUDED
 
 typedef tdf_pos bitstream;
 typedef tdf_pos bytestream;
 
-typedef struct sortname_t
-{int code;	/* code number for the sort - see sortmacs.h */
-	int result;	/* if code is TOKEN this is the code for the result */
-	struct sortname_list_t {struct sortname_t * elems;
-		int number;
+typedef struct sortname_t {
+	int		code;		/* code number for the sort - see sortmacs.h */
+	int		result;		/* if code is TOKEN this is the code for the result */
+	struct sortname_list_t {
+		 /* array of pars.number parameter sorts if the code is TOKEN */
+		struct sortname_t	*elems;
+		int					number;
 	} pars;
-	/* pars.elems is an array of pars.number parameter sorts 
-	 *	   if the code is TOKEN */
 } sortname;
 
 typedef struct sortname_list_t sortname_list;
@@ -102,7 +102,6 @@ typedef int tagdec_unit_list;
 typedef int tagdef_unit_list;
 typedef int linkinfo_props;
 
-
 typedef char usage;
 
 struct tok_define_t;
@@ -112,73 +111,67 @@ struct tok_define_t;
 #define LOCAL_TOKS 4
 
 typedef struct t_tag_con {
-	dec * namet;
-	exp e;
-	struct t_tag_con * rest;
+	dec		*namet;
+	exp		e;
+	struct t_tag_con *rest;
 } tag_con;
 
 typedef struct t_lab_con {
-	label namel;
-	exp e;
-	struct t_lab_con * rest;
+	label	namel;
+	exp		e;
+	struct t_lab_con *rest;
 } lab_con;
 
 
 typedef union tokval_u tokval;
 
 struct tok_define_t {
-	dec * * my_tagtab;		/* the tagtab current at the definition */
-	aldef * * my_altab;	/* the altab current at the definition */
-	exp * my_labtab;		/* the labtab current at the definition */
-	struct tok_define_t * * my_toktab;
-	/* the toktab current at the definition */
-	diag_tagdef * * my_diagtab;	/* OLD DIAGS */
-	/* the diagtab current at the definition */
-	dgtag_struct * * my_dgtab;		/* NEW DIAGS */
-	/* the dgtab current at the definition */
-	char * tok_name;	/* the identifier (if any) for the token */
-	struct tokformals_list_t	/* description of formal parameters */
-	{
-        sortname * par_sorts;	/* sorts of parameters */
-        int * par_names;	/* token index for parameters */
-        int number;		/* total number of parameters */
+	dec			**my_tagtab;		/* the tagtab current at the definition */
+	aldef		**my_altab;			/* the altab current at the definition */
+	exp			*my_labtab;			/* the labtab current at the definition */
+	struct tok_define_t **my_toktab;	/* the toktab current at the definition */
+	diag_tagdef	**my_diagtab;		/* OLD DIAGS, the diagtab current at the definition */
+	dgtag_struct **my_dgtab;		/* NEW DIAGS, the dgtab current at the definition */
+	char		*tok_name;			/* the identifier (if any) for the token */
+	struct tokformals_list_t {		/* description of formal parameters */
+		sortname	*par_sorts;		/* sorts of parameters */
+		int			*par_names;		/* token index for parameters */
+		int			number;			/* total number of parameters */
 	} params;
-	sortname tdsort;		/* sort of token result */
-	tdf_pos tdplace;		/* place to read definition from */
-	int unit_number;		/* number of unit in which defined */
-	union tokval_u tdvalue;	/* preserved value if valpresent */
-	int tok_index;		/* index number of this token */
-	int   tdtoken;		/* the token */
-	struct t_context * tok_context;     /* the context at the token definition */
-	char * signature; 
-	unsigned int re_evaluate :1;	/* needs to be reevaluated */
-	unsigned int defined : 1;
-	/* for diagnostic purposes, remove later */
-	unsigned int is_capsule_token :1;	/* it is a capsule level token */
-	unsigned int recursive :1;	/* used to check against recursion */
-	unsigned int valpresent :1;/* the value is constant and has been
-								*				   computed already */
-	unsigned int tok_special :1; /* locally defined token */
+	sortname	tdsort;				/* sort of token result */
+	tdf_pos		tdplace;			/* place to read definition from */
+	int			unit_number;		/* number of unit in which defined */
+	union tokval_u tdvalue;			/* preserved value if valpresent */
+	int			tok_index;			/* index number of this token */
+	int			tdtoken;			/* the token */
+	struct t_context *tok_context;	/* the context at the token definition */
+	char		*signature; 
+	unsigned int re_evaluate:1;		/* needs to be reevaluated */
+	unsigned int defined:1;			/* for diagnostic purposes, remove later */
+	unsigned int is_capsule_token:1;/* it is a capsule level token */
+	unsigned int recursive:1;		/* used to check against recursion */
+	unsigned int valpresent:1;		/* the value is constant and has been computed already */
+	unsigned int tok_special:1;		/* locally defined token */
 };
 
 
 /* struct holding a token definition */
-typedef struct tok_define_t  tok_define;
+typedef struct tok_define_t tok_define;
 
-typedef tok_define * token;
+typedef tok_define* token;
 
-typedef struct t_context{
-	tok_define loctoks[LOCAL_TOKS];    
-	tok_define * othertoks; /* most tokens have <=LOCAL_TOKS parameters and
-							 *				are not recursive so use of local space in
-							 *				apply_tok for 'tok_context' to avoid lots of
-							 *				mallocs ....*/ 
-	short no_toks;
-	short recursive;
-	tag_con * tags;	   /* ... don't know how many tag & labs defined in token
-						*			      so have to keep list; NB used only if recursive */
-	lab_con * labs;
-	struct t_context * outer;
+typedef struct t_context {
+	tok_define	loctoks[LOCAL_TOKS];    
+	tok_define	*othertoks; /* most tokens have <=LOCAL_TOKS parameters and
+							 * are not recursive so use of local space in
+							 * apply_tok for 'tok_context' to avoid lots of
+							 * mallocs ....*/ 
+	short		no_toks;
+	short		recursive;
+	tag_con		*tags;		 /* ... don't know how many tag & labs defined in token
+							  * so have to keep list; NB used only if recursive */
+	lab_con		*labs;
+	struct t_context *outer;
 } context;
 
 extern context * crt_context;
@@ -187,46 +180,41 @@ extern context * crt_context;
 typedef int taglink;
 typedef int allink;
 typedef int toklink;
-
-
 typedef int tokextern;
-
 typedef int tagextern;
 typedef int alextern;
-
 typedef int tokdec;
+typedef int tokdef;
 
-typedef struct tokformals_t
-{sortname sn;
-	int tk;
+typedef struct tokformals_t {
+	sortname	sn;
+	int			tk;
 } tokformals;	/* formal parameter during reading */
 
 typedef struct tokformals_list_t  tokformals_list;
 
-typedef int tokdef;
 
-typedef struct tagdec_t
-{tag tg;		/* identifying tag */
-	shape sha;		/* the shape of the tag */
-	access_option acc;	/* its access_options */
-	bool is_variable;	/* 1 for variable, 0 for identity */
-	bool is_common;	/* 1 for common_tagdec */
+typedef struct tagdec_t {
+	tag			tg;			/* identifying tag */
+	shape		sha;		/* the shape of the tag */
+	access_option acc;		/* its access_options */
+	bool		is_variable;/* 1 for variable, 0 for identity */
+	bool		is_common;	/* 1 for common_tagdec */
 } tagdec;
 
-typedef struct tagdef_t
-{
-	tag tg;		/* identifying tag */
-	exp def;		/* the associated exp definition */
-	bool var;		/* 1 for variable, 0 for identity */
-	bool is_common;	/* 1 for common_tagdef */
+typedef struct tagdef_t {
+	tag			tg;			/* identifying tag */
+	exp			def;		/* the associated exp definition */
+	bool		var;		/* 1 for variable, 0 for identity */
+	bool		is_common;	/* 1 for common_tagdef */
 } tagdef;
 
 typedef int al_tagdef;
 
 
-typedef struct tdfstring_list_t
-{int number;
-	tdfstring * elems;
+typedef struct tdfstring_list_t {
+	int			number;
+	tdfstring	*elems;
 } tdfstring_list;	/* number tdfstrings */
 
 typedef tdfstring tdfident;
@@ -235,28 +223,29 @@ typedef tdfstring_list tdfident_list;
 
 typedef tdfident_list unique;
 
-union external_u {unique u; tdfstring id;};
+union external_u {
+	unique		u;
+	tdfstring	id;
+};
 
-typedef struct external_t {union external_u ex; bool isstring;} external;
+typedef struct external_t {
+	union external_u ex;
+	bool		isstring;
+} external;
 
-typedef struct capsule_link_t
-{
-    char * id;	/* name */
-    tdfint n;	/* index */
+typedef struct capsule_link_t {
+	char		*id;	/* name */
+	tdfint		n;		/* index */
 } capsule_link;
 
-typedef struct capsule_link_list_t 
-{
-    int number;
-    capsule_link * members;
-    
+typedef struct capsule_link_list_t {
+	int			number;
+	capsule_link *members;
 } capsule_link_list;	/* number capsule_links */
 
-typedef struct tdfint_list_t 
-{
-    int number;
-    tdfint * members;
-    
+typedef struct tdfint_list_t {
+	int			number;
+	tdfint		*members;
 } tdfint_list;	/* number tdfints */
 
 /* dummy definitions */
@@ -278,10 +267,10 @@ typedef int al_tagdef_props;
 typedef int tokdef_bd;
 typedef int links_list;
 
-typedef struct capsule_frees_t
-{void * ptr;
-	struct capsule_frees_t * next;
+typedef struct capsule_frees_t {
+	void		*ptr;
+	struct capsule_frees_t *next;
 } capsule_frees;
 
 
-#endif
+#endif /* !TOKTYPES_INCLUDED */
