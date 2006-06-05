1062bfb736294760b5269436b95e21554aac5247
Eirik Nygaard <eirik@ngrd.no>
2006-06-05 19:46:29 +0000

    During the OSSG-removal some curly brackets were placed in wrong parts of apply_tok().
    svn path=/tendra/trunk/; revision=855

--- tendra/src/installers/common/reader/table_fns.c
+++ tendra/src/installers/common/reader/table_fns.c
@@ -270,220 +270,220 @@ apply_tok(token td, bitstream pars, int sortcode, tokval * actual_pars)
 			}
 			exit(EXIT_FAILURE);
 		}
+	}
 
-		{
-			/* number of parameters */
-			int npars = td->params.number;
-			/* to construct the bindings for this expansion */
-			context new_context;
-			context *old_context = crt_context;
+	{
+		/* number of parameters */
+		int npars = td->params.number;
+		/* to construct the bindings for this expansion */
+		context new_context;
+		context *old_context = crt_context;
 
-			tokval val;
-			/* to record the current place in the input stream */
-			place old_place;
+		tokval val;
+		/* to record the current place in the input stream */
+		place old_place;
 
-			tok_define *new_bindings;
-			int i, j;
-			dec **old_tagtab;
-			/* to remember the current tag table */
-			exp *old_labtab;
-			/* to remember the current label table */
-			tok_define **old_toktab;
-			/* to remember the current token table */
-			aldef **old_altab;
-			/* to remember the current alignment tag table */
-			diag_tagdef **old_diagtab;		/* OLD DIAGS */
-			dgtag_struct **old_dgtab;		/* NEW DIAGS */
+		tok_define *new_bindings;
+		int i, j;
+		dec **old_tagtab;
+		/* to remember the current tag table */
+		exp *old_labtab;
+		/* to remember the current label table */
+		tok_define **old_toktab;
+		/* to remember the current token table */
+		aldef **old_altab;
+		/* to remember the current alignment tag table */
+		diag_tagdef **old_diagtab;		/* OLD DIAGS */
+		dgtag_struct **old_dgtab;		/* NEW DIAGS */
 
-			/* now remember them */
-			old_tagtab = unit_ind_tags;
-			old_labtab = unit_labtab;
-			old_toktab = unit_ind_tokens;
-			old_altab = unit_ind_als;
-			old_diagtab = unit_ind_diagtags;	/* OLD DIAGS */
-			old_dgtab = unit_ind_dgtags;		/* NEW DIAGS */
+		/* now remember them */
+		old_tagtab = unit_ind_tags;
+		old_labtab = unit_labtab;
+		old_toktab = unit_ind_tokens;
+		old_altab = unit_ind_als;
+		old_diagtab = unit_ind_diagtags;	/* OLD DIAGS */
+		old_dgtab = unit_ind_dgtags;		/* NEW DIAGS */
 
-			new_context.no_toks = (short)npars;
-			nil_params.number = 0;
+		new_context.no_toks = (short)npars;
+		nil_params.number = 0;
 
-			if (td -> valpresent &&
-			    (td -> unit_number == crt_tagdef_unit_no)) {
-				/* if a value has already been computed (there
-				 * will be no parameters) */
+		if (td -> valpresent &&
+				(td -> unit_number == crt_tagdef_unit_no)) {
+			/* if a value has already been computed (there
+			 * will be no parameters) */
 
-				if (sortcode == f_exp.code) {
-					tokval v;
-					v.tk_exp = copy(td->tdvalue.tk_exp);
-					/* copy it if the result is an
-					 * expression since we may be going to
-					 * alter it */
-					return v;
-				} else  {
-					return td -> tdvalue;
-				}
-			}
-
-			/* remember the current input stream */
-			old_place = keep_place();
-
-			set_place(pars);
-
-			/* now set up the new parameter bindings */
-			if (npars > LOCAL_TOKS) {
-				new_context.othertoks =
-				    (tok_define *)xcalloc(npars - LOCAL_TOKS,
-							  sizeof(tok_define));
-			}
-
-			new_bindings = &new_context.loctoks[0];
-
-			for (j = 0; j < npars; ++j) {
-				/* read in the parameter values and bind them
-				 * to the formals */
-				sortname sn;
-				exp old_crt_repeat;  /* XX008 */
-				if (j >= LOCAL_TOKS) {
-					i = j - LOCAL_TOKS;
-					new_bindings = new_context.othertoks;
-				} else {
-					i = j;
-				}
-				sn = (td->params.par_sorts)[j];
-				new_bindings[i].tdsort = sn;
-				/* parameter sort */
-				new_bindings[i].tdtoken =
-				    (td->params.par_names)[j];
-				/* formal */
-				new_bindings[i].valpresent = 1;
-				/* the value is known */
-				new_bindings[i].unit_number =
-				    crt_tagdef_unit_no;
-				new_bindings[i].re_evaluate = 0;
-				/* and does not need re-evaluating */
-				new_bindings[i].params = nil_params;
-				/* it has no parameters itself */
-
-				/* XX008 */
-				if (sn.code == f_exp.code) {
-					old_crt_repeat = crt_repeat;
-					crt_repeat = nilexp;
-				} else {
-					SET(old_crt_repeat);
-				}
-				if (actual_pars != (tokval*)0) {
-					/* this is not used at present*/
-					new_bindings[i].tdvalue =
-					    actual_pars[j];
-				} else {
-					new_bindings[i].tdvalue =
-					    read_sort(sn.code);
-				}
-				/* read a parameter */
-				/* XX008 */
-				if (sn.code == f_exp.code) {
-					crt_repeat = old_crt_repeat;
-				}
-
-				new_bindings[i].defined = 1;
-				/* and say it is defined */
-				new_bindings[i].tok_special = 0;
-				/* and say it is not special */
-				new_bindings[i].recursive = (sn.code == TOKEN);
-				/* and say it is not recursive for simple
-				 * sorts */
-				new_bindings[i].tok_context = crt_context;
-			}
-
-			/* set up the place to read the definition */
-			set_place(td->tdplace);
-
-			new_context.recursive = td->recursive;
-			new_context.outer = td->tok_context;
-			new_context.tags = (tag_con *)0;
-			new_context.labs = (lab_con *)0;
-
-			crt_context = &new_context;
-
-			/* now set up the tables which belong to the place
-			 * where the token was defined */
-			unit_ind_tags = td->my_tagtab;
-			unit_labtab = td->my_labtab;
-			unit_ind_tokens = td->my_toktab;
-			unit_ind_als = td->my_altab;
-			unit_ind_diagtags = td->my_diagtab;	/* OLD DIAGS */
-			unit_ind_dgtags = td->my_dgtab;		/* NEW DIAGS */
-
-			/* read the body of the definition */
-			td->recursive = 1;  /* set up to detect recursion */
-			val = read_sort(sortcode);
-			td->recursive = new_context.recursive;
-
-			/* restore the place in the input stream */
-			set_place(old_place);
-
-			new_bindings = &new_context.loctoks[0];
-
-			/* kill off exps (they were copied) */
-			for (j = 0; j < npars; ++j) {
-				tok_define *q;
-				if (j >= LOCAL_TOKS) {
-					i = j - LOCAL_TOKS;
-					new_bindings = new_context.othertoks;
-				} else {
-					i = j;
-				}
-				q = &new_bindings[i];
-				if (q->tdsort.code == f_exp.code) {
-					exp ek = q->tdvalue.tk_exp;
-					kill_exp(ek, ek);
-				}
-			}
-
-			/* restore the old environment of tables */
-			crt_context = old_context;
-			unit_ind_tags = old_tagtab;
-			unit_labtab = old_labtab;
-			unit_ind_tokens = old_toktab;
-			unit_ind_als = old_altab;
-			unit_ind_diagtags = old_diagtab;	/* OLD DIAGS */
-			unit_ind_dgtags = old_dgtab;		/* NEW DIAGS */
-
-			if (!doing_aldefs && npars == 0 &&
-			    new_context.recursive == 0) {
-				/* if there were no parameters, record the
-				 * value for the next application of the
-				 * token */
-				td->valpresent = 1;
-				td->tdvalue = val;
-				if (sortcode == f_exp.code) {
-					tokval v;
-					v.tk_exp = copy(val.tk_exp);
-					/* if we are remembering it we must
-					 * copy, because the returned value
-					 * might be altered */
-					IGNORE hold(val.tk_exp);
-					return v;
-				}
-			} else {
-				if (npars > LOCAL_TOKS) {
-					xfree((void *)new_context.othertoks);
-					/* free the space used for parameter
-					 * binding */
-				}
-				while (new_context.tags != (tag_con *)0) {
-					tag_con *r = new_context.tags;
-					new_context.tags =
-					    new_context.tags->rest;
-					xfree((void *)r);
-				}
-				while (new_context.labs != (lab_con *)0) {
-					lab_con *r = new_context.labs;
-					new_context.labs =
-					    new_context.labs->rest;
-					xfree((void *)r);
-				}
-				return val;
+			if (sortcode == f_exp.code) {
+				tokval v;
+				v.tk_exp = copy(td->tdvalue.tk_exp);
+				/* copy it if the result is an
+				 * expression since we may be going to
+				 * alter it */
+				return v;
+			} else  {
+				return td -> tdvalue;
 			}
 		}
+
+		/* remember the current input stream */
+		old_place = keep_place();
+
+		set_place(pars);
+
+		/* now set up the new parameter bindings */
+		if (npars > LOCAL_TOKS) {
+			new_context.othertoks =
+				(tok_define *)xcalloc(npars - LOCAL_TOKS,
+						      sizeof(tok_define));
+		}
+
+		new_bindings = &new_context.loctoks[0];
+
+		for (j = 0; j < npars; ++j) {
+			/* read in the parameter values and bind them
+			 * to the formals */
+			sortname sn;
+			exp old_crt_repeat;  /* XX008 */
+			if (j >= LOCAL_TOKS) {
+				i = j - LOCAL_TOKS;
+				new_bindings = new_context.othertoks;
+			} else {
+				i = j;
+			}
+			sn = (td->params.par_sorts)[j];
+			new_bindings[i].tdsort = sn;
+			/* parameter sort */
+			new_bindings[i].tdtoken =
+				(td->params.par_names)[j];
+			/* formal */
+			new_bindings[i].valpresent = 1;
+			/* the value is known */
+			new_bindings[i].unit_number =
+				crt_tagdef_unit_no;
+			new_bindings[i].re_evaluate = 0;
+			/* and does not need re-evaluating */
+			new_bindings[i].params = nil_params;
+			/* it has no parameters itself */
+
+			/* XX008 */
+			if (sn.code == f_exp.code) {
+				old_crt_repeat = crt_repeat;
+				crt_repeat = nilexp;
+			} else {
+				SET(old_crt_repeat);
+			}
+			if (actual_pars != (tokval*)0) {
+				/* this is not used at present*/
+				new_bindings[i].tdvalue =
+					actual_pars[j];
+			} else {
+				new_bindings[i].tdvalue =
+					read_sort(sn.code);
+			}
+			/* read a parameter */
+			/* XX008 */
+			if (sn.code == f_exp.code) {
+				crt_repeat = old_crt_repeat;
+			}
+
+			new_bindings[i].defined = 1;
+			/* and say it is defined */
+			new_bindings[i].tok_special = 0;
+			/* and say it is not special */
+			new_bindings[i].recursive = (sn.code == TOKEN);
+			/* and say it is not recursive for simple
+			 * sorts */
+			new_bindings[i].tok_context = crt_context;
+		}
+
+		/* set up the place to read the definition */
+		set_place(td->tdplace);
+
+		new_context.recursive = td->recursive;
+		new_context.outer = td->tok_context;
+		new_context.tags = (tag_con *)0;
+		new_context.labs = (lab_con *)0;
+
+		crt_context = &new_context;
+
+		/* now set up the tables which belong to the place
+		 * where the token was defined */
+		unit_ind_tags = td->my_tagtab;
+		unit_labtab = td->my_labtab;
+		unit_ind_tokens = td->my_toktab;
+		unit_ind_als = td->my_altab;
+		unit_ind_diagtags = td->my_diagtab;	/* OLD DIAGS */
+		unit_ind_dgtags = td->my_dgtab;		/* NEW DIAGS */
+
+		/* read the body of the definition */
+		td->recursive = 1;  /* set up to detect recursion */
+		val = read_sort(sortcode);
+		td->recursive = new_context.recursive;
+
+		/* restore the place in the input stream */
+		set_place(old_place);
+
+		new_bindings = &new_context.loctoks[0];
+
+		/* kill off exps (they were copied) */
+		for (j = 0; j < npars; ++j) {
+			tok_define *q;
+			if (j >= LOCAL_TOKS) {
+				i = j - LOCAL_TOKS;
+				new_bindings = new_context.othertoks;
+			} else {
+				i = j;
+			}
+			q = &new_bindings[i];
+			if (q->tdsort.code == f_exp.code) {
+				exp ek = q->tdvalue.tk_exp;
+				kill_exp(ek, ek);
+			}
+		}
+
+		/* restore the old environment of tables */
+		crt_context = old_context;
+		unit_ind_tags = old_tagtab;
+		unit_labtab = old_labtab;
+		unit_ind_tokens = old_toktab;
+		unit_ind_als = old_altab;
+		unit_ind_diagtags = old_diagtab;	/* OLD DIAGS */
+		unit_ind_dgtags = old_dgtab;		/* NEW DIAGS */
+
+		if (!doing_aldefs && npars == 0 &&
+				new_context.recursive == 0) {
+			/* if there were no parameters, record the
+			 * value for the next application of the
+			 * token */
+			td->valpresent = 1;
+			td->tdvalue = val;
+			if (sortcode == f_exp.code) {
+				tokval v;
+				v.tk_exp = copy(val.tk_exp);
+				/* if we are remembering it we must
+				 * copy, because the returned value
+				 * might be altered */
+				IGNORE hold(val.tk_exp);
+				return v;
+			}
+		} else {
+			if (npars > LOCAL_TOKS) {
+				xfree((void *)new_context.othertoks);
+				/* free the space used for parameter
+				 * binding */
+			}
+		}
+		while (new_context.tags != (tag_con *)0) {
+			tag_con *r = new_context.tags;
+			new_context.tags =
+				new_context.tags->rest;
+			xfree((void *)r);
+		}
+		while (new_context.labs != (lab_con *)0) {
+			lab_con *r = new_context.labs;
+			new_context.labs =
+				new_context.labs->rest;
+			xfree((void *)r);
+		}
+		return val;
 	}
 }
