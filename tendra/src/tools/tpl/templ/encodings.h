01d0605b1419e34075e54ff57a4d070b2a54d068
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2006-07-08 18:46:21 +0000

    DeOSSG and clean up the template files a bit so that the resulting files will nicer too.
    svn path=/tendra/trunk/; revision=872

--- tendra/src/tools/tpl/templ/encodings.h
+++ tendra/src/tools/tpl/templ/encodings.h
@@ -1,4 +1,34 @@
 /*
+ * Copyright (c) 2002-2006 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1997
     
     This TenDRA(r) Computer Program is subject to Copyright
@@ -50,37 +80,38 @@
 @if cons.token
     TOK_APP (\
 @if cons.extends
-	out_extendable_int ( ( unsigned long ) e_%CN, ( unsigned ) %SB ),\
+	out_extendable_int((unsigned long)e_%CN, (unsigned)%SB),\
 @else
-	out_basic_int ( ( unsigned long ) e_%CN, ( unsigned ) %SB ),\
+	out_basic_int((unsigned long)e_%CN, (unsigned)%SB),\
 @endif
-	s_%SN, p0, p1 )
+	s_%SN, p0, p1)
 @else
 @if cons.cond
     TDF_COND (\
 @if cons.extends
-	out_extendable_int ( ( unsigned long ) e_%CN, ( unsigned ) %SB ),\
+	out_extendable_int((unsigned long)e_%CN, (unsigned)%SB),\
 @else
-	out_basic_int ( ( unsigned long ) e_%CN, ( unsigned ) %SB ),\
+	out_basic_int((unsigned long)e_%CN, (unsigned)%SB),\
 @endif
-	s_%SN, p0, p1, p2 )
+	s_%SN, p0, p1, p2)
 @else
     {\
 @if sort.basic
 @if cons.extends
-	out_extendable_int ( ( unsigned long ) e_%CN, ( unsigned ) %SB ) ;\
+	out_extendable_int((unsigned long)e_%CN, (unsigned)%SB);\
 @else
-	out_basic_int ( ( unsigned long ) e_%CN, ( unsigned ) %SB ) ;\
+	out_basic_int((unsigned long)e_%CN, (unsigned)%SB);\
 @endif
 @endif
 @loop cons.param
 @if param.sort.compound
-	o_%PSN ( p%PE ) ;\
+	o_%PSN(p%PE);\
 @else
-	p%PE ; ASSERT_SORT ( s_%PSN ) ;\
+	p%PE;\
+	ASSERT_SORT(s_%PSN);\
 @endif
 @end
-	SET_RSORT ( s_%SN ) ;\
+	SET_RSORT(s_%SN);\
     }
 @endif
 @endif
@@ -93,13 +124,13 @@
 
 @loop sort
 @if sort.option
-#define o_%SN( p )	%40tTDF_OPTION ( p, s_%SSN )
+#define o_%SN(p)	%40tTDF_OPTION(p, s_%SSN)
 @else
 @if sort.list
-#define o_%SN( p )	%40tTDF_LIST ( p, s_%SSN )
+#define o_%SN(p)	%40tTDF_LIST(p, s_%SSN)
 @else
 @if sort.slist
-#define o_%SN( p )	%40tTDF_SLIST ( p, s_%SSN )
+#define o_%SN(p)	%40tTDF_SLIST(p, s_%SSN)
 @endif
 @endif
 @endif
