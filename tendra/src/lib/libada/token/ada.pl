8d648f5dc426eb0ff88d8a73b1ee1bf301af99bd
Maxim Reznik <reznik@adacore.com>
2006-07-16 21:16:04 +0000

    More Ada related tokens for predefined unit Standard.

--- tendra/src/lib/libada/token/ada.pl
+++ tendra/src/lib/libada/token/ada.pl
@@ -1,24 +1,35 @@
-Tokdec Boolean#V : [] VARIETY;
+Tokdec Boolean.V : [] VARIETY;
+
+Tokdef Character.LOWER =
+  [] SIGNED_NAT
+
+  0;
+
+Tokdef Character.UPPER =
+  [] SIGNED_NAT
+
+  255;
+
 
 Tokdef COMPARE_INTEGER_VALUE =
   [Test  : NTEST,
    Left  : EXP,
    Right : EXP] EXP
 
-  ?{ ?(Left Test Right); 1(Boolean#V) | 0(Boolean#V) };
+  ?{ ?(Left Test Right); 1(Boolean.V) | 0(Boolean.V) };
 
 Tokdef BOOLEAN_JUMP =
   [Inv    : NAT,
    Value  : EXP,
    Target : LABEL] EXP
 
-  ?(Value == + Inv(Boolean#V) | Target);
+  ?(Value == + Inv(Boolean.V) | Target);
 
 Tokdef BOOLEAN_VALUE =
   [Target : LABEL,
    Value  : EXP] EXP
 
-  ?{ Value; 1(Boolean#V) | :Target: 0(Boolean#V) };
+  ?{ Value; 1(Boolean.V) | :Target: 0(Boolean.V) };
 
 Tokdef GENERIC_NAME =
   [Addr  : NAT,
@@ -26,22 +37,18 @@ Tokdef GENERIC_NAME =
    Name  : TAG,
    Type  : TOKEN [] SHAPE] EXP
 
-  EXP ? (+ Const(Boolean#V),
-    EXP ? (+ Addr(Boolean#V),
+  EXP ? (+ Const(Boolean.V),
+    EXP ? (+ Addr(Boolean.V),
              # "Can't get address of unaliased constant",
              Name),
-    EXP ? (+ Addr(Boolean#V),
+    EXP ? (+ Addr(Boolean.V),
              Name,
              * (Type) Name));
 
 Keep (COMPARE_INTEGER_VALUE,
       BOOLEAN_JUMP,
       BOOLEAN_VALUE,
-      GENERIC_NAME)
+      GENERIC_NAME,
+      Character.LOWER,
+      Character.UPPER)
 
-/*
-  Var i:Int = 0(Int)
-  {
-     return(i)
-  };
-*/
