21ab68399a2a1f7aa6c7d2b25e6b821b69287562
Maxim Reznik <reznik@adacore.com>
2006-07-16 21:18:49 +0000

    Ada Makefile fix

--- tendra/mk/base/tendra.ada.mk
+++ tendra/mk/base/tendra.ada.mk
@@ -3,12 +3,9 @@
 ADA_SRC?= ${ADA_PROG}.adb
 
 ${ADA_PROG}: ${DEPEND_SRC}
-	pwd
-	${BIN_ECHO} tendra.ada.mk  ${ADA_MAIN} => ${ADA_PROG}
 	${BIN_GNATMAKE} ${CFLAGS} -o ${.TARGET} ${ADA_SRC} -largs ${LDFLAGS}
 
 ${ADA_LIB}: ${DEPEND_SRC}
-	@${BIN_ECHO} "SRC:" ${.ALLSRC}
 	${BIN_GNATMAKE} ${CFLAGS} ${ADA_SRC}
 	${BIN_AR} cr ${.TARGET} *.o
 
