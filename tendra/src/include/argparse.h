6ee02893e372ee53361c4b8e4c92a0dd8d5f39f7
Stefan Farfeleder <stefanf@freebsd.org>
2006-07-30 13:58:59 +0000

    Redo the documentation for argparse.h.  Describe AP_OPT_* macros instead of the AT_* enumerators.  Don't describe stuff the user shouldn't care about.

--- tendra/src/include/argparse.h
+++ tendra/src/include/argparse.h
@@ -60,8 +60,6 @@
  *
  * original author: Steve Folkes <smf@hermes.mod.uk>
  *
- **** Commentary:
- *
  ***=== INTRODUCTION =========================================================
  *
  * This file specifies the interface to a command line argument parsing
@@ -81,153 +79,145 @@
  * one where the data associated with it is part of the option string, and not
  * in a following word).
  *
- ***=== TYPES ================================================================
- *
- ** Type:	ArgTypeT
- ** Repr:	enum {AT_SWITCH, AT_NEG_SWITCH, AT_PROC_SWITCH, AT_IMMEDIATE,
- *		      AT_EITHER, AT_FOLLOWING, AT_EMPTY, AT_FOLLOWING2, AT_FOLLOWING3,
- *		      AT_SET, AT_RESET}
- *
- * This is the type of the an option.  The constants have the following
- * meanings:
- *
- *	AT_SWITCH
- *
- * This is the type of a switch option.  The procedure value should be null.
- * The closure should be a pointer to a boolean.  If the option character is a
- * '-' then the boolean is set.  If the option character is '+' then the
- * boolean is reset.
- *
- *	AT_NEG_SWITCH
- *
- * This is the type of a negated switch option. The procedure value should be
- * null. The closure should be a pointer to a boolean.  If the option
- * character is a '-' then the boolean is reset.  If the option character is
- * '+' then the boolean is set.
- *
- *	AT_PROC_SWITCH
- *
- * This is the type of a switch option that needs some extra work to be
- * performed.  The procedure will be called with the option that was selected,
- * the error closure, the option closure, and a boolean (true if the option
- * character is '-', false otherwise).
- *
- *	AT_IMMEDIATE
- *
- * This is the type of an option with one immediate argument.  No shortest
- * prefix matching will be used for immediate arguments.  The procedure will
- * be called with the option that was selected, the error closure, the option
- * closure, and the argument to the option.  This type of argument should
- * appear at the end of the argument list that is passed to the
- * ``arg_parse_arguments'' function, as it may cause problems with shortest
- * prefix matching otherwise.
- *
- *	AT_EITHER
- *
- * This is the type of an option with one argument. If an immediate argument
- * exists, then that is used.  If no immediate argument exists, then the
- * following argument is used.  No shortest prefix matching will be used for
- * either arguments.  The procedure will be called with the option that was
- * selected, the error closure, the option closure, and the argument to the
- * option.  This type of argument should appear at the end of the argument
- * list that is passed to the ``arg_parse_arguments'' function, as it may
- * cause problems with shortest prefix matching otherwise.
- *
- *	AT_FOLLOWING
- *
- * This is the type of an option with one following argument.  The procedure
- * will be called with the option that was selected, the error closure, the
- * option closure, and the argument to the option.
- *
- *	AT_EMPTY
- *
- * This is the type of an option with no argument.  The procedure will be
- * called with the option that was selected, the error closure, and the option
- * closure.
- *
- *	AT_FOLLOWING2
- *
- * This is the type of an option with two following arguments.  The procedure
- * will be called with the option that was selected, the error closure, the
- * option closure, and the arguments to the option.
- *
- *	AT_FOLLOWING3
- *
- * This is the type of an option with three following arguments.  The
- * procedure will be called with the option that was selected, the error
- * closure, the option closure, and the arguments to the option.
- *
- *  AT_SET
- *
- * This is the type of an option with no argument.  The boolean variable 
- * passed as closure will be set to true.
- *
- *  AT_RESET
- *
- * This is the type of an option with no argument.  The boolean variable 
- * passed as closure will be set to false.
- *
- * Note that if a matched option is a short option, then the current position
- * is passed to the option handling procedure as the option name.  For long
- * options, the entire option string is passed.  If the handler is interested
- * in the option name, it should check the start of this string: if it is '-'
- * or '+' it should be a long option; otherwise it should be a short option.
- *
- ** Type:	ArgProcP
- ** Repr:	void (*) (char *, void *, ...)
- *
- * This is the type of a procedure to be called to parse a complex option.
- * Because of union initialisation problems, the latter arguments of this
- * function are untyped.
- *
+ ***=== TYPES =================================================================
+
  ** Type:	ArgListT
  ** Repr:	<private>
  *
  * This is the type of an entry in an option list.  A vector of such entries
  * should be passed to the ``arg_parse_arguments'' function, with the last
- * entry being the ``ARG_PARSE_END_LIST'' macro.  Each entry has the following
- * fields: a name field (the name of the option without the leading '--' or
- * '++'), a short name field (a character), a type field (the type of the
- * option), a procedure field (the procedure to handle the option), a closure
- * field (a pointer to some arbritary data used by the procedure), and a
- * description field (the name of a named string that describes how the option
- * is used - see the file "error.h" for more information on named strings).
- * The named strings used in the description fields should themselves be
- * interned before the ``arg_parse_arguments'' function is called.
+ * entry being the ``AP_OPT_EOL'' macro.
  *
  * A typical argument list definition would be something like the following:
  *
- *	static ArgListT arg_list [] = {
- *	    {
- *		"option name", 'o', AT_PROC_SWITCH, (ArgProcP) arg_proc,
- *		NULL,
- *		{ "option description name" }
- *	    }, ARG_PARSE_END_LIST
+ *	ArgListT arg_list [] = {
+ *		AP_OPT_PROC_SW(option_name, 'p', NULL, "option name", arg_proc),
+ *	    AP_OPT_EOL
  *	};
  *
  * If an option has only a short form, then the name should be NULL,
  * if it has only a long form, then the character should be '\0'.  It is
  * illegal for an option to have neither a long form or a short form.
  *
+ ***=== MACROS ================================================================
+ *
+ * The following macros expand to entries for an ArgListT array as shown
+ * above.  The description name is the identifier used in the message
+ * catalogue to look up the option description, the short name is the
+ * character used for a short option (eg. 'x' for -x), the long name is the
+ * string used for long options (eg. "xyz" for --xyz), the handler function
+ * is a function that is called if the specified option is matched and the
+ * closure is an arbitrary user pointer which is passed as context to the
+ * invoked handler function.
+ *
+ *
+ *	AP_OPT_SWITCH
+ *
+ * This is an entry for a switch option.  It accepts a description name, a
+ * short name, a long name and a pointer to a boolean.  If the option
+ * character is a '-' then the boolean is set.  If the option character is
+ * '+' then the boolean is reset.
+ *
+ *	AP_OPT_NEG_SW
+ *
+ * This is an entry for a negated switch option.  It accepts a description
+ * name, a short name, a long name and a pointer to a boolean.  If the
+ * option character is a '-' then the boolean is reset.  If the option
+ * character is '+' then the boolean is set.
+ *
+ *	AP_OPT_PROC_SW
+ *
+ * This is an entry for a switch option that needs some extra work to be
+ * performed.  It accepts a description name, a short name, a long name and
+ * a handler function.  The type of the passed function must be void
+ * (char *, void *, BoolT).  It will be called with the option that was
+ * selected, the option closure, and a boolean (true if the option character
+ * is '-', false otherwise).
+ *
+ *	AP_OPT_IMMEDIATE
+ *
+ * This is an entry for an option with one immediate argument.  It accepts a
+ * description name, a short name, a long name and a handler function.  No
+ * shortest prefix matching will be used for immediate arguments.  The type
+ * of the passed function must be void (char *, void *, char *).  It will be
+ * called with the option that was selected, the option closure, and the
+ * argument to the option.  This type of argument should appear at the end
+ * of the argument list that is passed to the ``arg_parse_arguments''
+ * function, as it may cause problems with shortest prefix matching
+ * otherwise.
+ *
+ *	AP_OPT_EITHER
+ *
+ * This is an entry for an option with one argument.  It accepts a
+ * description name, a short name, a long name and a handler function.  If an
+ * immediate argument exists, then that is used.  If no immediate argument
+ * exists, then the following argument is used.  No shortest prefix matching
+ * will be used for either arguments.  The type of the passed function must
+ * be void (char *, void *, char *).  It will be called with the option that
+ * was selected, the option closure, and the argument to the option.  This
+ * type of argument should appear at the end of the argument list that is
+ * passed to the ``arg_parse_arguments'' function, as it may cause problems
+ * with shortest prefix matching otherwise.
+ *
+ *	AP_OPT_FOLLOWING
+ *
+ * This is an entry for an option with one following argument.  It accepts a
+ * description name, a short name, a long name and a handler function.  The
+ * type of the passed function must be void (char *, void *, char *).  It
+ * will be called with the option that was selected, the option closure, and
+ * the argument to the option.
+ *
+ *	AP_OPT_EMPTY
+ *
+ * This is an entry for an option with no argument.  It accepts a
+ * description name, a short name, a long name and a handler function.  The
+ * type of the passed function must be void (char *, void *).  It will be
+ * called with the option that was selected and the option closure.
+ *
+ *	AP_OPT_FOLLOWING2
+ *
+ * This is an entry for an option with two following arguments.  It accepts
+ * a description name, a short name, a long name and a handler function.
+ * The type of the passed function must be void (char *, void *, char *,
+ * char *).  It will be called with the option that was selected, the option
+ * closure, and the arguments to the option.
+ *
+ *	AP_OPT_FOLLOWING3
+ *
+ * This is an entry for an option with three following arguments.  It
+ * accepts a description name, a short name, a long name and a function
+ * pointer.  The type of the passed function must be void (char *, void *,
+ * char *, char *, char *).  It will be called with the option that was
+ * selected, the option closure, and the arguments to the option.
+ *
+ * 	AP_OPT_SET
+ *
+ * This is an entry for an option with no argument.  It accepts a
+ * description name, a short name, a long name and a pointer to a boolean.
+ * The boolean variable will be set to true if the option is matched.
+ *
+ *	AP_OPT_RESET
+ *
+ * This is an entry for an option with no argument.  It accepts a
+ * description name, a short name, a long name and a pointer to a boolean.
+ * The boolean variable will be set to false if the option is matched.
+ *
+ *	AP_OPT_EOL
+ *
+ * This macro should be used to terminate an option list.
+ *
+ *
  ***=== FUNCTIONS ============================================================
  *
  ** Function: arg_parse_arguments
  *
- * This function does the argument parsing.  The arg_list argument should
- * specify the valid options for the program.  The usage estring should be a
- * named string, whose content is a summary of the program's usage.  The argc
- * and argv arguments should specify the actual arguments passed to the
- * program.  The parsing starts from the first argument, so the first element
+ * This function does the argument parsing.  The first argument should
+ * specify the valid options for the program.  The following two arguments
+ * should specify the actual arguments passed to the program (argc and
+ * argv).  The parsing starts from the first argument, so the first element
  * of the program's argument list (the program name) should not be passed to
  * this function.  The function returns the number of elements of the list
  * that it parsed.
- *
- ***=== MACROS ===============================================================
- *
- ** Macro:	ARG_PARSE_END_LIST
- ** Exceptions:
- *
- * This macro should be used to terminate an option list.
  */
 
 
