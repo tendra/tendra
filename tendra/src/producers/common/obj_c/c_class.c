e2d146d94288527dcc54d657df813f018ec94856
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2006-09-01 19:37:05 +0000

    Clean up from OSSG C and move to ANSI C.
    svn path=/tendra/trunk/; revision=919

--- tendra/src/producers/common/obj_c/c_class.c
+++ tendra/src/producers/common/obj_c/c_class.c
@@ -1,6 +1,36 @@
 /*
+ * Copyright (c) 2002-2006 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
     		 Crown Copyright (c) 1997
-    
+
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
     acting through the Defence Evaluation and Research Agency
@@ -9,18 +39,18 @@
     to other parties and amendment for any purpose not excluding
     product development provided that any such use et cetera
     shall be deemed to be acceptance of the following conditions:-
-    
+
         (1) Its Recipients shall ensure that this Notice is
         reproduced upon any copies or amended versions of it;
-    
+
         (2) Any amended version of it shall be clearly marked to
         show both the nature of and the organisation responsible
         for the relevant amendment or amendments;
-    
+
         (3) Its onward transfer from a recipient to another
         party shall be deemed to be that party's acceptance of
         these conditions;
-    
+
         (4) DERA gives no warranty or assurance as to its
         quality or suitability for any purpose and DERA accepts
         no liability whatsoever in relation to any use to which
@@ -44,7 +74,7 @@
 */
 
 #if c_class_SPECIFICATION
-FAIL_COMPILER ( Implementation functions compiled with token specifications )
+FAIL_COMPILER(Implementation functions compiled with token specifications)
 #endif
 
 
@@ -58,18 +88,18 @@ FAIL_COMPILER ( Implementation functions compiled with token specifications )
 #define SMALL_BLOCK		24
 #define ALLOC_BLOCK		2048
 
-#if ( c_class_GEN_MAX > SMALL_BLOCK )
-FAIL_COMPILER ( Free block array is too small )
+#if (c_class_GEN_MAX > SMALL_BLOCK)
+FAIL_COMPILER(Free block array is too small)
 #endif
 
-static c_class *free_c_classes = NULL ;
-static unsigned free_c_classes_left = 0 ;
-unsigned total_c_classes = 0 ;
+static c_class *free_c_classes = NULL;
+static unsigned free_c_classes_left = 0;
+unsigned total_c_classes = 0;
 
-static c_class *free_c_class_array [ SMALL_BLOCK ] = {
+static c_class *free_c_class_array[SMALL_BLOCK] = {
     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
-} ;
+};
 
 
 /*
@@ -80,32 +110,35 @@ static c_class *free_c_class_array [ SMALL_BLOCK ] = {
     c_class list.
 */
 
-c_class *gen_c_class
-    PROTO_N ( ( sz ) )
-    PROTO_T ( unsigned sz )
+c_class *
+gen_c_class(unsigned sz)
 {
-    c_class *p ;
-    unsigned n = sz ;
-    if ( n < SMALL_BLOCK ) {
-	/* Allocate from small block array */
-	if ( n == 0 ) return ( NULL ) ;
-	p = free_c_class_array [n] ;
-	if ( p ) {
-	    free_c_class_array [n] = TAIL_list ( p ) ;
-	    return ( p ) ;
+	c_class *p;
+	unsigned n = sz;
+	if (n < SMALL_BLOCK) {
+		/* Allocate from small block array */
+		if (n == 0) {
+			return(NULL);
+		}
+		p = free_c_class_array[n];
+		if (p) {
+			free_c_class_array[n] = TAIL_list(p);
+			return(p);
+		}
 	}
-    }
-    if ( n > free_c_classes_left ) {
-	/* Allocate new blocks */
-	unsigned m = ALLOC_BLOCK ;
-	if ( n > m ) m = n ;
-	free_c_classes = xmalloc_nof ( c_class, m ) ;
-	free_c_classes_left = m ;
-    }
-    p = free_c_classes ;
-    free_c_classes += n ;
-    free_c_classes_left -= n ;
-    return ( p ) ;
+	if (n > free_c_classes_left) {
+		/* Allocate new blocks */
+		unsigned m = ALLOC_BLOCK;
+		if (n > m) {
+			m = n;
+		}
+		free_c_classes = xmalloc_nof(c_class, m);
+		free_c_classes_left = m;
+	}
+	p = free_c_classes;
+	free_c_classes += n;
+	free_c_classes_left -= n;
+	return(p);
 }
 
 
@@ -118,14 +151,14 @@ c_class *gen_c_class
 */
 
 #ifdef RUNTIME
-#define clean_c_class( P, Z )\
+#define clean_c_class(P, Z)\
     {\
-	ASSERT ( TYPEID ( P ) != TYPEID_free ) ;\
-	TYPEID ( P ) = TYPEID_free ;\
-	total_c_classes -= ( Z ) ;\
+	ASSERT(TYPEID(P) != TYPEID_free);\
+	TYPEID(P) = TYPEID_free ;\
+	total_c_classes -= (Z);\
     }
 #else
-#define clean_c_class( P, Z )	/* empty */
+#define clean_c_class(P, Z)	/* empty */
 #endif
 
 
@@ -138,37 +171,40 @@ c_class *gen_c_class
 
 #ifdef RUNTIME
 
-c_class *debug_c_class
-    PROTO_N ( ( sz, t ) )
-    PROTO_T ( unsigned sz X unsigned t )
+c_class *
+debug_c_class(unsigned sz, unsigned t)
 {
-    c_class *p ;
-    unsigned n = sz ;
-    total_c_classes += n ;
-    if ( n < SMALL_BLOCK ) {
-	/* Allocate from small block array */
-	if ( n == 0 ) return ( NULL ) ;
-	p = free_c_class_array [n] ;
-	if ( p ) {
-	    free_c_class_array [n] = TAIL_list ( p ) ;
-	    ASSERT ( TYPEID ( p ) == TYPEID_free ) ;
-	    TYPEID ( p ) = t ;
-	    return ( p ) ;
+	c_class *p;
+	unsigned n = sz;
+	total_c_classes += n;
+	if (n < SMALL_BLOCK) {
+		/* Allocate from small block array */
+		if (n == 0) {
+			return(NULL);
+		}
+		p = free_c_class_array[n];
+		if (p) {
+			free_c_class_array[n] = TAIL_list(p);
+			ASSERT(TYPEID(p) == TYPEID_free);
+			TYPEID(p) = t;
+			return(p);
+		}
 	}
-    }
-    n += 1 ;
-    if ( n > free_c_classes_left ) {
-	/* Allocate new blocks */
-	unsigned m = ALLOC_BLOCK ;
-	if ( n > m ) m = n ;
-	free_c_classes = xmalloc_nof ( c_class, m ) ;
-	free_c_classes_left = m ;
-    }
-    p = free_c_classes + 1 ;
-    TYPEID ( p ) = t ;
-    free_c_classes += n ;
-    free_c_classes_left -= n ;
-    return ( p ) ;
+	n += 1;
+	if (n > free_c_classes_left) {
+		/* Allocate new blocks */
+		unsigned m = ALLOC_BLOCK;
+		if (n > m) {
+			m = n;
+		}
+		free_c_classes = xmalloc_nof(c_class, m);
+		free_c_classes_left = m;
+	}
+	p = free_c_classes + 1;
+	TYPEID(p) = t;
+	free_c_classes += n;
+	free_c_classes_left -= n;
+	return(p);
 }
 
 #endif /* RUNTIME */
@@ -180,19 +216,20 @@ c_class *debug_c_class
     This routine destroys the block of c_classes p of size sz.
 */
 
-void destroy_c_class
-    PROTO_N ( ( p, sz ) )
-    PROTO_T ( c_class *p X unsigned sz )
+void
+destroy_c_class(c_class *p, unsigned sz)
 {
-    if ( p ) {
-	unsigned n = sz ;
-	c_class **r = free_c_class_array ;
-	if ( n < SMALL_BLOCK ) r += n ;
-	clean_c_class ( p, n ) ;
-	TAIL_list ( p ) = *r ;
-	*r = p ;
-    }
-    return ;
+	if (p) {
+		unsigned n = sz;
+		c_class **r = free_c_class_array;
+		if (n < SMALL_BLOCK) {
+			r += n;
+		}
+		clean_c_class(p, n);
+		TAIL_list(p) = *r;
+		*r = p;
+	}
+	return;
 }
 
 
@@ -202,13 +239,12 @@ void destroy_c_class
     This routine is a dummy destructor which does nothing.
 */
 
-void dummy_destroy_c_class
-    PROTO_N ( ( p, sz ) )
-    PROTO_T ( c_class *p X unsigned sz )
+void
+dummy_destroy_c_class(c_class *p, unsigned sz)
 {
-    UNUSED ( p ) ;
-    UNUSED ( sz ) ;
-    return ;
+	UNUSED(p);
+	UNUSED(sz);
+	return;
 }
 
 
@@ -219,24 +255,25 @@ void dummy_destroy_c_class
     The list is added to the appropriate entry of the free c_class array.
 */
 
-void destroy_c_class_list
-    PROTO_N ( ( p, sz ) )
-    PROTO_T ( c_class *p X unsigned sz )
+void
+destroy_c_class_list(c_class *p, unsigned sz)
 {
-    if ( p ) {
-	c_class *q = p ;
-	unsigned n = sz + 1 ;
-	c_class **r = free_c_class_array ;
-	if ( n < SMALL_BLOCK ) r += n ;
-	while ( TAIL_list ( p ) ) {
-	    clean_c_class ( p, n ) ;
-	    p = TAIL_list ( p ) ;
+	if (p) {
+		c_class *q = p;
+		unsigned n = sz + 1;
+		c_class **r = free_c_class_array;
+		if (n < SMALL_BLOCK) {
+			r += n;
+		}
+		while (TAIL_list(p)) {
+			clean_c_class(p, n);
+			p = TAIL_list(p);
+		}
+		clean_c_class(p, n);
+		TAIL_list(p) = *r;
+		*r = q;
 	}
-	clean_c_class ( p, n ) ;
-	TAIL_list ( p ) = *r ;
-	*r = q ;
-    }
-    return ;
+	return;
 }
 
 
@@ -246,17 +283,16 @@ void destroy_c_class_list
     This routine calculates the length of the list p.
 */
 
-unsigned length_c_class_list
-    PROTO_N ( ( p ) )
-    PROTO_T ( c_class *p )
+unsigned
+length_c_class_list(c_class *p)
 {
-    unsigned n = 0 ;
-    c_class *q = p ;
-    while ( q ) {
-	n++ ;
-	q = TAIL_list ( q ) ;
-    }
-    return ( n ) ;
+	unsigned n = 0;
+	c_class *q = p;
+	while (q) {
+		n++;
+		q = TAIL_list(q);
+	}
+	return(n);
 }
 
 
@@ -266,19 +302,18 @@ unsigned length_c_class_list
     This routine reverses the order of the list p.
 */
 
-c_class *reverse_c_class_list
-    PROTO_N ( ( p ) )
-    PROTO_T ( c_class *p )
+c_class *
+reverse_c_class_list(c_class *p)
 {
-    c_class *r = NULL ;
-    c_class *q = p ;
-    while ( q ) {
-	c_class *nq = TAIL_list ( q ) ;
-	TAIL_list ( q ) = r ;
-	r = q ;
-	q = nq ;
-    }
-    return ( r ) ;
+	c_class *r = NULL;
+	c_class *q = p;
+	while (q) {
+		c_class *nq = TAIL_list(q);
+		TAIL_list(q) = r;
+		r = q;
+		q = nq;
+	}
+	return(r);
 }
 
 
@@ -288,15 +323,18 @@ c_class *reverse_c_class_list
     This routine appends the lists of c_class blocks p and q.
 */
 
-c_class *append_c_class_list
-    PROTO_N ( ( p, q ) )
-    PROTO_T ( c_class *p X c_class *q )
+c_class *
+append_c_class_list(c_class *p, c_class *q)
 {
-    c_class *r = p ;
-    if ( r == NULL ) return ( q ) ;
-    while ( TAIL_list ( r ) ) r = TAIL_list ( r ) ;
-    TAIL_list ( r ) = q ;
-    return ( p ) ;
+	c_class *r = p;
+	if (r == NULL) {
+		return(q);
+	}
+	while (TAIL_list(r)) {
+		r = TAIL_list(r);
+	}
+	TAIL_list(r) = q;
+	return(p);
 }
 
 
@@ -306,61 +344,63 @@ c_class *append_c_class_list
     This routine returns the last member of the list of c_class blocks p.
 */
 
-c_class *end_c_class_list
-    PROTO_N ( ( p ) )
-    PROTO_T ( c_class *p )
+c_class *
+end_c_class_list(c_class *p)
 {
-    c_class *r = p ;
-    if ( r == NULL ) return ( NULL ) ;
-    while ( TAIL_list ( r ) ) r = TAIL_list ( r ) ;
-    return ( r ) ;
+	c_class *r = p;
+	if (r == NULL) {
+		return(NULL);
+	}
+	while (TAIL_list(r)) {
+		r = TAIL_list(r);
+	}
+	return(r);
 }
 
 
 /*
-    GENERIC EMPTY VECTOR
+   GENERIC EMPTY VECTOR
 
-    This c_class represents the generic empty vector.  It is only defined
-    if vector operations have been enabled.  Note that the element field
-    of a vector is not be NULL, even if the vector is empty.
-*/
+   This c_class represents the generic empty vector.  It is only defined
+   if vector operations have been enabled.  Note that the element field
+   of a vector is not be NULL, even if the vector is empty.
+ */
 
 #ifdef VEC
-static c_class dummy_elem ;
-c_class_VEC empty_c_class_vec = { 0, { &dummy_elem, &dummy_elem } } ;
+static c_class dummy_elem;
+c_class_VEC empty_c_class_vec = {0, {&dummy_elem, &dummy_elem}};
 #endif
 
 
 #ifdef c_class_IO_ROUTINES
 
 /*
-    ALIASING VARIABLES
+   ALIASING VARIABLES
 
-    These variables give respectively the current alias number and the
-    list of all aliases.
-*/
+   These variables give respectively the current alias number and the
+   list of all aliases.
+ */
 
-unsigned crt_c_class_alias = 0 ;
-static c_class *crt_alias_list = NULL ;
+unsigned crt_c_class_alias = 0;
+static c_class *crt_alias_list = NULL;
 
 
 /*
-    SET AN ALIAS
+   SET AN ALIAS
 
-    This routine sets up an alias of p to n.
-*/
+   This routine sets up an alias of p to n.
+ */
 
-void set_c_class_alias
-    PROTO_N ( ( p, n ) )
-    PROTO_T ( c_class *p X unsigned n )
+void
+set_c_class_alias(c_class *p, unsigned n)
 {
-    c_class *q = GEN_c_class ( 2, TYPEID_list ) ;
-    TAIL_list ( q ) = crt_alias_list ;
-    HEAD_list ( q )->ag_ptr = p ;
-    ASSERT ( p != NULL ) ;
-    p->ag_tag = n ;
-    crt_alias_list = q ;
-    return ;
+	c_class *q = GEN_c_class(2, TYPEID_list);
+	TAIL_list(q) = crt_alias_list;
+	HEAD_list(q) ->ag_ptr = p;
+	ASSERT(p != NULL);
+	p->ag_tag = n;
+	crt_alias_list = q;
+	return;
 }
 
 
@@ -370,18 +410,19 @@ void set_c_class_alias
     This routine searches for alias number n.
 */
 
-c_class *find_c_class_alias
-    PROTO_N ( ( n ) )
-    PROTO_T ( unsigned n )
+c_class *
+find_c_class_alias(unsigned n)
 {
-    c_class *p = crt_alias_list ;
-    while ( p ) {
-	c_class *q = HEAD_list ( p )->ag_ptr ;
-	if ( q->ag_tag == n ) return ( q ) ;
-	p = TAIL_list ( p ) ;
-    }
-    error ( ERROR_INTERNAL, "Can't find alias %u", n ) ;
-    return ( NULL ) ;
+	c_class *p = crt_alias_list;
+	while (p) {
+		c_class *q = HEAD_list(p) ->ag_ptr;
+		if (q->ag_tag == n) {
+			return (q);
+		}
+		p = TAIL_list(p);
+	}
+	error(ERROR_INTERNAL, "Can't find alias %u", n);
+	return (NULL);
 }
 
 
@@ -392,23 +433,23 @@ c_class *find_c_class_alias
     zero, and the list itself is freed.
 */
 
-void clear_c_class_alias
-    PROTO_Z ()
+void
+clear_c_class_alias(void)
 {
-    c_class *p = crt_alias_list ;
-    c_class *q = NULL ;
-    while ( p ) {
-	HEAD_list ( p )->ag_ptr->ag_tag = 0 ;
-	q = p ;
-	p = TAIL_list ( p ) ;
-    }
-    if ( q ) {
-	TAIL_list ( q ) = free_c_class_array [2] ;
-	free_c_class_array [2] = crt_alias_list ;
-    }
-    crt_c_class_alias = 0 ;
-    crt_alias_list = NULL ;
-    return ;
+	c_class *p = crt_alias_list;
+	c_class *q = NULL;
+	while (p) {
+		HEAD_list(p)->ag_ptr->ag_tag = 0;
+		q = p;
+		p = TAIL_list(p);
+	}
+	if (q) {
+		TAIL_list(q) = free_c_class_array[2];
+		free_c_class_array[2] = crt_alias_list;
+	}
+	crt_c_class_alias = 0;
+	crt_alias_list = NULL;
+	return;
 }
 
 #endif /* c_class_IO_ROUTINES */
