a60945123a55ec0923533f6ec1487e02a8e4a820
Amar Takhar <verm@darkbeer.org>
2006-09-04 01:45:11 +0000

    add support for compiling svg's, how this works is still broken but it gets the job done, i'll fix it after all the docs are sorted.

--- doc/share/mk/doc.images.mk
+++ doc/share/mk/doc.images.mk
@@ -1,11 +1,11 @@
 #
-# $TenDRA$
+# $TenDRA: doc.images.mk 2447 2006-03-23 21:15:51Z verm $
 #
 
 PNMTOPNG?=	pnmtopng
 XPMTOPPM?=	xpmtoppm
 TGIF?=		tgif
-
+RSVG?=		rsvg
 
 IMAGES_PNG=	${IMAGES_OBJ:R:S/$/.png/g}
 IMAGES_EPS=	${IMAGES_OBJ:R:S/$/.eps/g}
@@ -13,9 +13,8 @@ IMAGES_EPS=	${IMAGES_OBJ:R:S/$/.eps/g}
 LOCAL_IMAGES_PNG=	${IMAGES_PNG}
 LOCAL_IMAGES_EPS=	${IMAGES_EPS}
 
-
 # Use suffix rules to convert .scr files to .png files
-.SUFFIXES:	.png .eps .obj
+.SUFFIXES:	.png .eps .obj .svg
 
 .obj.eps:
 	${TGIF} -print -eps -stdout ${.IMPSRC} > ${.CURDIR}/${.TARGET}
@@ -24,3 +23,6 @@ LOCAL_IMAGES_EPS=	${IMAGES_EPS}
 	${TGIF} -print -xpm -stdout ${.IMPSRC} |\
 		${XPMTOPPM} |\
 		${PNMTOPNG} > ${.CURDIR}/${.TARGET}
+
+.svg.png:
+	${RSVG} ${.IMPSRC} ${.TARGET}
