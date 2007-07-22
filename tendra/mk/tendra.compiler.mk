2f36eb495664309b57544ecd37d778834995f048
Harald van Dijk <harald@gigawatt.nl>
2007-07-22 16:09:05 +0000

    Don't require bootstrapping when performing a native build. The   utilities in ${BOBJ_DIR} are required when cross-compiling, and   will continue to be used when they exist, but otherwise, it is   possible to use the utilities only from ${OBJ_DIR}.
    svn path=/tendra/trunk/; revision=1347

--- tendra/mk/tendra.compiler.mk
+++ tendra/mk/tendra.compiler.mk
@@ -32,7 +32,6 @@ _TENDRA_COMPILER_MK_=1
 # EXEC_SUFFIX is used on those platforms where executable names need to
 # have a particular suffix, for example it may be set to ".exe".
 
-.if defined(BOOTSTRAP)
 # Assume that the cc on this system is the GNU C Compiler.
 .if "${CC:[1]:T}" == "cc" || "${CC:[1]:T}" == "gcc"
 
@@ -84,44 +83,50 @@ CWARNFLAGS+=-Wall
   LDOPTS+= ${LDFLAGS}
 .endif
 
-.if exists(${OBJ_DIR}/src/utilities/sid/sid)
+.if exists(${BOBJ_DIR}/src/tools/tcc/tcc) && !defined(BOOTSTRAP)
+CC=	${BOBJ_DIR}/src/tools/tcc/tcc -Y${BOBJ_DIR}/src/lib/env/build -yTENDRA_BASEDIR=${BOBJ_DIR}/src
+CFLAGS=
+LDFLAGS=
+.endif
+
+.if exists(${BOBJ_DIR}/src/utilities/sid/sid) && !defined(BOOTSTRAP)
+SID?=	${BOBJ_DIR}/src/utilities/sid/sid
+.elif exists(${OBJ_DIR}/src/utilities/sid/sid)
 SID?=	${OBJ_DIR}/src/utilities/sid/sid
 .else
 SID?=	sid
 .endif
 
-.if exists(${OBJ_DIR}/src/tools/tcc/tcc)
+.if exists(${BOBJ_DIR}/src/tools/tcc/tcc) && !defined(BOOTSTRAP)
+TCC?=	${BOBJ_DIR}/src/tools/tcc/tcc
+.elif exists(${OBJ_DIR}/src/tools/tcc/tcc)
 TCC?=	${OBJ_DIR}/src/tools/tcc/tcc
 .else
 TCC?=	tcc
 .endif
 
-.if exists(${OBJ_DIR}/src/tools/tld/tld)
+.if exists(${BOBJ_DIR}/src/tools/tld/tld) && !defined(BOOTSTRAP)
+TLD?=	${BOBJ_DIR}/src/tools/tld/tld
+.elif exists(${OBJ_DIR}/src/tools/tld/tld)
 TLD?=	${OBJ_DIR}/src/tools/tld/tld
 .else
 TLD?=	tld
 .endif
 
-.if exists(${OBJ_DIR}/src/tools/tnc/tnc)
+.if exists(${BOBJ_DIR}/src/tools/tnc/tnc) && !defined(BOOTSTRAP)
+TNC?=	${BOBJ_DIR}/src/tools/tnc/tnc
+.elif exists(${OBJ_DIR}/src/tools/tnc/tnc)
 TNC?=	${OBJ_DIR}/src/tools/tnc/tnc
 .else
 TNC?=	tnc
 .endif
 
-TSPEC?=	${OBJ_DIR}/src/tools/tspec/tspec
-.else # defined(BOOTSTRAP)
-CC=	${TCC}
-TCC=	${BOBJ_DIR}/src/tools/tcc/tcc
-TLD=	${BOBJ_DIR}/src/tools/tld/tld
-TNC=	${BOBJ_DIR}/src/tools/tnc/tnc
-TPL=	${BOBJ_DIR}/src/tools/tpl/tpl
+.if exists(${BOBJ_DIR}/src/tools/tspec/tspec) && !defined(BOOTSTRAP)
 TSPEC?=	${BOBJ_DIR}/src/tools/tspec/tspec
-# The build env must be the first -Y argument, since it sets the default env
-# search path.
-ENVFLAGS=-Y${BOBJ_DIR}/src/lib/env/build -yTENDRA_BASEDIR=${BOBJ_DIR}/src/
-TCCOPTS2:= ${TCCOPTS}
-TCCOPTS=
-CCOPTS+= ${ENVFLAGS} ${TCCOPTS2}
+.elif exists(${OBJ_DIR}/src/tools/tspec/tspec)
+TSPEC?=	${OBJ_DIR}/src/tools/tspec/tspec
+.else
+TSPEC?=	tspec
 .endif
 
 CCOPTS+=	-D_${OSVER}
