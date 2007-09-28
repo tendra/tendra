83e5157442f3f38f9573147cd5cfa6fb0df831b9
Tobias Ulmer <tobiasu@tmux.org>
2007-09-28 15:17:24 +0000

    use ==> like the top level makefile instead of #
    svn path=/tendra/trunk/; revision=1516

--- tendra/mk/tendra.rules.mk
+++ tendra/mk/tendra.rules.mk
@@ -9,32 +9,30 @@ _TENDRA_RULES_MK_=1
 
 PARTIMP=        ${.IMPSRC:C/^${BASE_DIR}\///1}
 
-# Implied rules.
-
 .SUFFIXES: .o .c .cc .sid .j .pl
 
 .c.o:
-	@${ECHO} "# Compiling ${PARTIMP}"
+	@${ECHO} "==> Compiling ${PARTIMP}"
 	${CC} ${CCOPTS} -c ${.IMPSRC} -o ${.TARGET}
 
 .cc.o:
-	@${ECHO} "# Compiling ${PARTIMP}"
+	@${ECHO} "==> Compiling ${PARTIMP}"
 	${CC} ${CCOPTS} -Yc++ -c ${.IMPSRC} -o ${.TARGET}
 
 .c.j:
-	@${ECHO} "# Compiling ${PARTIMP}"
+	@${ECHO} "==> Compiling ${PARTIMP}"
 	${CC} ${CCOPTS} ${JOPTS} -o ${.TARGET} ${.IMPSRC} -Ymakelib
 
 .cc.j:
-	@${ECHO} "# Compiling ${PARTIMP}"
+	@${ECHO} "==> Compiling ${PARTIMP}"
 	${CC} ${CCOPTS} ${JOPTS} -Yc++ -o ${.TARGET} ${.IMPSRC} -Ymakelib
 
 .pl.j:
-	@${ECHO} "# Transforming ${PARTIMP}"
+	@${ECHO} "==> Transforming ${PARTIMP}"
 	${TPL} ${TPLOPTS} ${.IMPSRC} ${.TARGET}
 
 .sid.c:
-	@${ECHO} "# Transforming ${PARTIMP} and ${PARTIMP:S/.sid/.act/}"
+	@${ECHO} "==> Transforming ${PARTIMP} and ${PARTIMP:S/.sid/.act/}"
 	${SID} ${SIDOPTS} ${.IMPSRC} ${.IMPSRC:S/.sid/.act/} ${.TARGET} \
 		${.TARGET:S/.c/.h/}
 
