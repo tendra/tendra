2ac9b5fd6f607d7a13b9fd6da8cc8a5087d1a6e0
Tobias Ulmer <tobiasu@tmux.org>
2007-09-29 14:33:47 +0000

    remove linux specific bit
    svn path=/tendra/trunk/; revision=1528

--- tendra/src/installers/80x86/openbsd/assembler.c
+++ tendra/src/installers/80x86/openbsd/assembler.c
@@ -344,10 +344,6 @@ outend(void)
   outnl();
   outs("___tdf_end:");
   outnl();
-/*  if (linux_elf) {
-    outs(".section .note.GNU-stack,\"\",@progbits");
-    outnl();
-  } */
   st = fclose(fpout);
   if (st == EOF) {
     failer("failed to close file");
