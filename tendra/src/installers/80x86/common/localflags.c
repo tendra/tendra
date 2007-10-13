f78448dbc0cdfea12417380bead0fb58e2c717c6
Tobias Ulmer <tobiasu@tmux.org>
2007-10-13 21:29:54 +0000

    remove flush_before_tell, we don't really support DOS anymore ;)
    svn path=/tendra/trunk/; revision=1645

--- tendra/src/installers/80x86/common/localflags.c
+++ tendra/src/installers/80x86/common/localflags.c
@@ -1,5 +1,35 @@
 /*
-    		 Crown Copyright (c) 1997
+ * Copyright (c) 2007 The TenDRA Project <http://www.tendra.org/>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of The TenDRA Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific, prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+/*
+		 Crown Copyright (c) 1997
 
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
@@ -27,48 +57,13 @@
         it may be put.
 */
 
-
-/* 80x86/localflags.c */
-
-/**********************************************************************
-$Author: release $
-$Date: 1998/01/17 15:55:52 $
-$Revision: 1.1.1.1 $
-$Log: localflags.c,v $
- * Revision 1.1.1.1  1998/01/17  15:55:52  release
- * First version to be checked into rolling release.
- *
- * Revision 1.5  1997/04/17  11:55:50  pwe
- * dwarf2 improvements
- *
- * Revision 1.4  1997/03/20  16:23:57  pwe
- * dwarf2
- *
- * Revision 1.3  1995/09/29  16:17:58  pwe
- * gcc_compatible default on Linux
- *
- * Revision 1.2  1995/01/30  12:56:30  pwe
- * Ownership -> PWE, tidy banners
- *
- * Revision 1.1  1994/10/27  14:15:22  jmf
- * Initial revision
- *
- * Revision 1.1  1994/07/12  14:36:14  jmf
- * Initial revision
- *
-**********************************************************************/
-
 #include "config.h"
 #include "common_types.h"
 
 #include "localflags.h"
 
-/* VARIABLES */
-/* All variables initialised */
-
 int is80486;		/* compile for 80486 */
 int is80586;		/* compile for 80586 */
-int flush_before_tell = 0;	/* to cure a bug in DOS system */
 int always_use_frame;
 int gcc_compatible = GCC_STRUCTS;
 int module_has_setjmp = 0;
@@ -78,4 +73,3 @@ int useful_double = 0;
 int keep_short = 0;	/* init by cproc */
 
 weak_cell * weak_list = (weak_cell*)0;
-
