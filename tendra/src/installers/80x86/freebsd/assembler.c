b833c9842941783d8dd03c53eb2d7cc00d46a752
Tobias Ulmer <tobiasu@tmux.org>
2007-10-13 20:30:04 +0000

    sync with the linux version. FreeBSD seems to lack all the PIC code
    svn path=/tendra/trunk/; revision=1641

--- tendra/src/installers/80x86/freebsd/assembler.c
+++ tendra/src/installers/80x86/freebsd/assembler.c
@@ -29,7 +29,7 @@
  * $Id$
  */
 /*
-    		 Crown Copyright (c) 1997
+		 Crown Copyright (c) 1997
 
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
@@ -57,9 +57,6 @@
         it may be put.
 */
 
-
-/* freebsd/assembler.c */
-
 #include "config.h"
 #include "common_types.h"
 #include "out.h"
@@ -70,315 +67,296 @@
 #include "basicread.h"
 #include "coder.h"
 
-
-/* PROCEDURES */
-
 void
 dot_align(int n)
 {
-  if (freebsd_elf) {
-    outs(".align ");
-    outn((long)n);
-    outnl();
-    return;
-  }
-  if (n == 1) {
-    return;
-  }
-  outs(".align ");
-  switch (n) {
-    case 16:
-      n = 4; break;
-    case 8:
-      n = 3; break;
-    case 4:
-      n = 2; break;
-    default:
-      n = 1; break;
-  }
-  outn((long)n);
-  outnl();
-  return;
-}
+	if (freebsd_elf) {
+		outs(".align ");
+		outn((long)n);
+		outnl();
+		return;
+	}
 
+	if (n == 1)
+		return;
+
+	outs(".align ");
+	switch (n) {
+	case 16:
+		n = 4;
+		break;
+	case 8:
+		n = 3;
+		break;
+	case 4:
+		n = 2;
+		break;
+	default:
+		n = 1;
+		break;
+	}
+
+	outn((long)n);
+	outnl();
+}
 
 void
 outbyte(void)
 {
-  outs(".byte ");
-  return;
+	outs(".byte ");
 }
 
 void
 outshort(void)
 {
-  outs(".value ");
-  return;
+	outs(".value ");
 }
 
 void
 outlong(void)
 {
-  outs(".long ");
-  return;
+	outs(".long ");
 }
 
 void
 align_label(int f, exp jr)
 {
-  if (freebsd_elf) {
-    if (is80486 && !is80586 && ptno(jr) != last_jump_label) {
-      if (f == 1) {	/* repeat jump */
-        outs(".align 4");
-      }
-      if (f == 2) {	/* preceded by a jmp or ret */
-        outs(".align 16");
-      }
-      outs("\n");
-      return;
-    }
-  }
-  if (is80486 && !is80586 && ptno(jr)!= last_jump_label) {
-    if (f == 1) {	/* repeat jump */
-      outs(".align 3,0x90");
-    }
-    if (f == 2) {	/* preceded by a jmp or ret */
-      outs(".align 4,0x90");
-    }
-    if (f == 3) {
-      outs(".align 2,0x90");
-    }
-    outs("\n");
-  }
-  if (is80586 && ptno(jr)!= last_jump_label) {
-    if (f >= 1 && f <= 3) {
-      outs(".align 2,0x90\n");
-    }
-  }
-  return;
+	if (freebsd_elf) {
+		if (is80486 && !is80586 && ptno(jr) != last_jump_label) {
+			/* repeat jump */
+			if (f == 1)
+				outs(".align 4");
+
+			/* preceded by a jmp or ret */
+			if (f == 2)
+				outs(".align 16");
+
+			outs("\n");
+		}
+	} else {
+		if (is80486 && !is80586 && ptno(jr) != last_jump_label) {
+			/* repeat jump */
+			if (f == 1)
+				outs(".align 3,0x90");
+
+			/* preceded by a jmp or ret */
+			if (f == 2)
+				outs(".align 4,0x90");
+
+			if (f == 3)
+				outs(".align 2,0x90");
+
+			outs("\n");
+		}
+
+		if (is80586 && ptno(jr) != last_jump_label)
+			if (f >= 1 && f <= 3)
+				outs(".align 2,0x90\n");
+	}
 }
 
 void
 eval_postlude(char *s, exp c)
 {
-  UNUSED(s);
-  UNUSED(c);
-  return;
+	UNUSED(s);
+	UNUSED(c);
 }
 
 void
 out_readonly_section(void)
 {
-  if (freebsd_elf) {
-    outs(".section .rodata");
-  } else {
-    outs(".text");
-  }
-  return;
+	if (freebsd_elf)
+		outs(".section .rodata");
+	else
+		outs(".text");
 }
 
 void
 out_dot_comm(char *id, shape sha)
 {
-  outs(".comm ");
-  outs(id);
-  outs(",");
-  outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
-  outnl();
-  return;
+	outs(".comm ");
+	outs(id);
+	outs(",");
+	outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
+	outnl();
 }
 
 void
 out_dot_lcomm(char *id, shape sha)
 {
-  outs(".lcomm ");
-  outs(id);
-  outs(",");
-  outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
-  outnl();
-  return;
+	outs(".lcomm ");
+	outs(id);
+	outs(",");
+	outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
+	outnl();
 }
 
 void
 out_bss(char *id, shape sha)
 {
-  outs(".bss ");
-  outs(id);
-  outs(",");
-  outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
-  outnl();
-  return;
+	outs(".bss ");
+	outs(id);
+	outs(",");
+	outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
+	outnl();
 }
 
 void
 pic_prelude(void)
 {
-  return;
 }
 
 void
 out_rename(char *oldid, char *newid)
 {
-  UNUSED(oldid);
-  UNUSED(newid);
-  return;
+	/* XXX */
+	UNUSED(oldid);
+	UNUSED(newid);
 }
 
 void
 out_switch_jump(int tab, where a, int min)
 {
-  outs(" jmp *");
-  outs(local_prefix);
-  outn((long)tab);
-  outs("-");
-  outn((long)(4 * min));
-  outs("(,");
-  operand(32, a, 1, 0);
-  outs(",4)");
-  outnl();
-  return;
+	outs(" jmp *");
+	outs(local_prefix);
+	outn((long)tab);
+	outs("-");
+	outn((long)(4 * min));
+	outs("(,");
+	operand(32, a, 1, 0);
+	outs(",4)");
+	outnl();
 }
 
 void
 out_switch_table(int tab, int min, int max, int *v, int absent)
 {
-  int i;
+	int i;
 
-  dot_align(4);
-  outnl();
+	dot_align(4);
+	outnl();
 
-  outs(local_prefix);
-  outn((long)tab);
-  outs(":");
-  outnl();
+	outs(local_prefix);
+	outn((long)tab);
+	outs(":");
+	outnl();
 
-  for (i = min; i <= max; ++i) {
-    outs(".long ");
-    if (v[i - min]!= -1) {
-      outs(local_prefix);
-      outn((long)v[i - min]);
-    } else {
-      if (absent == -1) {
-        outn((long)0);
-      } else {
-        outs(local_prefix);
-        outn((long)absent);
-      }
-    }
-    outnl();
-  }
-  outnl();
-  return;
+	for (i = min; i <= max; ++i) {
+		outs(".long ");
+		if (v[i - min]!= -1) {
+			outs(local_prefix);
+			outn((long)v[i - min]);
+		} else {
+			if (absent == -1) {
+				outn((long)0);
+			} else {
+				outs(local_prefix);
+				outn((long)absent);
+			}
+		}
+
+		outnl();
+	}
+	outnl();
 }
 
 void
 proc_size(char *s)
 {
-  outs(".align 4");
-  outnl();
-  outs(".size ");
-  outs(s);
-  outs(", .-");
-  outs(s);
-  outnl();
-  return;
+	outs(".align 4");
+	outnl();
+	outs(".size ");
+	outs(s);
+	outs(", .-");
+	outs(s);
+	outnl();
 }
 
 void
 proc_type(char *s)
 {
-  outs(".type ");
-  outs(s);
-  outs(",@function");
-  outnl();
-  return;
+	outs(".type ");
+	outs(s);
+	outs(",@function");
+	outnl();
 }
 
 void
 outend(void)
 {
-  int st;
-  outs(".text");
-  outnl();
-  dot_align(16);
-  outnl();
-  outs("___tdf_end:");
-  outnl();
-  out_close();
+	outs(".text");
+	outnl();
+	dot_align(16);
+	outnl();
+	outs("___tdf_end:");
+	outnl();
+	out_close();
 }
 
 void
 outopenbr(void)
 {
-  return;
 }
-
 
 void
 outclosebr(void)
 {
-  return;
 }
 
 void
 outdivsym(void)
 {
-  outs("/");
-  return;
+	outs("/");
 }
 
 void
 out_initialiser(char *id)
 {
-  if (freebsd_elf) {
-    outs(".section .init");
-    outnl();
-    outs(" call ");
-    outs(id);
-  } else {
-    outs(".stabs \"___TDFI_LIST__\",22,0,0,");
-    outs(id);
-    outnl();
-    outnl();
-  }
-  return;
+	if (freebsd_elf) {
+		outs(".section .init");
+		outnl();
+		outs(" call ");
+		outs(id);
+	} else {
+		outs(".stabs \"___TDFI_LIST__\",22,0,0,");
+		outs(id);
+		outnl();
+		outnl();
+	}
 }
 
 
 void
 out_main_prelude(void)
 {
-  int nl1 = next_lab();
-  int nl2 = next_lab();
-  min_rfree |= 0x8;
-  outs(" movl $___TDFI_LIST__+4, %ebx\n");
-  outs(local_prefix);
-  outn((long)nl1);
-  outs(":\n");
-  outs(" movl (%ebx),%eax\n");
-  outs(" cmpl $0,%eax\n");
-  simple_branch("je", nl2);
-  outs(" call *%eax\n");
-  outs(" addl $4,%ebx\n");
-  simple_branch("jmp", nl1);
-  outs(local_prefix);
-  outn((long)nl2);
-  outs(":\n");
-  return;
+	int nl1 = next_lab();
+	int nl2 = next_lab();
+	min_rfree |= 0x8;
+	outs(" movl $___TDFI_LIST__+4, %ebx\n");
+	outs(local_prefix);
+	outn((long)nl1);
+	outs(":\n");
+	outs(" movl (%ebx),%eax\n");
+	outs(" cmpl $0,%eax\n");
+	simple_branch("je", nl2);
+	outs(" call *%eax\n");
+	outs(" addl $4,%ebx\n");
+	simple_branch("jmp", nl1);
+	outs(local_prefix);
+	outn((long)nl2);
+	outs(":\n");
 }
 
 void
 out_main_postlude(void)
 {
-  char *sdummy = "Idummy";
-  char *pdummy = (char *)xcalloc(((int)strlen(local_prefix) +
-				  (int)strlen(sdummy) + 1), sizeof(char));
-  strcpy(pdummy, local_prefix);
-  strcat(pdummy, sdummy);
-  outs(".text\n");
-  outs(pdummy);
-  outs(":\n");
-  outs(" ret\n");
-  out_initialiser(pdummy);
-  return;
+	char *sdummy = "Idummy";
+	char *pdummy = (char *)xcalloc(((int)strlen(local_prefix) + (int)strlen(sdummy) + 1), sizeof(char));
+	strcpy(pdummy, local_prefix);
+	strcat(pdummy, sdummy);
+	outs(".text\n");
+	outs(pdummy);
+	outs(":\n");
+	outs(" ret\n");
+	out_initialiser(pdummy);
 }
