e27e2c1b88652ab94026abde85571c9e1d23897e
Tobias Ulmer <tobiasu@tmux.org>
2007-10-13 16:52:59 +0000

    indent properly, remove all useless comments
    svn path=/tendra/trunk/; revision=1633

--- tendra/src/installers/80x86/linux/machine.c
+++ tendra/src/installers/80x86/linux/machine.c
@@ -29,7 +29,7 @@
  * $Id$
  */
 /*
-    		 Crown Copyright (c) 1997
+		 Crown Copyright (c) 1997
 
     This TenDRA(r) Computer Program is subject to Copyright
     owned by the United Kingdom Secretary of State for Defence
@@ -57,42 +57,6 @@
         it may be put.
 */
 
-
-/* linux/machine.c */
-
-/**********************************************************************
-$Author: release $
-$Date: 1998/01/17 15:55:52 $
-$Revision: 1.1.1.1 $
-$Log: machine.c,v $
- * Revision 1.1.1.1  1998/01/17  15:55:52  release
- * First version to be checked into rolling release.
- *
- * Revision 1.6  1996/03/12  12:44:28  pwe
- * 64-bit ints compatible with gcc long long
- *
- * Revision 1.5  1996/02/08  13:45:32  pwe
- * Linux elf v aout option
- *
- * Revision 1.4  1995/10/02  08:12:15  pwe
- * correct gcc reg_result structs
- *
- * Revision 1.3  1995/09/29  16:18:07  pwe
- * gcc_compatible default on Linux
- *
- * Revision 1.2  1995/01/30  12:57:14  pwe
- * Ownership -> PWE, tidy banners
- *
- * Revision 1.1  1994/11/08  16:28:22  jmf
- * Initial revision
- *
- * Revision 1.3  1994/07/13  07:51:05  jmf
- * Added Log
- *
-**********************************************************************/
-
-
-
 #include "config.h"
 #include "common_types.h"
 #include "expmacs.h"
@@ -102,51 +66,46 @@ $Log: machine.c,v $
 #include "localflags.h"
 #include "szs_als.h"
 
-/* VARIABLES */
-/* All variables are initialised, jmf */
-
 int linux_elf = 1;
-
 char *local_prefix = ".L";
 char *name_prefix = "";
-
-
-
-/* PROCEDURES */
 
 void
 set_linux_format(int elf)
 {
-  linux_elf = elf;
-  if (elf) {
-    local_prefix = ".L";
-    name_prefix = "";
-  } else {
-    local_prefix = "L";
-    name_prefix = "_";
-  }
-  return;
+	linux_elf = elf;
+	if (elf) {
+		local_prefix = ".L";
+		name_prefix = "";
+	} else {
+		local_prefix = "L";
+		name_prefix = "_";
+	}
 }
 
-
-/* is the result of a procedure delivering
-   this shape produced in registers. */
+/*
+ * is the result of a procedure delivering
+ * this shape produced in registers.
+ */
 int
 reg_result(shape sha)
 {
-  unsigned char n = name(sha);
-  if (gcc_compatible) {
-    int sz = shape_size(sha);
-    if (n == nofhd) {
-      return 0;
-    }
-    if (n == cpdhd && sz != 8 && sz != 16 && sz != 32 && sz != 64) {
-      return 0;
-    }
-    return 1;
-  }
-  if (n == cpdhd || n == nofhd) {
-    return 0;
-  }
-  return 1;
+	unsigned char n = name(sha);
+
+	if (gcc_compatible) {
+		int sz = shape_size(sha);
+
+		if (n == nofhd)
+			return 0;
+
+		if (n == cpdhd && sz != 8 && sz != 16 && sz != 32 && sz != 64)
+			return 0;
+
+		return 1;
+	}
+
+	if (n == cpdhd || n == nofhd)
+		return 0;
+
+	return 1;
 }
