9a8f4ad652e2cfd4370ff6018c47defd7a5202fa
Kate F <kate@elide.org>
2008-02-25 00:44:21 +0000

    NoReturnT is now known as NORETURN for consistency. Reworked its definition a little.
    svn path=/trunk/; revision=1965

--- tendra/src/shared/check/check.h
+++ tendra/src/shared/check/check.h
@@ -69,19 +69,15 @@
 
 /*
  * This is the return type of a function that doesn't return (e.g.
- * ``longjmp'').  Normally this is just ``void'', however the GNU C compiler
- * can produce better code if this is declared as ``__volatile__ void''.  The
- * TenDRA compiler can be told that a function doesn't return as well.
+ * ``longjmp'').
  */
-# ifdef __GNUC__
-typedef void NoReturnT;
+# if defined __TenDRA__
+#  pragma TenDRA type NORETURN for bottom
+# elif defined __GNUC__
+#  define NORETURN __attribute__((noreturn)) void
 # else
-#  ifdef __TenDRA__
-#   pragma TenDRA type NoReturnT for bottom
-#  else
-#   define NoReturnT void
-#  endif /* defined (__TenDRA__) */
-# endif /* defined (__GNUC__) */
+#  define NORETURN void
+# endif
 
 /*
  * This macro documents the fact that the specified variable will no longer be
@@ -121,7 +117,7 @@ typedef void NoReturnT;
 # else
 #  include <assert.h>
 #  include <stdlib.h>
-#  define UNREACHED assert(FALSE); abort()
+#  define UNREACHED do { assert(!"unreachable"); abort(); } while (0)
 # endif /* defined (__TenDRA__) */
 
 /*
