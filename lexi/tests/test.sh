0b132d9521f30ec85da29163cca83e0577c6db43
Kate F <kate@elide.org>
2008-05-03 13:58:43 +0000

    A simple start on a few acceptance tests for the `.lxi` format.
    svn path=/trunk/; revision=2060

---
+++ lexi/tests/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Simple acceptance tests for Lexi input.
+# $Id$
+
+# Test a .lxi file for validity. This is parsing and semantic analysis only; no
+# verification of the generated code is performed; that can follow in the future.
+#
+# $1 - The expected value for $?
+# $2 - The .lxi file under test
+lxitest() {
+	../src/lexi $2 actions.lct - - 2>&- >&-
+	r=$?
+	if [ $r -ne $1 ]; then
+		echo "FAIL: $r where expecting $1 for $2"
+	else
+		echo "PASS: $r where expecting $1 for $2"
+	fi
+}
+
+for t in pass-*.lxi; do
+	lxitest 0 $t
+done
+
+for t in fail-*.lxi; do
+	lxitest 1 $t
+done
+
