63f2143a5c042e95cd6fa60e15e1ef3eb2f68286
Kate F <kate@elide.org>
2009-05-25 12:38:01 +0000

    Move out `tendra/mk` (1 of 2).
    This is a start on converting all TenDRA projects to use the same makefile system. There are two goals here:
     * A top-level makefile for users to conveniently build the entire system, mostly to help people who just want to try it out
     * Keeping self-contained tars for individual projects, per-release. This is currently done by having tar fold in symlinks (in this case, to [source:/trunk/shared/mk /shared/mk]), which seems to be a sane approach.
    
    svn path=/trunk/; revision=2271

---
+++ shared/mk/tendra.functions.mk
@@ -0,0 +1,18 @@
+# TenDRA make functions
+#
+# $Id$
+
+.if !defined(_TENDRA_FUNCTIONS_MK_)
+_TENDRA_FUNCTIONS_MK_=1
+
+# This shell function is inlined in rules below. Careful about quoting.
+CONDCREATE=     @Condcreate() {                                      \
+                        for dir in $${*}; do                         \
+                                if ${TEST} ! -e $${dir}; then        \
+                                        ${ECHO} "\# Creating $${dir}/"; \
+                                        ${MKDIR} -p $${dir};         \
+                                fi;                                  \
+                        done;                                        \
+                }; Condcreate
+
+.endif	# !defined(_TENDRA_FUNCTIONS_MK_)
