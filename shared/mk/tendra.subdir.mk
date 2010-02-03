6a67849181990beed7cb0227595142eb064bfd94
Kate F <kate@elide.org>
2010-02-03 04:05:16 +0000

    Include `<tendra.subdir.mk>` as required per-Makefile, instead of globally.
    svn path=/trunk/; revision=2295

--- shared/mk/tendra.subdir.mk
+++ shared/mk/tendra.subdir.mk
@@ -8,18 +8,17 @@ _TENDRA_SUBDIR_MK_=1
 .include <tendra.base.mk>
 .include <tendra.functions.mk>
 
-# TODO: make SUBDIR non-optional when this makefile is included explicitly
-#.if !defined(SUBDIR)
-#.BEGIN:
-#	@${ECHO} '$${SUBDIR} must be set'
-#	@${EXIT} 1;
-#.endif
+.if !defined(SUBDIR)
+.BEGIN:
+	@${ECHO} '$${SUBDIR} must be set'
+	@${EXIT} 1;
+.endif
 
 
 
 # Proceed to subdirs.
 _SUBDIR: .USE
-.if defined(SUBDIR) && !empty(SUBDIR)
+.if !empty(SUBDIR)
 . for entry in ${SUBDIR}
 	@cd ${.CURDIR}/${entry}; ${MAKE} ${.TARGET}
 . endfor
