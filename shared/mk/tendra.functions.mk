b344f863d63d384b428e09fca5abb346619d6b34
Kate F <kate@elide.org>
2010-02-12 20:08:50 +0000

    Whitespace.
    svn path=/trunk/; revision=2313

--- shared/mk/tendra.functions.mk
+++ shared/mk/tendra.functions.mk
@@ -6,13 +6,13 @@
 _TENDRA_FUNCTIONS_MK_=1
 
 # This shell function is inlined in rules below. Careful about quoting.
-CONDCREATE=     @Condcreate() {                                      \
-                        for dir in $${*}; do                         \
-                                if ${TEST} ! -e $${dir}; then        \
-                                        ${ECHO} "\# Creating $${dir}/"; \
-                                        ${MKDIR} -p $${dir};         \
-                                fi;                                  \
-                        done;                                        \
-                }; Condcreate
+CONDCREATE=	@Condcreate() {                              \
+           	    for dir in $${*}; do                     \
+           	        if ${TEST} ! -e $${dir}; then        \
+           	            ${ECHO} "==> Creating $${dir}/"; \
+           	                ${MKDIR} -p $${dir};         \
+           	        fi;                                  \
+           	    done;                                    \
+           	}; Condcreate
 
 .endif	# !defined(_TENDRA_FUNCTIONS_MK_)
