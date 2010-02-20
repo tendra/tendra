a6bd6048aaf563281f053cedd7ceedcb5583ff3b
Kate F <kate@elide.org>
2010-02-20 20:03:03 +0000

    Split off API-generation rules to their own targets.
    svn path=/trunk/; revision=2370

--- tspec/mk-local/tendra.tspec.mk
+++ tspec/mk-local/tendra.tspec.mk
@@ -10,31 +10,41 @@ _TENDRA_WORK_TSPEC_MK_=1
 
 
 
-TSPEC_FLAGS+=	-O${OBJ_DIR}/share/TenDRA/include
-TSPEC_FLAGS+=	-S${OBJ_DIR}/share/TenDRA/src
+TSPEC_TIOUT=	${OBJ_DIR}/share/TenDRA/include
+TSPEC_TSOUT=	${OBJ_DIR}/share/TenDRA/src
+
+TSPEC_FLAGS+=	-O${TSPEC_TIOUT}
+TSPEC_FLAGS+=	-S${TSPEC_TSOUT}
+
+ALL_APIS=	${BASE_APIS} ${EXT_APIS}
+
+
+.for api in ${BASE_APIS}
+${TSPEC_TIOUT}/${api}.api ${TSPEC_TSOUT}/${api}.api: ${TSPEC}
+	@${ECHO} "==> Creating base API source for ${api}"
+	${TSPEC} ${TSPEC_FLAGS} -I${BASE_DIR}/base ${api}
+.endfor
+
+.for api in ${EXT_APIS}
+${TSPEC_TIOUT}/${api}.api ${TSPEC_TSOUT}/${api}.api: ${TSPEC}
+	@${ECHO} "==> Creating extension API source for ${api}"
+	${TSPEC} ${TSPEC_FLAGS} -I${BASE_DIR}/base -I${BASE_DIR}/extension ${api}
+.endfor
+
 
 
 #
 # User-facing targets
 #
 
-all:: ${TSPEC}
-.for api in ${BASE_APIS}
-	@${ECHO} "==> Creating API source for ${api}"
-	${TSPEC} ${TSPEC_FLAGS} -I${BASE_DIR}/base ${api}
-.endfor
-.for api in ${EXT_APIS}
-	@${ECHO} "==> Creating API source for ${api}"
-	${TSPEC} ${TSPEC_FLAGS} -I${BASE_DIR}/base -I${BASE_DIR}/extension ${api}
-.endfor
+all:: ${TSPEC} ${ALL_APIS:C/^/${TSPEC_TIOUT}\//g:C/$/.api/g} \
+               ${ALL_APIS:C/^/${TSPEC_TSOUT}\//g:C/$/.api/g}
 
 
 clean::
 
 
-# TODO: dependencies
-# TODO: output makefiles?
-install::
+install:: all
 	@${CONDCREATE} "${SHARE_DIR}/tspec"
 	${INSTALL} -m 755 -d ${SHARE_DIR}/tspec
 	${COPY} -R ${OBJ_DIR}/share/TenDRA ${SHARE_DIR}/tspec
