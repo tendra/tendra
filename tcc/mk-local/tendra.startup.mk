c167376347605b92c666e5ea3b998fafc6a5310c
Kate F <kate@elide.org>
2010-02-21 08:51:02 +0000

    Moved out startup files (modes, portability tables and such, not the API building startup files, which unfortunately have the same name).
    I was a little unsure as to whether these belong with the producers, or with tcc. Certianly they configure the producers, but then so do all the options tcc passes. I've opted to put them with tcc for two reasons: firstly to keep all tcc's configuration stuff in one place (alongside the environments), and secondly because if these were placed with the producers, you'd still need tcc to find them (unless you went exploring the filesystem by hand). In other words, they're here because this is where they're used.
    
    svn path=/trunk/; revision=2375

---
+++ tcc/mk-local/tendra.startup.mk
@@ -0,0 +1,46 @@
+# TenDRA make build infrastructure
+# XXX: due to go, when tspec is split off
+#
+# $Id$
+
+.if !defined(_TENDRA_WORK_STARTUP_MK_)
+_TENDRA_WORK_STARTUP_MK_=1
+
+.include <tendra.base.mk>
+.include <tendra.functions.mk>
+
+.if !defined(STARTUPSUBDIR)
+.BEGIN:
+	@${ECHO} '$${STARTUPSUBDIR} must be set'
+	@${EXIT} 1;
+.endif
+
+
+
+#
+# User-facing targets
+#
+
+all::
+
+
+clean::
+
+
+install::
+	@${ECHO} "==> Installing ${WRKDIR}/${STARTUPSUBDIR} startup directories"
+	@${CONDCREATE} ${STARTUPSUBDIR:S/^/${PREFIX}/lib/tcc\/startup\//g}
+	@${FIND} ${STARTUPSUBDIR} -name '.*' -prune -o -print |                               \
+	while read file; do                                                                   \
+		if ${TEST} -d $${file}; then                                                      \
+			${ECHO} ${INSTALL} -m 755 -d ${PREFIX}/lib/tcc/startup/$${file};              \
+			${INSTALL} -m 755 -d ${PREFIX}/lib/tcc/startup/$${file} || ${EXIT} $$?;       \
+		else                                                                              \
+			${ECHO} ${INSTALL} -m 644 $${file} ${PREFIX}/lib/tcc/startup/$${file};        \
+			${INSTALL} -m 644 $${file} ${PREFIX}/lib/tcc/startup/$${file} || ${EXIT} $$?; \
+		fi;                                                                               \
+	done
+
+
+
+.endif	# !defined(_TENDRA_WORK_STARTUP_MK_)
