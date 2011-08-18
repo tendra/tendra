fc59aeb14072582dfafc34361d4d35389d8d1894
Kate F <kate@elide.org>
2011-08-18 10:38:23 +0000

    I believe `<ansi_compat.h>` is required on ULTRIX, but not elsewhere, so I am making it conditinal such that `-DALLARCH` can compile this installer on other systems. Tested on ULTRIX 4.2/MIPS.
    svn path=/trunk/; revision=2592

--- installers-dra/src/mips/ultrix/cross_config.h
+++ installers-dra/src/mips/ultrix/cross_config.h
@@ -31,12 +31,14 @@
 #ifndef __CROSS_CONFIG_INCLUDED
 #define __CROSS_CONFIG_INCLUDED
 
+#ifdef __ultrix
 #ifndef __ANSI_COMPAT
 #define __ANSI_COMPAT
 #ifndef CROSS_INCLUDE
 #include <ansi_compat.h>
 #else
 #include CROSS_INCLUDE/ansi_compat.h>
+#endif
 #endif
 #endif
 
