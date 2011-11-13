60a0c618a1042e6f3f189ded76a3f273d6ffe477
Kate F <kate@elide.org>
2011-11-13 17:43:42 +0000

    Style.
    svn path=/trunk/; revision=2697

--- shared/include/getopt.h
+++ shared/include/getopt.h
@@ -1,4 +1,18 @@
+/* $Id$ */
+
+/*
+ * Copyright 2011, The TenDRA Project.
+ * Public Domain, 1985, AT&T, Inc.
+ *
+ * See doc/copyright/ for the full copyright terms.
+ */
+
+#ifndef GETOPT_INCLUDED
+#define GETOPT_INCLUDED
+
 extern char *optarg;
 extern int optind, opterr;
 
-int getopt (int argc, char **argv, const char *options);
+int getopt(int argc, char * const *argv, const char *options);
+
+#endif
--- shared/src/getopt.c
+++ shared/src/getopt.c
@@ -1,27 +1,18 @@
+/* $Id$ */
+
 /*
- * Here's something you've all been waiting for:  the AT&T public domain
- * source for getopt(3).  It is the code which was given out at the 1985
- * UNIFORUM conference in Dallas.  I obtained it by electronic mail
- * directly from AT&T.  The people there assure me that it is indeed
- * in the public domain.
+ * Copyright 2011, The TenDRA Project.
+ * Public Domain, 1985, AT&T, Inc.
  *
- * There is no manual page.  That is because the one they gave out at
- * UNIFORUM was slightly different from the current System V Release 2
- * manual page.  The difference apparently involved a note about the
- * famous rules 5 and 6, recommending using white space between an option
- * and its first argument, and not grouping options that have arguments.
- * Getopt itself is currently lenient about both of these things White
- * space is allowed, but not mandatory, and the last option in a group can
- * have an argument.  That particular version of the man page evidently
- * has no official existence, and my source at AT&T did not send a copy.
- * The current SVR2 man page reflects the actual behavor of this getopt.
- * However, I am not about to post a copy of anything licensed by AT&T.
+ * See doc/copyright/ for the full copyright terms.
  */
 
 #include <stdio.h>
 #include <string.h>
 
-#define ERR(szz,czz) if(opterr){fprintf(stderr,"%s%s%c\n",argv[0],szz,czz);}
+#include <shared/getopt.h>
+
+#define ERR(szz, czz) if (opterr) { fprintf(stderr, "%s%s%c\n", argv[0], szz, czz); }
 
 int opterr = 1;
 int optind = 1;
@@ -29,46 +20,56 @@ int optopt;
 char *optarg;
 
 int
-getopt(int argc, char **argv, char *opts)
+getopt(int argc, char * const *argv, const char *opts)
 {
-    static int sp = 1;
-    register int c;
-    register char *cp;
+	static int sp = 1;
+	const char *cp;
 
-    if (sp == 1) {
-	if (optind >= argc ||
-	    argv[optind][0] != '-' || argv[optind][1] == '\0')
-	    return (EOF);
-	else if (strcmp(argv[optind], "--") == 0) {
-	    optind++;
-	    return (EOF);
+	if (sp == 1) {
+		if (optind >= argc) {
+			return EOF;
+		} else if (argv[optind][0] != '-' || argv[optind][1] == '\0') {
+			return EOF;
+		} else if (strcmp(argv[optind], "--") == 0) {
+			optind++;
+			return EOF;
+		}
 	}
-    }
-    optopt = c = argv[optind][sp];
-    if (c == ':' || (cp = strchr(opts, c)) == NULL) {
-	ERR(": illegal option -- ", c);
-	if (argv[optind][++sp] == '\0') {
-	    optind++;
-	    sp = 1;
+
+	optopt = argv[optind][sp];
+
+	if (optopt == ':' || (cp = strchr(opts, optopt)) == NULL) {
+		ERR(": illegal option -- ", optopt);
+
+		if (argv[optind][++sp] == '\0') {
+			optind++;
+			sp = 1;
+		}
+
+		return '?';
 	}
-	return ('?');
-    }
-    if (*++cp == ':') {
-	if (argv[optind][sp + 1] != '\0')
-	    optarg = &argv[optind++][sp + 1];
-	else if (++optind >= argc) {
-	    ERR(": option requires an argument -- ", c);
-	    sp = 1;
-	    return ('?');
-	} else
-	    optarg = argv[optind++];
-	sp = 1;
-    } else {
-	if (argv[optind][++sp] == '\0') {
-	    sp = 1;
-	    optind++;
+
+	if (*++cp == ':') {
+		if (argv[optind][sp + 1] != '\0') {
+			optarg = &argv[optind++][sp + 1];
+		} else if (++optind >= argc) {
+			ERR(": option requires an argument -- ", optopt);
+
+			sp = 1;
+			return '?';
+		} else {
+			optarg = argv[optind++];
+		}
+
+		sp = 1;
+	} else {
+		if (argv[optind][++sp] == '\0') {
+			sp = 1;
+			optind++;
+		}
+		optarg = NULL;
 	}
-	optarg = NULL;
-    }
-    return (c);
+
+	return optopt;
 }
+
