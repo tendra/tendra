2666cc57d29c7a2b0a8c596d2c88fbf161bc519f
Kate F <kate@elide.org>
2012-05-24 21:10:57 +0000

    Added support for GLIBC 2.14 on Linux 80x86. Tested on Fedora 16.
    svn path=/trunk/; revision=2720

--- osdep/machines/linux/80x86/startup/posix.h
+++ osdep/machines/linux/80x86/startup/posix.h
@@ -23,3 +23,18 @@
 #define __WRONG_POSIX_SYS_WAIT_H	/* incompatible dev_t */
 #endif
 
+#ifdef _GLIBC2_14
+#define __WRONG_POSIX_GRP_H	/* incompatible dev_t */
+#define __WRONG_POSIX_SYS_STAT_H	/* incompatible dev_t */
+#define __WRONG_POSIX_STDIO_H_CTUID	/* i don't know what to do about va_list */
+#define __WRONG_POSIX_SYS_WAIT_H	/* incompatible dev_t */
+#define __WRONG_POSIX_STDIO_H	/* i don't know what to do about va_list */
+#define __WRONG_POSIX_FCNTL_H	/* incompatible dev_t */
+#define __WRONG_POSIX_PWD_H	/* incompatible dev_t */
+#define __WRONG_POSIX_UTIME_H	/* incompatible dev_t */
+#define __WRONG_POSIX_UNISTD_H_U_PROTO	/* incompatible dev_t */
+#define __WRONG_POSIX_SIGNAL_H	/* incompatible dev_t */
+#define __WRONG_POSIX_SYS_TYPES_H_STAT_T	/* incompatible dev_t */
+#define __WRONG_POSIX_UNISTD_H_TCPGRP	/* incompatible dev_t */
+#endif
+
