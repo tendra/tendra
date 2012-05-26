27e7f26caadaff4c3c9b660213c56a1e0ca6f3be
Kate F <kate@elide.org>
2012-05-26 12:57:59 +0000

    Added posix1, posix2, xpg3, xpg4, unix95 and svid3, with 100% conformance for each. The only non-conformant thing I found for glibc 2.14 is `MAXFLOAT` missing under `_XOPEN_SOURCE 1`, which I worked around by defining it myself. Tested on Fedora 16.
    svn path=/trunk/; revision=2724

---
+++ osdep/machines/linux/80x86/include/math.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+
+#ifndef __HACKED_MATH_H
+#define __HACKED_MATH_H
+
+/*
+ * glibc only defines MAXFLOAT under _XOPEN_SOURCE 500 or above,
+ * but XPG3 is _XOPEN_SOURCE 1. So I'm just defining it here
+ * rather than attempting to trick glibc into defining it.
+ */
+#ifdef _GLIBC2_14
+#ifdef __BUILDING_TDF_XPG3_MATH_H
+#define MAXFLOAT 3.40282347e+38F
+#endif
+#endif
+
+
+#include_next <math.h>
+
+#endif
+
