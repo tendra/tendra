13dd2eb9c3087e15c1f7510bda42fae54e878cc2
Kate F <kate@elide.org>
2012-05-28 15:37:41 +0000

    Added the tspec API environments to the tcc environment search path.
    svn path=/trunk/; revision=2731

--- tcc/src/tcc.sh
+++ tcc/src/tcc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 exec -PREFIX_LIBEXEC-/tcc \
-    --SED${TCCENV:-"-PREFIX_ENV-"} --CFE -Ybase -Ydefault ${@+"$@"}
+    --SED${TCCENV:-"-PREFIX_ENV-"}:-PREFIX_TSPEC-/TenDRA/env --CFE \
+    -Ybase -Ydefault ${@+"$@"}
 
