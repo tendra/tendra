65364e0a0561d4ec39f1be387e0261d0ff8abe06
Kate F <kate@elide.org>
2012-06-02 04:53:58 +0000

    Remove `SUBSTVARS`; obsoleted by [2762].
    svn path=/trunk/; revision=2763

--- shared/mk/tendra.functions.mk
+++ shared/mk/tendra.functions.mk
@@ -19,28 +19,5 @@ CONDCREATE= Condcreate() {                       \
         done;                                    \
     }; Condcreate
 
-SUBSTVARS=	                                     \
-    ${SED} -e '                                  \
-        s,-PREFIX-,${PREFIX},g;                  \
-        s,-PREFIX_BIN-,${PREFIX_BIN},g;          \
-        s,-PREFIX_LIB-,${PREFIX_LIB},g;          \
-        s,-PREFIX_LIBEXEC-,${PREFIX_LIBEXEC},g;  \
-        s,-PREFIX_SHARE-,${PREFIX_SHARE},g;      \
-        s,-PREFIX_INCLUDE-,${PREFIX_INCLUDE},g;  \
-        s,-PREFIX_MAN-,${PREFIX_MAN},g;          \
-        s,-PREFIX_TSPEC-,${PREFIX_TSPEC},g;      \
-        s,-PREFIX_STARTUP-,${PREFIX_STARTUP},g;  \
-        s,-PREFIX_ENV-,${PREFIX_ENV},g;          \
-        s,-PREFIX_API-,${PREFIX_API},g;          \
-        s,-PREFIX_LPI-,${PREFIX_LPI},g;          \
-        s,-PREFIX_SYS-,${PREFIX_SYS},g;          \
-        s,-PREFIX_TMP-,${PREFIX_TMP},g;          \
-                                                 \
-        s,-EXECFORMAT-,${EXECFORMAT},g;          \
-        s,-BLDARCH-,${BLDARCH},g;                \
-        s,-BLDARCHBITS-,${BLDARCHBITS},g;        \
-        s,-OSFAM-,${OSFAM},g;                    \
-        s,-OSVER-,${OSVER},g'                    \
-
 
 .endif	# !defined(_TENDRA_FUNCTIONS_MK_)
