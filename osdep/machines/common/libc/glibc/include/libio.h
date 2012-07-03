6ce030f588bd433f0c8106c3942b891fab7b7154
Kate F <kate@elide.org>
2012-07-03 12:54:50 +0000

    Added GLIBC 2.3.
    svn path=/trunk/; revision=2843

--- osdep/machines/common/libc/glibc/include/libio.h
+++ osdep/machines/common/libc/glibc/include/libio.h
@@ -3,8 +3,8 @@
 #ifndef __HACKED_LIBC_LIBIO_H
 #define __HACKED_LIBC_LIBIO_H
 
-#if defined(_GLIBC2_5) || defined(_GLIBC2_7) || defined(_GLIBC2_11) \
-	|| defined(_GLIBC2_12)
+#if defined(_GLIBC2_3) || defined(_GLIBC2_5) || defined(_GLIBC2_7) \
+	|| defined(_GLIBC2_11) || defined(_GLIBC2_12)
 #include <err.h>	/* for __gnuc_va_list */
 #endif
 
--- shared/mk/tendra.makedefs.mk
+++ shared/mk/tendra.makedefs.mk
@@ -133,6 +133,7 @@ MD_OSVER!=                               \
 MD_LIBCVER!=                             \
     case "${LIBC_VER}" in                \
         EGLIBC_2_11_*) echo EGLIBC2_11;; \
+        GLIBC_2_3_*)   echo GLIBC2_3;;   \
         GLIBC_2_5)     echo GLIBC2_5;;   \
         GLIBC_2_7)     echo GLIBC2_7;;   \
         GLIBC_2_11_*)  echo GLIBC2_11;;  \
