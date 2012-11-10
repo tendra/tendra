2377b76df022a765a07b476f071782ebcd8b9471
Kate F <kate@elide.org>
2012-11-10 16:09:53 +0000

    Consolidated `loc_signal.h`.
    svn path=/trunk/; revision=2869

--- installers-dra/src/mips/common/loc_signal.h
+++ installers-dra/src/mips/common/loc_signal.h
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 /*
- * Copyright 2011, The TenDRA Project.
+ * Copyright 2012, The TenDRA Project.
  * Copyright 1997, United Kingdom Secretary of State for Defence.
  *
  * See doc/copyright/ for the full copyright terms.
@@ -12,6 +12,8 @@
  
 #define MIPS_SIGFPE  8
 #define MIPS_SIGSEGV 11
-#define MIPS_SIGUSR1 16
+
+/* XXX: Really these ought to be passed in at runtime instead. */
+#define MIPS_SIGUSR1 (assembler == ASM_ULTRIX ? 30 : 16)
 
 #endif
