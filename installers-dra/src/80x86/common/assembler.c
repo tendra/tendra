c8811183bb5ae8fb62099848625895d478097599
Kate F <kate@elide.org>
2012-11-10 01:49:37 +0000

    Consolidated assembler.c for each OS.
    These actually contain no OS-specific parts; rather the differences are due to assembler dialect, PIC, executable format, and so on. Those have all been broken out as seperate parameters. So, I've merged these files and made those parts conditional.
    
    svn path=/trunk/; revision=2852

--- installers-dra/src/80x86/common/assembler.c
+++ installers-dra/src/80x86/common/assembler.c
@@ -1,65 +1,57 @@
-/* $Id$ */
+/* $Id */
 
 /*
- * Copyright 2002-2011, The TenDRA Project.
+ * Copyright 2002-2012, The TenDRA Project.
  * Copyright 1997, United Kingdom Secretary of State for Defence.
  *
  * See doc/copyright/ for the full copyright terms.
  */
 
-#include <string.h>
-
 #include <shared/xalloc.h>
 
 #include "config.h"
 
-#include "exptypes.h"
-#include <construct/exp.h>
-#include "out.h"
-#include "expmacs.h"
-#include <construct/exp.h>
-#include "localflags.h"
-#include <construct/shapemacs.h>
-#include "operand.h"
-#include <construct/machine.h>
-#include <construct/flags.h>
 #include <reader/basicread.h>
-#include "coder.h"
-#include "assembler.h"
+
+#include <construct/exp.h>
+#include <construct/machine.h>
+#include <construct/install_fns.h>
+
+#include "exptypes.h"
 #include "instr.h"
 #include "instr386.h"
-#include <construct/install_fns.h>
+#include "out.h"
+#include "expmacs.h"
+#include "operand.h"
+#include "localflags.h"
+#include "coder.h"
+
+static int pic_label;
 
 void
 dot_align(int n)
 {
 	if (format == FORMAT_ELF) {
 		outs(".align ");
-		outn((long)n);
+		outn((long) n);
 		outnl();
 		return;
 	}
 
-	if (n == 1)
+	if (n == 1) {
 		return;
-
-	outs(".align ");
-	switch (n) {
-	case 16:
-		n = 4;
-		break;
-	case 8:
-		n = 3;
-		break;
-	case 4:
-		n = 2;
-		break;
-	default:
-		n = 1;
-		break;
 	}
 
-	outn((long)n);
+	outs(".align ");
+
+	switch (n) {
+	case 16: n = 4; break;
+	case 8:  n = 3; break;
+	case 4:  n = 2; break;
+	default: n = 1; break;
+	}
+
+	outn((long) n);
 	outnl();
 }
 
@@ -88,16 +80,20 @@ align_label(int f, exp jr)
 		if (is80486 && !is80586 && ptno(jr) != last_jump_label) {
 #if 0
 			/* forward jump and continued into */
-			if (f == 0)
+			if (f == 0) {
 				outs(".align 8");
+			}
 #endif
+
 			/* repeat jump */
-			if (f == 1)
+			if (f == 1) {
 				outs(".align 4");
+			}
 
 			/* preceded by a jmp or ret */
-			if (f == 2)
+			if (f == 2) {
 				outs(".align 16");
+			}
 
 			outs("\n");
 		}
@@ -105,39 +101,47 @@ align_label(int f, exp jr)
 		if (is80486 && !is80586 && ptno(jr) != last_jump_label) {
 #if 0
 			/* forward jump and continued into */
-			if ( f== 0)
-				outs(".align 16,7,1");
+			if (f == 0) {
+				outs(".align 8");
+			}
 #endif
+
 			/* repeat jump */
-			if (f == 1)
+			if (f == 1)	{
 				outs(".align 3,0x90");
+			}
 
 			/* preceded by a jmp or ret */
-			if (f == 2)
+			if (f == 2)	{
 				outs(".align 4,0x90");
+			}
 
-			if (f == 3)
+			if (f == 3) {
 				outs(".align 2,0x90");
+			}
 
 			outs("\n");
 		}
 
-		if (is80586 && ptno(jr) != last_jump_label)
-			if (f >= 1 && f <= 3)
+		if (is80586 && ptno(jr) != last_jump_label) {
+			if (f >= 1 && f <= 3) {
 				outs(".align 2,0x90\n");
+			}
+		}
 	}
 }
 
 void
 eval_postlude(char *s, exp c)
 {
-	if (format == FORMAT_AOUT)
+	if (format == FORMAT_AOUT) {
 		return;
+	}
 
 	outs(".size ");
 	outs(s);
 	outs(",");
-	outn((long)(shape_size(sh(c)) + 7) / 8);
+	outn((long) (shape_size(sh(c)) + 7) / 8);
 	outnl();
 	outs(".type ");
 	outs(s);
@@ -148,10 +152,11 @@ eval_postlude(char *s, exp c)
 void
 out_readonly_section(void)
 {
-	if (format == FORMAT_ELF)
+	if (format == FORMAT_ELF) {
 		outs(".section .rodata");
-	else
+	} else {
 		outs(".text");
+	}
 }
 
 void
@@ -160,7 +165,7 @@ out_dot_comm(char *id, shape sha)
 	outs(".comm ");
 	outs(id);
 	outs(",");
-	outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
+	outn((long) (((shape_size(sha) / 8) + 3) / 4) * 4);
 	outnl();
 }
 
@@ -170,7 +175,7 @@ out_dot_lcomm(char *id, shape sha)
 	outs(".lcomm ");
 	outs(id);
 	outs(",");
-	outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
+	outn((long) (((shape_size(sha) / 8) + 3) / 4) * 4);
 	outnl();
 }
 
@@ -180,38 +185,37 @@ out_bss(char *id, shape sha)
 	outs(".bss ");
 	outs(id);
 	outs(",");
-	outn((long)(((shape_size(sha) / 8) + 3) / 4) * 4);
+	outn((long) (((shape_size(sha) / 8) + 3) / 4) * 4);
 	outnl();
 }
-
-static int pic_label; /* XXX: useless */
 
 void
 pic_prelude(void)
 {
-	int n = next_lab();
-	pic_label = n;
-	outs(" call ");
-	outs(local_prefix);
-	outn((long)n);
-	outnl();
-	outs(local_prefix);
-	outn((long)n);
-	outs(":");
-	outnl();
-	outs(" popl %ebx");
-	outnl();
-	outs(" addl $_GLOBAL_OFFSET_TABLE_+ [.-");
-	outs(local_prefix);
-	outn((long)n);
-	outs("],%ebx");
-	outnl();
+	if (PIC_code) {
+		int n = next_lab();
+		pic_label = n;
+		outs(" call ");
+		outs(local_prefix);
+		outn((long) n);
+		outnl();
+		outs(local_prefix);
+		outn((long) n);
+		outs(":");
+		outnl();
+		outs(" popl %ebx");
+		outnl();
+		outs(" addl $_GLOBAL_OFFSET_TABLE_+ [.-");
+		outs(local_prefix);
+		outn((long)n);
+		outs("],%ebx");
+		outnl();
+	}
 }
 
 void
 out_rename(char *oldid, char *newid)
 {
-	/* XXX */
 	UNUSED(oldid);
 	UNUSED(newid);
 }
@@ -219,47 +223,75 @@ out_rename(char *oldid, char *newid)
 void
 out_switch_jump(int tab, where a, int min)
 {
-	if (PIC_code) {
-		if (min != 0) {
-			sub(slongsh, mw(zeroe,min), a, reg0);
-			a = reg0;
-		}
+	if (!PIC_code) {
+		outs(" jmp *");
+		outs(local_prefix);
+		outn((long) tab);
+		outs("-");
+		outn((long) (4 * min));
+		outs("(,");
+		operand(32, a, 1, 0);
+		outs(",4)");
+		outnl();
+		return;
+	}
 
-		if (eq_where(a, reg0))
-			outs(" movl ");
-		else {
-			outs(" movl %ebx,%eax");
-			outnl();
-			outs(" subl ");
-		}
-
+	/* XXX: this was present for solaris x86. I'm not sure if it's still
+	 * relevant. Possibly it's obsoleted by the code below. */
+	if (assembler == ASM_SUN && PIC_code) {
+		outs(" leal ");
 		outs(local_prefix);
 		outn((long)tab);
 		outs("@GOTOFF(%ebx,");
 		operand(32, a, 1, 0);
 		outs(",4),%eax");
 		outnl();
-
-		if (eq_where(a, reg0)) {
-			outs(" subl %ebx,%eax");
-			outnl();
-			outs(" negl %eax");
-			outnl();
-		}
-
-		outs(" jmp *%eax");
-		outnl();
-	} else {
-		outs(" jmp *");
-		outs(local_prefix);
-		outn((long)tab);
+		outs(" movl ");
 		outs("-");
 		outn((long)(4 * min));
-		outs("(,");
-		operand(32, a, 1, 0);
-		outs(",4)");
+		outs("(%eax),%eax");
+		outnl();
+		outs(" addl %ebx,%eax");
+		outnl();
+		outs(" subl $_GLOBAL_OFFSET_TABLE_+ [.-.L");
+		outn(pic_label);
+		outs("],%eax");
+		outnl();
+		outs(" jmp *%eax");
+		outnl();
+		/* MODIFY FOR NEW CASE !!!!! */
+		return;
+	}
+
+	if (min != 0) {
+		sub(slongsh, mw(zeroe,min), a, reg0);
+		a = reg0;
+	}
+
+	if (eq_where(a, reg0)) {
+		outs(" movl ");
+	} else {
+		outs(" movl %ebx,%eax");
+		outnl();
+		outs(" subl ");
+	}
+
+	outs(local_prefix);
+	outn((long) tab);
+	outs("@GOTOFF(%ebx,");
+	operand(32, a, 1, 0);
+	outs(",4),%eax");
+	outnl();
+
+	if (eq_where(a, reg0)) {
+		outs(" subl %ebx,%eax");
+		outnl();
+		outs(" negl %eax");
 		outnl();
 	}
+
+	outs(" jmp *%eax");
+	outnl();
 }
 
 void
@@ -271,13 +303,14 @@ out_switch_table(int tab, int min, int max, int *v, int absent)
 	outnl();
 
 	outs(local_prefix);
-	outn((long)tab);
+	outn((long) tab);
 	outs(":");
 	outnl();
 
 	for (i = min; i <= max; ++i) {
 		outs(".long ");
-		if (v[i - min]!= -1) {
+
+		if (v[i - min] != -1) {
 			if (PIC_code) {
 				outs(" _GLOBAL_OFFSET_TABLE_+ [.-");
 				outs(local_prefix);
@@ -287,22 +320,24 @@ out_switch_table(int tab, int min, int max, int *v, int absent)
 				outs(local_prefix);
 				outn((long)v[i - min]);
 			}
-		} else {
-			if (absent == -1) {
-				outn((long)0);
-			} else {
-				if (PIC_code) {
-					outs(" _GLOBAL_OFFSET_TABLE_+ [.-");
-					outs(local_prefix);
-					outn((long)absent);
-					outs("]");
-				} else {
-					outs(local_prefix);
-					outn((long)absent);
-				}
-			}
+
+			outnl();
+			continue;
 		}
 
+		if (absent == -1) {
+			outn((long) 0);
+		} else {
+			if (PIC_code) {
+				outs(" _GLOBAL_OFFSET_TABLE_+ [.-");
+				outs(local_prefix);
+				outn((long)absent);
+				outs("]");
+			} else {
+				outs(local_prefix);
+				outn((long)absent);
+			}
+		}
 		outnl();
 	}
 	outnl();
@@ -332,6 +367,12 @@ proc_type(char *s)
 void
 outend(void)
 {
+	if (assembler == ASM_SUN) {
+		dot_align(16);
+		out_close();
+		return;
+	}
+
 	outs(".text");
 	outnl();
 	dot_align(16);
@@ -339,6 +380,7 @@ outend(void)
 	outs("___tdf_end:");
 	outnl();
 
+	/* TODO: linux-specific? */
 	if (format == FORMAT_ELF) {
 		outs(".section .note.GNU-stack,\"\",@progbits");
 		outnl();
@@ -350,11 +392,18 @@ outend(void)
 void
 outopenbr(void)
 {
+	if (assembler == ASM_SUN) {
+		outs("[");
+	}
 }
+
 
 void
 outclosebr(void)
 {
+	if (assembler == ASM_SUN) {
+		outs("]");
+	}
 }
 
 void
@@ -372,53 +421,58 @@ out_initialiser(char *id)
 		outs(" call ");
 		outs(id);
 
-		if (PIC_code)
+		if (PIC_code) {
 			outs("@PLT");
+		}
 
-		outnl();
-		outnl();
 	} else {
 		outs(".stabs \"___TDFI_LIST__\",22,0,0,");
 		outs(id);
-		outnl();
-		outnl();
 	}
+
+	outnl();
+	outnl();
 }
 
 
 void
-out_main_prelude(void)
+out_main_prelude(void) /* FORMAT_AOUT */
 {
-	/* if FORMAT_AOUT */
 	int nl1 = next_lab();
 	int nl2 = next_lab();
+
 	min_rfree |= 0x8;
+
 	outs(" movl $___TDFI_LIST__+4, %ebx\n");
 	outs(local_prefix);
-	outn((long)nl1);
+	outn((long) nl1);
 	outs(":\n");
+
 	outs(" movl (%ebx),%eax\n");
 	outs(" cmpl $0,%eax\n");
 	simple_branch("je", nl2);
 	outs(" call *%eax\n");
 	outs(" addl $4,%ebx\n");
 	simple_branch("jmp", nl1);
+
 	outs(local_prefix);
-	outn((long)nl2);
+	outn((long) nl2);
 	outs(":\n");
 }
 
 void
-out_main_postlude(void)
+out_main_postlude(void) /* FORMAT_AOUT */
 {
-	/* if FORMAT_AOUT */
 	char *sdummy = "Idummy";
-	char *pdummy = (char *)xcalloc(((int)strlen(local_prefix) + (int)strlen(sdummy) + 1), sizeof(char));
+	char *pdummy = xcalloc(strlen(local_prefix) + strlen(sdummy) + 1, 1);
+
 	strcpy(pdummy, local_prefix);
 	strcat(pdummy, sdummy);
+
 	outs(".text\n");
 	outs(pdummy);
 	outs(":\n");
 	outs(" ret\n");
 	out_initialiser(pdummy);
 }
+
