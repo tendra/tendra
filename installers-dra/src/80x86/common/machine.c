2f9d97d027e53e9548f535e49d5afd43f534c9b1
Kate F <kate@elide.org>
2012-11-10 02:56:15 +0000

    Consolidated machine.c for each OS.
    As with assembler.c, these actually contain no OS-specific parts; rather the differences (in fact the only difference I could see) was due to assembler dialect. I've merged these files and made those parts conditional.
    
    I'm not even sure that difference need exist, or if the condition in question ends up identical on Solaris anyhow.
    
    svn path=/trunk/; revision=2853

--- installers-dra/src/80x86/common/machine.c
+++ installers-dra/src/80x86/common/machine.c
@@ -1,16 +1,18 @@
 /* $Id$ */
 
 /*
- * Copyright 2002-2011, The TenDRA Project.
+ * Copyright 2002-2012, The TenDRA Project.
  * Copyright 1997, United Kingdom Secretary of State for Defence.
  *
  * See doc/copyright/ for the full copyright terms.
  */
 
 #include "config.h"
-#include "expmacs.h"
+
 #include <construct/exp.h>
 #include <construct/shapemacs.h>
+
+#include "expmacs.h"
 #include "out.h"
 #include "localflags.h"
 #include "szs_als.h"
@@ -27,17 +29,27 @@ reg_result(shape sha)
 	if (gcc_compatible) {
 		int sz = shape_size(sha);
 
-		if (n == nofhd)
+		if (n == nofhd) {
 			return 0;
+		}
 
-		if (n == cpdhd && sz != 8 && sz != 16 && sz != 32 && sz != 64)
-			return 0;
+		if (assembler == ASM_SUN) {
+			if (n == cpdhd && sz > 32 && sz != 64) {
+				return 0;
+			}
+		} else {
+			if (n == cpdhd && sz != 8 && sz != 16 && sz != 32 && sz != 64) {
+				return 0;
+			}
+		}
 
 		return 1;
 	}
 
-	if (n == cpdhd || n == nofhd)
+	if (n == cpdhd || n == nofhd) {
 		return 0;
+	}
 
 	return 1;
 }
+
