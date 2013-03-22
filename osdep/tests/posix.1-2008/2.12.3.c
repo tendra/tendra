888381b33a0d2351aa91eb58bf025561471c2859
Kate F <kate@elide.org>
2013-03-22 17:38:29 +0000

    Added tests for function pointer conversion to `void *` for POSIX.1-2008.
    svn path=/trunk/; revision=2911

---
+++ osdep/tests/posix.1-2008/2.12.3.c
@@ -0,0 +1,44 @@
+/* $Id$ */
+
+#include <assert.h>
+#include <string.h>
+
+int main(void) {
+
+	/*
+	 * S2.12.3 "All function pointer types shall have the same
+	 * representation as the type pointer to void."
+	 */
+	{
+		void (*f)(void) = 0;
+		void *p = 0;
+		assert(sizeof f == sizeof p);
+		assert(0 == memcmp(&f, &p, sizeof f));
+	}
+
+	/*
+	 * "S2.12.3 "Conversion of a function pointer to void * shall not
+	 * alter the representation."
+	 */
+	{
+		int (*f)(void) = main;
+		void *p = (void *) f;
+		assert(sizeof f == sizeof p);
+		assert(0 == memcmp(&f, &p, sizeof f));
+	}
+
+	/*
+	 * S2.12.3 "A void * value resulting from such a conversion
+	 * can be converted back to the original function pointer type,
+	 * using an explicit cast, without loss of information."
+	 */
+	{
+		int (*f)(void) = main;
+		void *p = (void *) f;
+		int (*g)(void) = (int (*)(void)) p;
+		assert(0 == memcmp(&f, &g, sizeof f));
+	}
+
+	return 0;
+}
+
