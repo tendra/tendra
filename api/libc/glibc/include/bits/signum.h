3b23223f161b8f8641b7bd9dc52ec93555a9ebc4
Kate F <kate@elide.org>
2013-03-25 12:19:26 +0000

    Added permissive pointer conversion for `SIG_IGN` and `SIG_ERR` on glibc 2.7
    svn path=/trunk/; revision=2928

--- api/libc/glibc/include/bits/signum.h
+++ api/libc/glibc/include/bits/signum.h
@@ -3,14 +3,15 @@
 #ifndef __HACKED_LIBC_BITS_SIGNUM_H
 #define __HACKED_LIBC_BITS_SIGNUM_H
 
-#if defined(_GLIBC2_3) || defined(_GLIBC2_11)
+/* for SIG_IGN and SIG_ERR */
+#if defined(_GLIBC2_3) || defined(_GLIBC2_7) || defined(_GLIBC2_11)
 #pragma TenDRA begin
 #pragma TenDRA conversion analysis (pointer-int) off
 #endif
 
 #include_next <bits/signum.h>
 
-#if defined(_GLIBC2_3) || defined(_GLIBC2_11)
+#if defined(_GLIBC2_3) || defined(_GLIBC2_7) || defined(_GLIBC2_11)
 #pragma TenDRA end
 #endif
 
