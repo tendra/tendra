6055c2339dc3d1e9e298af3d3efac0415d4a91fa
Kate F <kate@elide.org>
2013-03-26 21:34:29 +0000

    Some `__WRONG` things which accidentally ended up in the wrong place.
    svn path=/trunk/; revision=2934

--- api/libc/glibc/api/posix.h
+++ api/libc/glibc/api/posix.h
@@ -24,16 +24,3 @@
 #define __WRONG_POSIX_SYS_WAIT_H	/* incompatible dev_t */
 #endif
 
-#ifdef _EGLIBC2_15
-#define __WRONG_POSIX_SYS_STAT_H	/* incompatible dev_t */
-#define __WRONG_POSIX_SYS_TYPES_H_STAT_T	/* incompatible dev_t */
-#define __WRONG_POSIX_SYS_WAIT_H	/* incompatible dev_t */
-#define __WRONG_POSIX_SIGNAL_H	/* incompatible dev_t */
-#define __WRONG_POSIX_PWD_H	/* incompatible dev_t */
-#define __WRONG_POSIX_FCNTL_H	/* incompatible dev_t */
-#define __WRONG_POSIX_GRP_H	/* incompatible dev_t */
-#define __WRONG_POSIX_UTIME_H	/* incompatible dev_t */
-#define __WRONG_POSIX_UNISTD_H_TCPGRP	/* incompatible dev_t */
-#define __WRONG_POSIX_UNISTD_H_U_PROTO	/* incompatible dev_t */
-#endif
-
