c5b5d6abc00ecc648520651fc43867b77ea441ef
Kate F <kate@elide.org>
2013-03-26 04:25:27 +0000

    Updated NetBSD 5.1 support for the `_POSIX_SAVED_IDS` subset.
    svn path=/trunk/; revision=2930

--- api/libc/netbsd/api/posix.h
+++ api/libc/netbsd/api/posix.h
@@ -1,13 +1,12 @@
 /* $Id$ */
 
 /*
- * Copyright 2012, The TenDRA Project.
+ * Copyright 2012-2013, The TenDRA Project.
  *
  * See doc/copyright/ for the full copyright terms.
  */
-
-#ifdef _NETBSD5_1
-#define __WRONG_POSIX_UNISTD_H_U_PROTO /* _POSIX_SAVED_IDS not implemented
-	TODO: purposefully - break this out into its own tspec subsection */
+ 
+#if defined(_NETBSD5_1)
+#define __WRONG_POSIX_UNISTD_H_SAVED_IDS /* not implemented */
 #endif
 
