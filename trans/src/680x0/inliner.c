eab5326eb00fbe1ec16be6b49fcd518c4c747651
Kate F <kate@elide.org>
2013-09-15 11:56:31 +0000

    Missing headers.
    svn path=/trunk/; revision=3002

--- trans/src/680x0/inliner.c
+++ trans/src/680x0/inliner.c
@@ -7,6 +7,8 @@
  * See doc/copyright/ for the full copyright terms.
  */
 
+#include <stddef.h>
+
 #include <shared/check.h>
 
 #include <construct/installglob.h>
--- trans/src/680x0/scan2.c
+++ trans/src/680x0/scan2.c
@@ -15,6 +15,8 @@
 
 */
 
+#include <stdlib.h>
+
 #include <shared/check.h>
 
 #include <construct/exp.h>
