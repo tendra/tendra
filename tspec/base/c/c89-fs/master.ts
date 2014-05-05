e3af09421caf2bf16fdc5e0280b55102eb7479ff
Kate F <kate@elide.org>
2014-05-05 05:37:29 +0000

    Split off the C89 freestanding headers. The standard specifies this as "limited to" those headers, which I read as being defined in terms of c89 proper. Which is lucky, because I want to keep `-Yc89` as a short name, rather than calling it `-Yc89-hosted` or somesuch.
    svn path=/trunk/; revision=3150

---
+++ tspec/base/c/c89-fs/master.ts
@@ -0,0 +1,19 @@
+# $Id$
+
+# Copyright 2002-2011, The TenDRA Project.
+# Copyright 1997, United Kingdom Secretary of State for Defence.
+#
+# See doc/copyright/ for the full copyright terms.
+
+$PROTECT = "" ;
+
+$UNIQUE = 1 ;
+$VERSION = "2.6" ;
++BASE_API ;
+
+# 1.7
++IMPLEMENT "c/c89-fs", "float.h.ts" ;
++IMPLEMENT "c/c89-fs", "limits.h.ts" ;
++IMPLEMENT "c/c89-fs", "stdarg.h.ts" ;
++IMPLEMENT "c/c89-fs", "stddef.h.ts" ;
+
