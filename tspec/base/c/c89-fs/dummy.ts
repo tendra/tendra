87496d8793ca61ff91cc360e9315d70a6ce5304c
Kate F <kate@elide.org>
2014-05-05 19:32:51 +0000

    Add a dummy file with `$INTERFACE` set, to force generation of the `src/c89-fs.api` directory with at least one `.c` file present. This is to stop the build system getting confused and invoking tld with no files, because the `c89-fs` API declares nothing itself, and only inherits from `c89` proper.
    svn path=/trunk/; revision=3152

---
+++ tspec/base/c/c89-fs/dummy.ts
@@ -0,0 +1,13 @@
+# $Id$
+
+# Copyright 2014, The TenDRA Project.
+#
+# See doc/copyright/ for the full copyright terms.
+
+$PROTECT = "";
+$INTERFACE = 1;
+
+%%%
+/* empty API; this file is so that the src/c89-fs.api/ directory is not empty */
+%%%
+
--- tspec/base/c/c89-fs/master.ts
+++ tspec/base/c/c89-fs/master.ts
@@ -11,9 +11,11 @@ $UNIQUE = 1 ;
 $VERSION = "2.6" ;
 +BASE_API ;
 
++IMPLEMENT "c/c89-fs", "dummy.ts" ;
+
 # 1.7
-+IMPLEMENT "c/c89-fs", "float.h.ts" ;
-+IMPLEMENT "c/c89-fs", "limits.h.ts" ;
-+IMPLEMENT "c/c89-fs", "stdarg.h.ts" ;
-+IMPLEMENT "c/c89-fs", "stddef.h.ts" ;
+#+IMPLEMENT "c/c89-fs", "float.h.ts" ;
+#+IMPLEMENT "c/c89-fs", "limits.h.ts" ;
+#+IMPLEMENT "c/c89-fs", "stdarg.h.ts" ;
+#+IMPLEMENT "c/c89-fs", "stddef.h.ts" ;
 
