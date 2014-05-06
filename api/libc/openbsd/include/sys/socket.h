46cad425159d4fb11bc95f359635c3bff41042de
Kate F <kate@elide.org>
2014-05-06 02:00:39 +0000

    Enabled the `xns5-socket` API.
    svn path=/trunk/; revision=3162

---
+++ api/libc/openbsd/include/sys/socket.h
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+#ifndef __HACKED_LIBC_SYS_SOCKET_H
+#define __HACKED_LIBC_SYS_SOCKET_H
+
+/* for off_t et al */
+#if defined(_OPENBSD5_1)
+#if defined(__BUILDING_TDF_XNS5_SOCKET_SYS_SOCKET_H_SA_FAMILY_T)
+#include <sys/types.h>
+#endif
+#endif
+
+#include_next <sys/socket.h>
+
+#endif
+
