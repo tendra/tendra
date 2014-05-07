9be5ddd9a2f2bfae21c3f103e131a749e1b62b3f
Kate F <kate@elide.org>
2014-05-07 16:55:43 +0000

    Clarification.
    svn path=/trunk/; revision=3178

--- tspec/base/cae/xns5-ar/master.ts
+++ tspec/base/cae/xns5-ar/master.ts
@@ -4,6 +4,11 @@
 #
 # See doc/copyright/ for the full copyright terms.
 
+# This is Chapter 11, "IP Address Resolution Headers"
+# of the CAE Networking Services (XNS) Issue 5,
+# Specified by The Open Group. Document number C523,
+# ISBN: 1-85912-165-9 published Feburary 1997.
+
 $UNIQUE = 1;
 $VERSION = "2.8";
 
