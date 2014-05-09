f47233fd0ddcf5b3103fd13f212900efdd2fa60d
Kate F <kate@elide.org>
2014-05-09 17:20:37 +0000

    Clarification.
    svn path=/trunk/; revision=3183

--- tspec/base/cae/xsh4-shm/master.ts
+++ tspec/base/cae/xsh4-shm/master.ts
@@ -4,6 +4,12 @@
 #
 # See doc/copyright/ for the full copyright terms.
 
+# This is the "Shared Memory" (SHARED MEM) Feature Group
+# of XSH Issue 4, "System Interfaces and Headers",
+# which constitutes the C headers part of XPG4.
+# Specified by X/Open as part of CAE. Document number C202,
+# ISBN: 1-872630-47-2 publushed July 1992.
+
 $UNIQUE  = 1;
 $VERSION = "2.6";
 +BASE_API;
