943e2a64c2feee5ac4ebd6b61b8a7877f559de64
Kate F <kate@elide.org>
2014-05-10 17:15:53 +0000

    Added tcc environment generation to the build.
    svn path=/trunk/; revision=3190

--- tspec/mk-local/tendra.tspec.mk
+++ tspec/mk-local/tendra.tspec.mk
@@ -15,15 +15,23 @@ _TENDRA_WORK_TSPEC_MK_=1
 
 TSPEC_TIOUT=	${OBJ_DIR}/share/TenDRA/include
 TSPEC_TSOUT=	${OBJ_DIR}/share/TenDRA/src
+TSPEC_TEOUT=	${OBJ_DIR}/share/TenDRA/env
 
-TSPEC_FLAGS+=	-O${TSPEC_TIOUT}
-TSPEC_FLAGS+=	-S${TSPEC_TSOUT}
+TSPEC_TFLAGS+=	-O${TSPEC_TIOUT}
+TSPEC_TFLAGS+=	-S${TSPEC_TSOUT}
+TSPEC_EFLAGS+=	-E${TSPEC_TEOUT}
 
 
 .for api in ${APIS}
 ${TSPEC_TIOUT}/${api}.api ${TSPEC_TSOUT}/${api}.api: ${TSPEC}
-	@${ECHO} "==> Creating base API source for ${api}"
-	${TSPEC} ${TSPEC_FLAGS} -I${BASE_DIR}/base -b ${api}
+	@${ECHO} "==> Creating API source for ${api}"
+	${TSPEC} ${TSPEC_TFLAGS} -I${BASE_DIR}/base -b ${api}
+.endfor
+
+.for api in ${APIS}
+${TSPEC_TEOUT}/${api}: ${TSPEC}
+	@${ECHO} "==> Creating tcc environment for ${api}"
+	${TSPEC} ${TSPEC_EFLAGS} -I${BASE_DIR}/base -b -y ${api}
 .endfor
 
 
@@ -33,7 +41,8 @@ ${TSPEC_TIOUT}/${api}.api ${TSPEC_TSOUT}/${api}.api: ${TSPEC}
 #
 
 all:: ${TSPEC} ${APIS:S/^/${TSPEC_TIOUT}\//g:S/$/.api/g} \
-               ${APIS:S/^/${TSPEC_TSOUT}\//g:S/$/.api/g}
+               ${APIS:S/^/${TSPEC_TSOUT}\//g:S/$/.api/g} \
+               ${APIS:S/^/${TSPEC_TEOUT}\//g}
 
 
 clean::
