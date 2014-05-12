2def4f472870a635687c43bb216a14446da00f2e
Kate F <kate@elide.org>
2014-05-12 02:42:29 +0000

    Whitespace.
    svn path=/trunk/; revision=3204

--- make_err/src/main.c
+++ make_err/src/main.c
@@ -28,45 +28,55 @@
 int
 main(int argc, char **argv)
 {
-	int act = 0;
-	int ch;
-	char *input = NULL;
-	char *output = NULL;
+	char *input;
+	char *output;
+	int act;
 
-	/* Process arguments */
+	input  = NULL;
+	output = NULL;
+	act = 0;
+
 	set_progname(argv[0], "1.1");
 
-	while ((ch = getopt(argc, argv, "dnuv")) != -1) {
-		switch (ch) {
-		case 'd':
-			act = 1;
-			break;
-		case 'n':
-			act = 2;
-			break;
-		case 'u':
-			act = 3;
-			break;
-		case 'v':
-			report_version();
-			break;
-		default:
-			return 1;
+	{
+		int c;
+
+		while (c = getopt(argc, argv, "dnuv"), c != -1) {
+			switch (c) {
+			case 'd': act = 1; break;
+			case 'n': act = 2; break;
+			case 'u': act = 3; break;
+
+			case 'v':
+				report_version();
+				break;
+
+			default:
+				return 1;
+			}
 		}
+
+		argc -= optind;
+		argv += optind;
 	}
-	argc -= optind;
-	argv += optind;
 
 	/* Check arguments */
-	if (argc >= 1)
+	if (argc >= 1) {
 		input = argv[0];
-	if (argc == 2)
+	}
+
+	if (argc == 2) {
 		output = argv[1];
-	if (argc > 2)
+	}
+
+	if (argc > 2) {
 		error(ERROR_FATAL, "Too many arguments");
+	}
 
 	/* Process the input */
 	process_file(input);
 	output_all(output, act);
+
 	return exit_status;
 }
+
