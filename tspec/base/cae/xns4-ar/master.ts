47691ed2b9991168d03c637843ff24eb2edee2af
Kate F <kate@elide.org>
2014-05-13 16:01:54 +0000

    Clarification.
    svn path=/trunk/; revision=3215

--- tspec/base/cae/xns4-ar/master.ts
+++ tspec/base/cae/xns4-ar/master.ts
@@ -8,7 +8,12 @@
 $UNIQUE  = 1 ;
 $VERSION = "2.7" ;
 
-$INFO = "X/Open CAE Networking Services Issue 4";
+# This is Chapter 11, "IP Address Resolution Headers"
+# of the CAE Networking Services (XNS) Issue 4,
+# Specified by The Open Group. Document number C438,
+# ISBN: 1-85912-049-0 published August 1994.
+
+$INFO = "X/Open CAE XNS Issue 4 - IP Address Resolution Headers";
 
 +IMPLEMENT "cae/xns4-ar", "arpa/inet.h.ts" ;
 
--- tspec/base/cae/xns4-xti/master.ts
+++ tspec/base/cae/xns4-xti/master.ts
@@ -8,7 +8,12 @@
 $UNIQUE  = 1 ;
 $VERSION = "2.7" ;
 
-$INFO = "X/Open CAE Networking Services Issue 4";
+# This is Appendix F, "Headers and Definitions for XTI"
+# of the CAE Networking Services (XNS) Issue 4,
+# Specified by The Open Group. Document number C438,
+# ISBN: 1-85912-049-0 published August 1994.
+
+$INFO = "X/Open CAE XNS Issue 4 - Headers and Definitions for XTI";
 
 +IMPLEMENT "cae/xns4-xti", "xti.h.ts" ;
 
