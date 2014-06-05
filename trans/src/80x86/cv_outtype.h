f174dcc128917306c4a40d7ae7b001a5fd9ce5b1
Kate F <kate@elide.org>
2014-06-05 14:51:57 +0000

    Banner update to the current style.
    svn path=/trunk/; revision=3348

--- trans/src/80x86/cv_outtype.h
+++ trans/src/80x86/cv_outtype.h
@@ -1,83 +1,11 @@
+/* $Id$ */
+
 /*
- * Copyright (c) 2002-2005 The TenDRA Project <http://www.tendra.org/>.
- * All rights reserved.
+ * Copyright 2002-2014, The TenDRA Project.
+ * Copyright 1997, United Kingdom Secretary of State for Defence.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of The TenDRA Project nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific, prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
- * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * $Id: cv_outtype.h 740 2005-09-07 07:36:12Z asmodai $
+ * See doc/copyright/ for the full copyright terms.
  */
-/*
-    		 Crown Copyright (c) 1997
-
-    This TenDRA(r) Computer Program is subject to Copyright
-    owned by the United Kingdom Secretary of State for Defence
-    acting through the Defence Evaluation and Research Agency
-    (DERA).  It is made available to Recipients with a
-    royalty-free licence for its use, reproduction, transfer
-    to other parties and amendment for any purpose not excluding
-    product development provided that any such use et cetera
-    shall be deemed to be acceptance of the following conditions:-
-
-        (1) Its Recipients shall ensure that this Notice is
-        reproduced upon any copies or amended versions of it;
-
-        (2) Any amended version of it shall be clearly marked to
-        show both the nature of and the organisation responsible
-        for the relevant amendment or amendments;
-
-        (3) Its onward transfer from a recipient to another
-        party shall be deemed to be that party's acceptance of
-        these conditions;
-
-        (4) DERA gives no warranty or assurance as to its
-        quality or suitability for any purpose and DERA accepts
-        no liability whatsoever in relation to any use to which
-        it may be put.
-*/
-
-
-/* sco/cv_outtype.h */
-
-/**********************************************************************
-$Author: release $
-$Date: 1998/01/17 15:55:51 $
-$Revision: 1.1.1.1 $
-$Log: cv_outtype.h,v $
- * Revision 1.1.1.1  1998/01/17  15:55:51  release
- * First version to be checked into rolling release.
- *
- * Revision 1.3  1995/03/20  09:23:51  pwe
- * move codeview into sco directory
- *
- * Revision 1.2  1995/01/30  12:57:07  pwe
- * Ownership -> PWE, tidy banners
- *
- * Revision 1.1  1994/07/13  08:33:04  jmf
- * Initial revision
- *
-**********************************************************************/
 
 #ifndef cv_outtype_key
 #define cv_outtype_key 1
