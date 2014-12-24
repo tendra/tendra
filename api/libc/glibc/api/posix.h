2cfc2933fe7956491179f414d72658fd0f358549
Kate F <kate@elide.org>
2014-12-24 09:36:56 +0000

    Provide `CLK_TCK` for POSIX on glibc 2.12.
    svn path=/trunk/; revision=3399

--- api/libc/glibc/api/posix.h
+++ api/libc/glibc/api/posix.h
@@ -6,7 +6,3 @@
  * See doc/copyright/ for the full copyright terms.
  */
 
-#ifdef _GLIBC2_12
-#define __WRONG_POSIX_TIME_H	/* partial */
-#endif
-
--- api/libc/glibc/include/time.h
+++ api/libc/glibc/include/time.h
@@ -3,7 +3,7 @@
 #ifndef __HACKED_LIBC_TIME_H
 #define __HACKED_LIBC_TIME_H
 
-#if defined(_GLIBC2_15)
+#if defined(_GLIBC2_12) || defined(_GLIBC2_15)
 #ifdef __BUILDING_TDF_POSIX_TIME_H
 #define CLK_TCK CLOCKS_PER_SEC
 #endif
