ac6dee814f6ac9e9306815c63312d43c396e78c8
Kate F <kate@elide.org>
2014-12-25 11:34:20 +0000

    Moved the tspec-generated include directory to API environments, rather than specificying a `-I` path in the API-building makefile. This keeps API environments self-contained wrt paths.
    svn path=/trunk/; revision=3407

--- api/shared/mk-local/tendra.api.mk
+++ api/shared/mk-local/tendra.api.mk
@@ -85,7 +85,6 @@ ${OBJ_SDIR}/apis/${api}.api/${src:R}.j: ${PREFIX_TSPEC}/TenDRA/src/${api}.api/${
 	@${CONDCREATE} "${.TARGET:H}"
 	@${ECHO} "==> Compiling ${api}.api/${src}"
 	${TCC} ${HACKOPTS} ${TCCOPTS} ${CCOPTS} ${JOPTS} ${JOPTS${api}} \
-		-I${PREFIX_TSPEC}/TenDRA/include \
 		-Ymakelib -Y${api} -Xp \
 		-o ${.TARGET} ${.ALLSRC}
 
