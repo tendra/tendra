8ce592aff733fc573b58e02e8f2ada3eb6a9ebae
Kate F <kate@elide.org>
2015-01-08 13:05:32 +0000

    Grouped together ABI tokens; renamed LPI tokens.
    svn path=/trunk/; revision=3495

--- osdep/shared/mk/tendra.machtok.mk
+++ osdep/shared/mk/tendra.machtok.mk
@@ -68,20 +68,19 @@ _TENDRA_WORK_MACHTOK_MK_=1
 .endif
 
 TOKENS_COMMON?=	machines/common/tokens
-TOKENS_MODEL?=	model
 TOKENS_EXCEPT?=	except
-TOKENS_MAP?=	map
-TOKENS_FLOAT?=	float
+TOKENS_MODEL?=	abi/model
+TOKENS_FLOAT?=	abi/float
+TOKENS_CHAR?=  	abi/char
+TOKENS_BITF?=  	abi/bitfield
+TOKENS_ALIGN?= 	abi/align
+TOKENS_STRUCT?=	abi/struct
+TOKENS_LPI?=	lpi
 TOKENS_INT?=  	int
-TOKENS_CHAR?=  	char
-TOKENS_BITF?=  	bitfield
-TOKENS_ALIGN?= 	align
-TOKENS_STRUCT?=	struct
 TOKENS_VAR?=   	var
 
-MAP_LANG += c
-MAP_LANG += f
-
+LPI += c
+LPI += f
 
 ${OBJ_SDIR}/c_toks.j: ${BASE_DIR}/${TOKENS_COMMON}/c_toks.tpl
 	@${CONDCREATE} "${OBJ_SDIR}"
@@ -92,48 +91,48 @@ ${OBJ_SDIR}/pun.j: ${BASE_DIR}/${TOKENS_COMMON}/pun.tpl
 	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
 	${TPL} ${.ALLSRC} ${.TARGET}
 
-${OBJ_SDIR}/model_toks.j: ${BASE_DIR}/${TOKENS_MODEL}/${MACHTOK_MODEL}
+${OBJ_SDIR}/abi_model.j: ${BASE_DIR}/${TOKENS_MODEL}/${MACHTOK_MODEL}
 	@${CONDCREATE} "${OBJ_SDIR}"
 	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
 	${TPL} ${.ALLSRC} ${.TARGET}
 
-${OBJ_SDIR}/float_toks.j: ${BASE_DIR}/${TOKENS_FLOAT}/${MACHTOK_FLOAT}
+${OBJ_SDIR}/abi_float.j: ${BASE_DIR}/${TOKENS_FLOAT}/${MACHTOK_FLOAT}
 	@${CONDCREATE} "${OBJ_SDIR}"
 	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
 	${TPL} -I${BASE_DIR}/${TOKENS_FLOAT} ${.ALLSRC} ${.TARGET}
+
+${OBJ_SDIR}/abi_char.j: ${BASE_DIR}/${TOKENS_CHAR}/${MACHTOK_CHAR}
+	@${CONDCREATE} "${OBJ_SDIR}"
+	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
+	${TPL} -I${BASE_DIR}/${TOKENS_CHAR} ${.ALLSRC} ${.TARGET}
+
+${OBJ_SDIR}/abi_bitfield.j: ${BASE_DIR}/${TOKENS_BITF}/${MACHTOK_BITF}
+	@${CONDCREATE} "${OBJ_SDIR}"
+	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
+	${TPL} -I${BASE_DIR}/${TOKENS_BITF} ${.ALLSRC} ${.TARGET}
+
+${OBJ_SDIR}/abi_align.j: ${BASE_DIR}/${TOKENS_ALIGN}/${MACHTOK_ALIGN}
+	@${CONDCREATE} "${OBJ_SDIR}"
+	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
+	${TPL} -I${BASE_DIR}/${TOKENS_ALIGN} ${.ALLSRC} ${.TARGET}
+
+${OBJ_SDIR}/abi_struct.j: ${BASE_DIR}/${TOKENS_STRUCT}/${MACHTOK_STRUCT}
+	@${CONDCREATE} "${OBJ_SDIR}"
+	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
+	${TPL} -I${BASE_DIR}/${TOKENS_STRUCT} ${.ALLSRC} ${.TARGET}
 
 ${OBJ_SDIR}/int_toks.j: ${BASE_DIR}/${TOKENS_INT}/${MACHTOK_INT}
 	@${CONDCREATE} "${OBJ_SDIR}"
 	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
 	${TPL} -I${BASE_DIR}/${TOKENS_INT} ${.ALLSRC} ${.TARGET}
 
-${OBJ_SDIR}/char_toks.j: ${BASE_DIR}/${TOKENS_CHAR}/${MACHTOK_CHAR}
-	@${CONDCREATE} "${OBJ_SDIR}"
-	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
-	${TPL} -I${BASE_DIR}/${TOKENS_CHAR} ${.ALLSRC} ${.TARGET}
-
-${OBJ_SDIR}/bitfield_toks.j: ${BASE_DIR}/${TOKENS_BITF}/${MACHTOK_BITF}
-	@${CONDCREATE} "${OBJ_SDIR}"
-	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
-	${TPL} -I${BASE_DIR}/${TOKENS_BITF} ${.ALLSRC} ${.TARGET}
-
-${OBJ_SDIR}/align_toks.j: ${BASE_DIR}/${TOKENS_ALIGN}/${MACHTOK_ALIGN}
-	@${CONDCREATE} "${OBJ_SDIR}"
-	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
-	${TPL} -I${BASE_DIR}/${TOKENS_ALIGN} ${.ALLSRC} ${.TARGET}
-
-${OBJ_SDIR}/struct_toks.j: ${BASE_DIR}/${TOKENS_STRUCT}/${MACHTOK_STRUCT}
-	@${CONDCREATE} "${OBJ_SDIR}"
-	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
-	${TPL} -I${BASE_DIR}/${TOKENS_STRUCT} ${.ALLSRC} ${.TARGET}
-
 ${OBJ_SDIR}/var_toks.j: ${BASE_DIR}/${TOKENS_VAR}${MACHTOKS_VAR}
 	@${CONDCREATE} "${OBJ_SDIR}"
 	@${ECHO} "==> Translating ${TOKENS_VAR}/${MACHTOKS_VAR}"
 	${TPL} -I${BASE_DIR}/${TOKENS_VAR} ${.ALLSRC} ${.TARGET}
 
-.for lang in ${MAP_LANG}
-${OBJ_SDIR}/map_${lang}.j: ${BASE_DIR}/${TOKENS_MAP}/${lang}.tpl
+.for lang in ${LPI}
+${OBJ_SDIR}/lpi_${lang}.j: ${BASE_DIR}/${TOKENS_LPI}/${lang}.tpl
 	@${CONDCREATE} "${OBJ_SDIR}"
 	@${ECHO} "==> Translating ${WRKDIR}/${.ALLSRC}"
 	${TPL} ${.ALLSRC} ${.TARGET}
@@ -154,28 +153,28 @@ ${OBJ_SDIR}/sys.j: ${OBJ_SDIR}/sys_toks.j
 	@${ECHO} "==> Rewriting ${WRKDIR}/${.TARGET:T}"
 	${TNC} -t -d -L'.~' ${.ALLSRC} ${.TARGET}
 
-.for lang in ${MAP_LANG}
-${OBJ_SDIR}/sys_toks.j: ${OBJ_SDIR}/map_${lang}.j
+.for lang in ${LPI}
+${OBJ_SDIR}/sys_toks.j: ${OBJ_SDIR}/lpi_${lang}.j
 .endfor
 
 ${OBJ_SDIR}/sys_toks.j: ${OBJ_SDIR}/c_toks.j \
 	${OBJ_SDIR}/pun.j \
-	${OBJ_SDIR}/model_toks.j ${OBJ_SDIR}/float_toks.j ${OBJ_SDIR}/int_toks.j \
-	${OBJ_SDIR}/char_toks.j ${OBJ_SDIR}/bitfield_toks.j ${OBJ_SDIR}/align_toks.j ${OBJ_SDIR}/struct_toks.j
+	${OBJ_SDIR}/abi_model.j ${OBJ_SDIR}/abi_float.j ${OBJ_SDIR}/int_toks.j \
+	${OBJ_SDIR}/abi_char.j ${OBJ_SDIR}/abi_bitfield.j ${OBJ_SDIR}/abi_align.j ${OBJ_SDIR}/abi_struct.j
 	@${CONDCREATE} "${OBJ_SDIR}"
 	@${ECHO} "==> Linking ${WRKDIR}/${.TARGET:T}"
 	${TLD} -o ${.TARGET} ${.ALLSRC}
 
 
-.for lang in ${MAP_LANG}
-${OBJ_SDIR}/target_tok.tl: ${OBJ_SDIR}/map_${lang}.j
+.for lang in ${LPI}
+${OBJ_SDIR}/target_tok.tl: ${OBJ_SDIR}/lpi_${lang}.j
 .endfor
 
 # Target-dependent token library
 ${OBJ_SDIR}/target_tok.tl: ${OBJ_SDIR}/pun.j \
 		${OBJ_SDIR}/except_toks.t ${OBJ_SDIR}/var_toks.t \
-		${OBJ_SDIR}/model_toks.j ${OBJ_SDIR}/float_toks.j ${OBJ_SDIR}/int_toks.j \
-		${OBJ_SDIR}/char_toks.j ${OBJ_SDIR}/bitfield_toks.j ${OBJ_SDIR}/align_toks.j ${OBJ_SDIR}/struct_toks.j
+		${OBJ_SDIR}/abi_model.j ${OBJ_SDIR}/abi_float.j ${OBJ_SDIR}/int_toks.j \
+		${OBJ_SDIR}/abi_char.j ${OBJ_SDIR}/abi_bitfield.j ${OBJ_SDIR}/abi_align.j ${OBJ_SDIR}/abi_struct.j
 	@rm -f ${.TARGET}
 	@${CONDCREATE} "${OBJ_SDIR}"
 	@${ECHO} "==> Linking ${WRKDIR}/${.TARGET:T}"
@@ -229,13 +228,13 @@ clean::
 	${RMFILE} ${OBJ_SDIR}/c.tl ${OBJ_DIR}/src/target_tok.tl
 	${RMFILE} ${OBJ_SDIR}/pun.j
 	${RMFILE} ${OBJ_SDIR}/dep_toks.j ${OBJ_SDIR}/c_toks.j
-	${RMFILE} ${OBJ_SDIR}/model_toks.j ${OBJ_SDIR}/float_toks.j ${OBJ_SDIR}/int_toks.j
-	${RMFILE} ${OBJ_SDIR}/char_toks.j ${OBJ_SDIR}/bitfield_toks.j ${OBJ_SDIR}/align_toks.j ${OBJ_SDIR}/struct_toks.j
+	${RMFILE} ${OBJ_SDIR}/abi_model.j ${OBJ_SDIR}/abi_float.j ${OBJ_SDIR}/int_toks.j
+	${RMFILE} ${OBJ_SDIR}/abi_char.j ${OBJ_SDIR}/abi_bitfield.j ${OBJ_SDIR}/abi_align.j ${OBJ_SDIR}/abi_struct.j
 	${RMFILE} ${OBJ_SDIR}/sys.j ${OBJ_SDIR}/sys_toks.j
 	${RMFILE} ${OBJ_SDIR}/except_toks.j ${OBJ_SDIR}/except_toks.t
 	${RMFILE} ${OBJ_SDIR}/var_toks.j ${OBJ_SDIR}/var_toks.t
-.for lang in ${MAP_LANG}
-	${RMFILE} ${OBJ_SDIR}/map_${lang}.j
+.for lang in ${LPI}
+	${RMFILE} ${OBJ_SDIR}/lpi_${lang}.j
 .endfor
 
 
