cea4ab776b4e2611f106a181187cad26e1db1d51
Kate F <kate@elide.org>
2015-01-09 03:14:38 +0000

    Whitespace.
    svn path=/trunk/; revision=3510

--- osdep/gen_tokens/main.c
+++ osdep/gen_tokens/main.c
@@ -7,68 +7,58 @@
  * See doc/copyright/ for the full copyright terms.
  */
 
-
-/************************************************************************/
-/*									*/
-/*		gen_tokens.c	Version 2.0				*/
-/*									*/
-/*  This program produces some of the target dependency tokens		*/
-/*  in 'PL TDF' or 'tnc' format for the target system it is		*/
-/*  compiled and run on.						*/
-/*									*/
-/*  Results are not guaranteed - they should be checked.		*/
-/*									*/
-/*  Each token value may be pre-set by externally defining a macro	*/
-/*  with the appropriate local name and value.				*/
-/*									*/
-/*  Output format is PL TDF by default, 'tnc' if the macro TNC_SRC	*/
-/*  is defined.								*/
-/*									*/
-/************************************************************************/
-
+/*
+ * This program produces some of the target dependency tokens
+ * in 'PL TDF' or 'tnc' format for the target system it is
+ * compiled and run on.
+ *
+ * Results are not guaranteed - they should be checked.
+ *
+ * Each token value may be pre-set by externally defining a macro
+ * with the appropriate local name and value.
+ *
+ * Output format is PL TDF by default, 'tnc' if the macro TNC_SRC
+ * is defined.
+ */
 
 #include <stdio.h>
 #include <stddef.h>
 #include <limits.h>
 
-
 static void
 out_nat(char *name, int value)
 {
 #ifdef TNC_SRC
-	(void)printf("( make_tokdef .~%s nat %d )\n\n", name, value);
-#else	/* PL TDF source */
-	(void)printf("Tokdef .~%s = [] NAT %d;\n\n", name, value);
+	(void) printf("( make_tokdef .~%s nat %d )\n\n", name, value);
+#else
+	(void) printf("Tokdef .~%s = [] NAT %d;\n\n", name, value);
 #endif
-	return;
 }
-
 
 static void
 out_bool(char *name, int value)
 {
 	char *str = (value ? "true" : "false");
 #ifdef TNC_SRC
-	(void)printf("( make_tokdef .~%s bool %s )\n\n", name, str);
-#else	/* PL TDF source */
-	(void)printf("Tokdef .~%s = [] BOOL %s;\n\n", name, str);
+	(void) printf("( make_tokdef .~%s bool %s )\n\n", name, str);
+#else
+	(void) printf("Tokdef .~%s = [] BOOL %s;\n\n", name, str);
 #endif
-	return;
 }
-
 
 static int
 calc_width(unsigned long s_max)
 {
 	int i;
-	for (i=1;;i++) {
-		if ((((unsigned long)1) <<i) > s_max) return i+1;
+
+	for (i = 1; ; i++) {
+		if ((((unsigned long) 1) << i) > s_max) return i + 1;
 	}
+
 	/* UNREACHED */
 }
 
-#define width(c)	calc_width((unsigned long)(c))
-
+#define width(c) calc_width((unsigned long) (c))
 
 int
 main(void)
@@ -108,14 +98,14 @@ main(void)
 #ifdef size_t_width
 	sz_width = size_t_width;
 #else
-	sz_width = sizeof(size_t)* c_width;
+	sz_width = sizeof (size_t) * c_width;
 #endif
 	out_nat("size_t_width", sz_width);
 
 #ifdef ptr_width
 	p_width = ptr_width;
 #else
-	p_width = sizeof(char*)* c_width;
+	p_width = sizeof (char*) * c_width;
 #endif
 	out_nat("ptr_width", p_width);
 
@@ -126,8 +116,8 @@ main(void)
 	{
 		struct t { char c; struct { char c; } s; };
 		struct c { char c; };
-		al_width = offsetof(struct t,s)* c_width;
-		str_ch_width = sizeof(struct c)* c_width;
+		al_width = offsetof(struct t, s) * c_width;
+		str_ch_width = sizeof (struct c) * c_width;
 	}
 #endif
 	if (al_width == str_ch_width) {
@@ -137,7 +127,7 @@ main(void)
 #ifdef char_is_signed
 	c_sgn = char_is_signed;
 #else
-	c_sgn = (CHAR_MIN<0)? 1 : 0;
+	c_sgn = (CHAR_MIN < 0)? 1 : 0;
 #endif
 	out_nat("char_is_signed", c_sgn);
 
@@ -145,9 +135,9 @@ main(void)
 	bf_sgn = bitfield_is_signed;
 #else
 	{
-		struct { int ibits : 2; } s;
+		struct { int ibits :2; } s;
 		s.ibits = -1;
-		bf_sgn = (s.ibits<0)? 1 : 0;
+		bf_sgn = (s.ibits < 0)? 1 : 0;
 	}
 #endif
 	out_nat("bitfield_is_signed", bf_sgn);
@@ -155,7 +145,7 @@ main(void)
 #ifdef best_div
 	bdiv = best_div;
 #else
-	bdiv = (4%(-6) >0) +1;
+	bdiv = (4 % (-6) > 0) +1;
 #endif
 	out_nat("best_div", bdiv);
 
@@ -166,8 +156,8 @@ main(void)
 		unsigned u;
 		u = ('a' << 24) | ('b' << 16) | ('c' << 8) | ('d' << 0);
 		l_end = -1;
-		if (sizeof(u) ==4 && *(char *) &u == 'd')l_end = 1;
-		if (sizeof(u) ==4 && *(char *) &u == 'a')l_end = 0;
+		if (sizeof u == 4 && * (char *) &u == 'd') l_end = 1;
+		if (sizeof u == 4 && * (char *) &u == 'a') l_end = 0;
 	}
 #endif
 	if (l_end >= 0 && l_end <= 1) {
