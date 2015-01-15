78b2ebb268faba1dbcfcc36538d2ca39b875ce1a
Kate F <kate@elide.org>
2015-01-15 04:42:44 +0000

    Print some more things.
    svn path=/trunk/; revision=3566

--- trans/src/stub/translate.c
+++ trans/src/stub/translate.c
@@ -6,30 +6,71 @@
  * See doc/copyright/ for the full copyright terms.
  */
 
+#include <assert.h>
 #include <stdio.h>
 
 #include <construct/installtypes.h>
 #include <construct/installglob.h>
+#include <construct/tags.h>
 
 #include <refactor/optimise.h>
+
+#include <main/flags.h>
+
+static void
+something(dec *d)
+{
+	const char *id;
+	exp c, s;
+
+	assert(d != NULL);
+
+	id = d->dec_u.dec_val.dec_id;
+
+	c = d->dec_u.dec_val.dec_exp;
+	s = son(c);
+
+	if (s == NULL) {
+		shape sha;
+
+		sha = d->dec_u.dec_val.dec_shape;
+
+		fprintf(stderr, "shape %u: %s\n", name(sha), id);
+	} else {
+		switch (name(s)) {
+		case proc_tag:
+		case general_proc_tag:
+			fprintf(stderr, "proc: %s\n", id);
+			break;
+
+		case string_tag:
+			fprintf(stderr, "string: %s = \"%s\"\n", id, nostr(s));
+			break;
+
+		default:
+			fprintf(stderr, "tag %d: %s\n", name(s), id);
+			return;
+		}
+	}
+
+	d->dec_u.dec_val.processed = true;
+}
 
 void 
 translate_capsule(void)
 {
 	dec *d;
 
-	opt_all_exps();
+	if (diag == DIAG_NONE) {
+		opt_all_exps();
+	}
 
 	for (d = top_def; d != NULL; d = d->def_next) {
-		const char *id;
-
 		if (d->dec_u.dec_val.processed) {
 			continue;
 		}
 
-		id = d->dec_u.dec_val.dec_id;
-
-		fprintf(stderr, "dec: %s\n", id);
+		something(d);
 	}
 }
 
