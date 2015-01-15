11e6076119f54b65dbcad04f2d39568a7fe3e264
Kate F <kate@elide.org>
2015-01-15 03:03:32 +0000

    Print something.
    svn path=/trunk/; revision=3565

--- trans/src/stub/translate.c
+++ trans/src/stub/translate.c
@@ -6,11 +6,30 @@
  * See doc/copyright/ for the full copyright terms.
  */
 
+#include <stdio.h>
+
+#include <construct/installtypes.h>
+#include <construct/installglob.h>
+
 #include <refactor/optimise.h>
 
 void 
 translate_capsule(void)
 {
+	dec *d;
+
 	opt_all_exps();
+
+	for (d = top_def; d != NULL; d = d->def_next) {
+		const char *id;
+
+		if (d->dec_u.dec_val.processed) {
+			continue;
+		}
+
+		id = d->dec_u.dec_val.dec_id;
+
+		fprintf(stderr, "dec: %s\n", id);
+	}
 }
 
