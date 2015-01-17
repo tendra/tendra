9f1e5a3df9f3349b9cc3b47596403260e8149283
Kate F <kate@elide.org>
2015-01-17 12:47:19 +0000

    A first attempt at presenting values in a form which suits C's `<float.h>`. The idea here is that the header may derive these values from the TDF ABI tokens, rather than reproducing the same values independently.
    Note these are written in terms of IEEE's ranges, rather than C's ranges. These differ for `FLT_MIN_EXP` and `FLT_MAX_EXP`, where the C macros add a (seemingly arbitary!) +1 offset. That offset is not present in the PL_TDF; it is the header's responsibility to add.
    
    svn path=/trunk/; revision=3581

--- osdep/machines/eglibc/80x86/include/float.h
+++ osdep/machines/eglibc/80x86/include/float.h
@@ -10,38 +10,70 @@
 
 #if defined(_EGLIBC2_19)
 
-#define FLT_RADIX       2 /* b */
-#define FLT_ROUNDS      -1
+#pragma TenDRA token EXP const : int         : __ABI_FLT_RADIX #
 
-#define FLT_DIG         6               /* floor((p-1) * log10(b)) + (b == 10) */
-#define FLT_EPSILON     1.19209290E-07F /* b**(1 - p) */
-#define FLT_MANT_DIG    24              /* p */
-#define FLT_MAX         3.40282347E+38F /* (1 - b**(-p)) * b**emax */
-#define FLT_MAX_10_EXP  38              /* floor(log10((1 - b**(-p)) * b**emax)) */
-#define FLT_MAX_EXP     128             /* emax */
-#define FLT_MIN         1.17549435E-38F /* b**(emin - 1) */
-#define FLT_MIN_10_EXP  (-37)           /* ceil(log10(b**(emin - 1))) */
-#define FLT_MIN_EXP     (-125)          /* emin */
+#pragma TenDRA token EXP const : int         : __ABI_FLT_DIG #
+#pragma TenDRA token EXP const : float       : __ABI_FLT_EPSILON #
+#pragma TenDRA token EXP const : int         : __ABI_FLT_MANT_DIG #
+#pragma TenDRA token EXP const : float       : __ABI_FLT_MIN #
+#pragma TenDRA token EXP const : float       : __ABI_FLT_MAX #
+#pragma TenDRA token EXP const : int         : __ABI_FLT_MIN_10_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_FLT_MAX_10_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_FLT_MIN_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_FLT_MAX_EXP #
 
-#define DBL_DIG         15
-#define DBL_EPSILON     2.2204460492503131E-16
-#define DBL_MANT_DIG    53
-#define DBL_MAX         1.7976931348623157E+308
-#define DBL_MAX_10_EXP  308
-#define DBL_MAX_EXP     1024
-#define DBL_MIN         2.2250738585072014E-308
-#define DBL_MIN_10_EXP  (-307)
-#define DBL_MIN_EXP     (-1021)
- 
-#define LDBL_DIG        18
-#define LDBL_EPSILON    1.08420217248550443401e-19L
-#define LDBL_MANT_DIG   64
-#define LDBL_MAX        1.18973149535723176502e+4932L
-#define LDBL_MAX_10_EXP 4932
-#define LDBL_MAX_EXP    16384
-#define LDBL_MIN        3.36210314311209350626e-4932L
-#define LDBL_MIN_10_EXP (-4931)
-#define LDBL_MIN_EXP    (-16381)
+#pragma TenDRA token EXP const : int         : __ABI_DBL_DIG #
+#pragma TenDRA token EXP const : double      : __ABI_DBL_EPSILON #
+#pragma TenDRA token EXP const : int         : __ABI_DBL_MANT_DIG #
+#pragma TenDRA token EXP const : double      : __ABI_DBL_MIN #
+#pragma TenDRA token EXP const : double      : __ABI_DBL_MAX #
+#pragma TenDRA token EXP const : int         : __ABI_DBL_MIN_10_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_DBL_MAX_10_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_DBL_MIN_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_DBL_MAX_EXP #
+
+#pragma TenDRA token EXP const : int         : __ABI_LDBL_DIG #
+#pragma TenDRA token EXP const : long double : __ABI_LDBL_EPSILON #
+#pragma TenDRA token EXP const : int         : __ABI_LDBL_MANT_DIG #
+#pragma TenDRA token EXP const : long double : __ABI_LDBL_MIN #
+#pragma TenDRA token EXP const : long double : __ABI_LDBL_MAX #
+#pragma TenDRA token EXP const : int         : __ABI_LDBL_MIN_10_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_LDBL_MAX_10_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_LDBL_MIN_EXP #
+#pragma TenDRA token EXP const : int         : __ABI_LDBL_MAX_EXP #
+
+#define FLT_RADIX        __ABI_FLT_RADIX /* b */
+#define FLT_ROUNDS       -1 /* XXX: perhaps this ought to come from TDF. maybe by rounding_mode_cond or round_as_state */
+
+#define FLT_DIG          __ABI_FLT_DIG
+#define FLT_EPSILON      __ABI_FLT_EPSILON
+#define FLT_MANT_DIG     __ABI_FLT_MANT_DIG
+#define FLT_MIN          __ABI_FLT_MIN
+#define FLT_MAX          __ABI_FLT_MAX
+#define FLT_MIN_10_EXP   __ABI_FLT_MIN_10_EXP
+#define FLT_MAX_10_EXP   __ABI_FLT_MAX_10_EXP
+#define FLT_MIN_EXP     (__ABI_FLT_MIN_EXP + 1)
+#define FLT_MAX_EXP     (__ABI_FLT_MAX_EXP + 1)
+
+#define DBL_DIG          __ABI_DBL_DIG
+#define DBL_EPSILON      __ABI_DBL_EPSILON
+#define DBL_MANT_DIG     __ABI_DBL_MANT_DIG
+#define DBL_MIN          __ABI_DBL_MIN
+#define DBL_MAX          __ABI_DBL_MAX
+#define DBL_MIN_10_EXP   __ABI_DBL_MIN_10_EXP
+#define DBL_MAX_10_EXP   __ABI_DBL_MAX_10_EXP
+#define DBL_MIN_EXP     (__ABI_DBL_MIN_EXP + 1)
+#define DBL_MAX_EXP     (__ABI_DBL_MAX_EXP + 1)
+
+#define LDBL_DIG         __ABI_LDBL_DIG
+#define LDBL_EPSILON     __ABI_LDBL_EPSILON
+#define LDBL_MANT_DIG    __ABI_LDBL_MANT_DIG
+#define LDBL_MIN         __ABI_LDBL_MIN
+#define LDBL_MAX         __ABI_LDBL_MAX
+#define LDBL_MIN_10_EXP  __ABI_LDBL_MIN_10_EXP
+#define LDBL_MAX_10_EXP  __ABI_LDBL_MAX_10_EXP
+#define LDBL_MIN_EXP    (__ABI_LDBL_MIN_EXP + 1)
+#define LDBL_MAX_EXP    (__ABI_LDBL_MAX_EXP + 1)
 
 #endif
 
