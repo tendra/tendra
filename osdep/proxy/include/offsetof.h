3c20f0d5db409e66154bf67462741c8262b72478
Kate F <kate@elide.org>
2015-01-27 18:15:10 +0000

    Excess definition. As far as I understand, this should have specified `size_t` as a tokdec, but not produced a tokdef. At least that was my intention. But it seems to have caused a tokdef instead, which gave multiple definitions of `size_t` across various APIs.
    svn path=/trunk/; revision=3687

--- osdep/proxy/include/offsetof.h
+++ osdep/proxy/include/offsetof.h
@@ -3,8 +3,6 @@
 #ifndef __PROXY_OFFSETOF_H
 #define __PROXY_OFFSETOF_H
 
-#pragma TenDRA token VARIETY unsigned size_t #
-
 #pragma TenDRA begin
 #pragma TenDRA keyword __literal for keyword literal
 #pragma TenDRA conversion analysis (pointer-int) off
