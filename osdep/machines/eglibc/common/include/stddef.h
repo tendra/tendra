b01671d15c97ddc148eb093a986b6a028ce2a42d
Kate F <kate@elide.org>
2015-01-27 01:46:10 +0000

    Bring together the architecture-dependent parts of `<stddef.h>` to `common/`, rather than keeping them in individual directories per architecture.
    svn path=/trunk/; revision=3686

--- osdep/machines/eglibc/common/include/stddef.h
+++ osdep/machines/eglibc/common/include/stddef.h
@@ -8,13 +8,13 @@
  * The definitions here must be compatible.
  */
 
-#if defined(_EGLIBC2_15) || defined(_EGLIBC2_19)
+#if defined(_EGLIBC2_11) || defined(_EGLIBC2_15) || defined(_EGLIBC2_19)
 
+#if defined(_ARCH_mips) || defined(_ARCH_x86_32) || defined(_ARCH_x32_64)
 typedef unsigned int size_t;
-
 typedef int ptrdiff_t;
-
 typedef int wchar_t;
+#endif
 
 #include <proxy/include/offsetof.h>
 
--- osdep/machines/glibc/common/include/stddef.h
+++ osdep/machines/glibc/common/include/stddef.h
@@ -8,13 +8,15 @@
  * The definitions here must be compatible.
  */
 
-#if defined(_EGLIBC2_11)
+#if defined(_GLIBC2_5) || defined(_GLIBC2_7) || defined(_GLIBC2_11) \
+	|| defined(_GLIBC2_12) || defined(_GLIBC2_14) || defined(_GLIBC2_15) \
+	|| defined(_GLIBC2_17) || defined(_GLIBC2_18) || defined(_GLIBC2_19)
 
+#if defined(_ARCH_x86_32) || defined(_ARCH_x32_64)
 typedef unsigned int size_t;
-
 typedef int ptrdiff_t;
-
-typedef int wchar_t;
+typedef long int wchar_t;
+#endif
 
 #include <proxy/include/offsetof.h>
 
