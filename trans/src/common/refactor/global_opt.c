ba9df368dfd7fda889133e1d0bfffd0e4ffa484c
Kate F <kate@elide.org>
2015-02-10 09:48:39 +0000

    Cenralise glopts; none of these seem platform-specific.
    svn path=/trunk/; revision=3792

--- trans/src/common/refactor/global_opt.c
+++ trans/src/common/refactor/global_opt.c
@@ -8,9 +8,13 @@
  */
 
 #include <stddef.h>
+#include <string.h>
 
 #include <shared/error.h>
 
+#include <reader/externs.h>
+#include <reader/table_fns.h>
+#include <reader/basicread.h>
 
 #include <construct/installglob.h>
 #include <construct/exp.h>
@@ -38,6 +42,125 @@ rec_inl(exp p)
 	return;
 }
 
+/*
+ * This function replaces calls to the strcpy function
+ * with a direct mem -> mem copy.
+ */
+static void
+common_opt(dec *dp)
+{
+	if (!writable_strings && !strcmp(dp->dec_u.dec_val.dec_id, "strcpy")) {
+		exp i = dp->dec_u.dec_val.dec_exp;
+		exp t;
+
+		for (t = pt(i); t != NULL; t = pt(t)) {
+			if (!last(t) && !last(bro(t)) && last(bro(bro(t))) &&
+			    name(bro(bro(bro(t)))) == apply_tag &&
+			    son(bro(bro(bro(t)))) == t)
+			{
+				exp dest = bro(t);
+				exp source = bro(dest);
+
+				if (name(source) == name_tag && isglob(son(source)) &&
+				    isvar(son(source)) && no(son(source)) == 1)
+				{
+					dec *source_dec = brog(son(source));
+					if (!source_dec->dec_u.dec_val.extnamed &&
+					    son(source_dec->dec_u.dec_val.dec_exp) != NULL)
+					{
+						exp source_def = son(son(source));
+						shape sha = sh(source_def);
+						if (name(source_def) == string_tag &&
+#if defined(ARCH_alpha) || defined(ARCH_m68k)
+						    ptno(source_def) == 8)
+#else
+						    props(source_def) == 8)
+#endif
+						{
+							char *s = nostr(source_def);
+							size_t l = shape_size(sha) / 8;
+							size_t j;
+
+							for (j = 0; j < l && s[j] != 0; j++)
+								;
+
+							if (j < l) {
+								exp q;
+								exp to_change = bro(source);
+								exp idsc = getexp(sh(bro(source)), NULL, 0, dest, NULL, 0, 2, ident_tag);
+								exp n1 = getexp(sh(dest), NULL, 0, idsc, NULL, 0, 0, name_tag);
+								exp n2 = getexp(sh(dest), NULL, 0, idsc, n1, 0, 0, name_tag);
+								exp_list el;
+
+								pt(idsc) = n2;
+
+								q = f_assign(n1, f_contents(sha, source));
+								el.start = q;
+								el.end = q;
+								el.number = 1;
+
+								q = f_sequence(el, n2);
+								clearlast(dest);
+								bro(dest) = q;
+								setfather(idsc, q);
+								kill_exp(t, t);
+								replace(to_change, idsc, idsc);
+								t = pt(i);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if (!writable_strings && !strcmp(dp->dec_u.dec_val.dec_id, "strlen")) {
+		exp i = dp->dec_u.dec_val.dec_exp;
+		exp t;
+
+		for (t = pt(i); t != NULL; t = pt(t)) {
+			if (!last(t) && last(bro(t)) &&
+			    name(bro(bro(t))) == apply_tag &&
+			    son(bro(bro(t))) == t) {
+				exp st = bro(t);
+
+				if (name(st) == name_tag && isglob(son(st)) &&
+				    isvar(son(st)) && no(son(st)) == 1)
+				{
+					dec *source_dec = brog(son(st));
+					if (!source_dec->dec_u.dec_val.extnamed &&
+					    son(source_dec->dec_u.dec_val.dec_exp) != NULL)
+					{
+						exp st_def = son(son(st));
+						shape sha = sh(st_def);
+						if (name(st_def) == string_tag &&
+#if defined(ARCH_alpha) || defined(ARCH_m68k)
+						    ptno(st_def) == 8)
+#else
+						    props(st_def) == 8)
+#endif
+						{
+							char *s = nostr(st_def);
+							size_t l = shape_size(sha) / 8;
+							size_t j;
+
+							for (j = 0; j < l && s[j] != 0; j++)
+								;
+
+							if (j < l) {
+								exp to_change = bro(st);
+								exp res = getexp(sh(to_change), NULL, 0, NULL, NULL, 0, j, val_tag);
+								kill_exp(t, t);
+								replace(to_change, res, NULL);
+								t = pt(i);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
 
 void
 opt_all_exps(void)
@@ -57,6 +180,7 @@ opt_all_exps(void)
 	for (my_def = top_def; my_def != NULL; my_def = my_def -> def_next) {
 		exp crt_exp = my_def->dec_u.dec_val.dec_exp;
 		refactor_ext(crt_exp);
+		common_opt(my_def);
 		global_opt(my_def);
 	}
 
