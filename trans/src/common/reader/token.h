a24fc986a8aec530ac71a683e692f4faafb554a4
Kate F <kate@elide.org>
2015-03-21 17:21:33 +0000

    Merge `sort_union.h` into `token.h` (it describes the contents of a token, which only happen incidentally to be SORTs).
    svn path=/trunk/; revision=4210

--- trans/src/common/reader/token.h
+++ trans/src/common/reader/token.h
@@ -94,7 +94,57 @@ typedef int linkinfo_props;
 
 typedef char usage;
 
-#include <reader/sort_union.h>
+
+#include <tdf/nat.h>
+
+#include <construct/installtypes.h>
+#include <construct/aldefs.h>
+
+#ifdef TDF_DIAG4
+#include <diag4/dg_first.h>
+#include <diag4/dg_types.h>
+#else
+#include <diag3/dg_first.h>
+#include <diag3/dg_types.h>
+#endif
+
+
+/*
+ * the union of the representations of all sorts:
+ * used in table_fns for token application
+ */
+
+union tokval_u {
+	alignment		tk_alignment;
+	al_tag			tk_al_tag;
+	bitfield_variety	tk_bitfield_variety;
+	bool			tk_bool;
+	error_treatment		tk_error_treatment;
+	exp			tk_exp;
+	floating_variety	tk_floating_variety;
+	label			tk_label;
+	nat			tk_nat;
+	ntest			tk_ntest;
+	rounding_mode		tk_rounding_mode;
+	shape			tk_shape;
+	signed_nat		tk_signed_nat;
+	tag			tk_tag;
+	struct tok_define_t	*tk_token;
+	transfer_mode		tk_transfer_mode;
+	variety			tk_variety;
+	diag_type		tk_diag_type;	/* TDF_DIAG3 */
+	filename		tk_filename;	/* TDF_DIAG3 */
+	access			tk_access;
+	procprops		tk_procprops;
+	string			tk_string;
+	dg			tk_dg;		/* TDF_DIAG4 */
+	dg_dim			tk_dg_dim;	/* TDF_DIAG4 */
+	dg_filename		tk_dg_filename;	/* TDF_DIAG4 */
+	dg_idname		tk_dg_idname;	/* TDF_DIAG4 */
+	dg_name			tk_dg_name;	/* TDF_DIAG4 */
+	dg_type			tk_dg_type;	/* TDF_DIAG4 */
+};
+
 
 #define LOCAL_TOKS 4
 
