d5821f59241a2600c655f38be58082b8e8a35a4e
Kate F <kate@elide.org>
2015-03-22 17:43:24 +0000

    Whitespace.
    svn path=/trunk/; revision=4226

--- trans/src/common/construct/dec.h
+++ trans/src/common/construct/dec.h
@@ -18,48 +18,44 @@
 #endif
 
 
- /* structure which describes a declaration */
+/* structure which describes a declaration */
 struct dec_t {
+	/*
+	 * If this is a global declaration this field holds next global
+	 * declaration which needs processing. If there is none, it is NULL.
+	 */
 	struct dec_t *def_next;
-	/* if this is a global declaration this field holds next global
-	   declaration which needs processing. If there is none, it is NULL */
-	char *dec_id;
-	/* identifier to be used */
-	shape dec_shape;
-	/* shape of the value */
-	exp dec_exp;
-	/* definition or NULL */
+
+	char *dec_id;    /* identifier to be used */
+	shape dec_shape; /* shape of the value */
+	exp dec_exp;     /* definition or NULL */
+
 	int unit_number;
+
 #ifdef TDF_DIAG4
 	struct dg_name_t *dg_name;
 #else
 	diag_descriptor *diag_info;
 #endif
+
 	access acc;
-	int index;
-	/* used in inl_norm to hold procedure index number */
-	
+
+	int index; /* used in inl_norm to hold procedure index number */
+
 #if TRANS_MIPS || TRANS_SPARC || TRANS_POWER || TRANS_ALPHA || TRANS_HPPA
 	int sym_number;
 #endif
 
 	/* bitfields only after this */
-	unsigned int extnamed :1;
-	/* external */
-	unsigned int dec_var :1;
-	/* this is a variable */
-	unsigned int dec_outermost :1;
-	/* this is global */
-	unsigned int have_def :1;
-	/* we have a definition */
-	unsigned int processed :1;
-	/* this exp has been output */
-	unsigned int isweak :1;
-	/* definition is weak */
-	unsigned int is_common :1;
-	/* declaration is common */
-	unsigned int has_signature : 1;
-	/* declaration has signature */
+
+	unsigned int extnamed      :1; /* external */
+	unsigned int dec_var       :1; /* this is a variable */
+	unsigned int dec_outermost :1; /* this is global */
+	unsigned int have_def      :1; /* we have a definition */
+	unsigned int processed     :1; /* this exp has been output */
+	unsigned int isweak        :1; /* definition is weak */
+	unsigned int is_common     :1; /* declaration is common */
+	unsigned int has_signature :1; /* declaration has signature */
 };
 
 typedef struct dec_t dec;
