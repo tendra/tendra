d7920bcdcfe806a5dc09e26d0278e7cc66db9d9c
Kate F <kate@elide.org>
2016-03-21 21:48:46 +0000

    Grouped by file.
    svn path=/trunk/; revision=4466

--- trans/src/m68k/ops.h
+++ trans/src/m68k/ops.h
@@ -14,37 +14,7 @@ extern bool have_cond;
 extern bool D1_is_special;
 extern int crt_rmode;
 
-extern void absop(shape, where, where);
-extern void add(shape, where, where, where);
-extern void and(shape, where, where, where);
-extern void bitf_to_int(exp, shape, where, ash);
-extern void bit_test(shape, where, where);
-extern void callins(long, exp);
-extern void jmpins(exp);
-extern void trap_ins(int);
-extern void change_var_sh(shape, shape, where, where);
-extern void change_var(shape, where, where);
-extern bool cmp(shape, where, where, long);
-extern void div1(shape, where, where, where);
-extern void div2(shape, where, where, where);
-extern void int_to_bitf(exp, exp, ash);
-extern void maxop(shape, where, where, where);
-extern void minop(shape, where, where, where);
-extern void mova(where, where);
-extern void move(shape, where, where);
-extern void move_const(shape, long, long, where);
-extern void move_bytes(long, where, where, int);
-extern void mult(shape, where, where, where);
-extern void negate(shape, where, where);
-extern void not(shape, where, where);
-extern void or(shape, where, where, where);
-extern void rem1(shape, where, where, where);
-extern void rem2(shape, where, where, where);
-extern void rshift(shape, where, where, where);
-extern void shift(shape, where, where, where);
-extern void sub(shape, where, where, where);
-extern void xor(shape, where, where, where);
-
+/* ops_float.c */
 extern void abs_float(shape, where, where);
 extern void change_flvar(shape, where, where);
 extern void fl_binop(int, shape, where, where, where);
@@ -52,8 +22,43 @@ extern void int_to_float(shape, where, where);
 extern void negate_float(shape, where, where);
 extern void round_float(shape, where, where);
 
+/* ops_int.c */
+extern void add(shape, where, where, where);
+extern void sub(shape, where, where, where);
+extern void negate(shape, where, where);
+extern void mult(shape, where, where, where);
+extern void div1(shape, where, where, where);
+extern void div2(shape, where, where, where);
+extern void rem1(shape, where, where, where);
+extern void rem2(shape, where, where, where);
+extern void maxop(shape, where, where, where);
+extern void minop(shape, where, where, where);
+extern void absop(shape, where, where);
+
+/* ops_misc.c */
+extern void trap_ins(int);
+extern void callins(long, exp);
+extern void jmpins(exp);
+extern bool cmp(shape, where, where, long);
+extern void move_const(shape, long, long, where);
+extern void move_bytes(long, where, where, int);
+extern void move(shape, where, where);
+extern void mova(where, where);
 extern long range_max(shape);
 extern long range_min(shape);
+extern void change_var_sh(shape, shape, where, where);
+extern void change_var(shape, where, where);
+
+/* ops_logic.c */
+extern void and(shape, where, where, where);
+extern void or(shape, where, where, where);
+extern void xor(shape, where, where, where);
+extern void not(shape, where, where);
+extern void rshift(shape, where, where, where);
+extern void shift(shape, where, where, where);
+extern void bitf_to_int(exp, shape, where, ash);
+extern void int_to_bitf(exp, exp, ash);
+extern void bit_test(shape, where, where);
 
 extern int overflow_jump;
 extern int err_continue;
