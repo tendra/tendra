d63ccfbc500eb77e13f20a425e4795f9900d144d
Kate F <kate@elide.org>
2020-08-09 04:05:54 -0700

    Missing handling for 10.4.

--- osdep/machines/freebsd/common/include/sys/signal.h
+++ osdep/machines/freebsd/common/include/sys/signal.h
@@ -5,6 +5,7 @@
 #if defined(_FREEBSD8_3) \
 	|| defined(_FREEBSD9_0) || defined(_FREEBSD9_1) || defined(_FREEBSD9_2) \
 	|| defined(_FREEBSD10_1) || defined(_FREEBSD10_2) || defined(_FREEBSD10_3) \
+	|| defined(_FREEBSD10_4) \
 	|| defined(_FREEBSD11_0)
 #pragma TenDRA begin
 #pragma TenDRA conversion analysis ( int-pointer explicit ) off
@@ -15,6 +16,7 @@
 #if defined(_FREEBSD8_3) \
 	|| defined(_FREEBSD9_0) || defined(_FREEBSD9_1) || defined(_FREEBSD9_2) \
 	|| defined(_FREEBSD10_1) || defined(_FREEBSD10_2) || defined(_FREEBSD10_3) \
+	|| defined(_FREEBSD10_4) \
 	|| defined(_FREEBSD11_0)
 #pragma TenDRA end
 #endif
