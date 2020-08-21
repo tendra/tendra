66f18922979309fd38b35f9781b0ba39bbee821b
Partouf <partouf@gmail.com>
2020-08-21 12:16:58 +0200

    32bit things

--- osdep/machines/glibc/x32_64/src/crtbegin.s
+++ osdep/machines/glibc/x32_64/src/crtbegin.s
@@ -1,9 +1,8 @@
 
 	.global __dso_handle
 	.type __dso_handle,@object
-	.size __dso_handle,8
-	.section .sbss
-	.align 8
+	.size __dso_handle,4
+	.align 4
 __dso_handle:
-	.long __dso_handle
+	.int __dso_handle
 	.hidden __dso_handle
