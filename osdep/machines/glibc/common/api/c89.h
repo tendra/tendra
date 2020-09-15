31283efb60375797149f29cbb35213f830d8e8cd
itiel <itiel.sadeh@gmail.com>
2020-09-15 11:55:46 +0300

    Add support for glibc 2.32

--- osdep/machines/glibc/common/api/c89.h
+++ osdep/machines/glibc/common/api/c89.h
@@ -4,7 +4,8 @@
  * See doc/copyright/ for the full copyright terms.
  */
 
-#if defined(_GLIBC2_14) || defined(_GLIBC2_31)
+#if defined(_GLIBC2_14) || defined(_GLIBC2_31) \
+	|| defined(_GLIBC2_32)
 #define __WRONG_C89_FLOAT_H_XPG4_RANGES /* provided by gcc; we need to provide our own */
 #endif
 
